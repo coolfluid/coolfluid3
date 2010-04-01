09fe72ae52d9efcffb2c4fd45c9ab83392ecc460
gquentin <gquentin@gmail.com>
2010-04-01 06:36:33 +0000

    QG: cmake was crashing on Linux. Fixed.
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@10 4892e656-8fb9-448a-b5bc-b1919152596f

--- cmake/CheckExplicitInstantiation.cmake
+++ cmake/CheckExplicitInstantiation.cmake
@@ -4,36 +4,36 @@
 # both the instantiation syntax and symbol linkage is handled
 # properly.
 
- MAKE_DIRECTORY(${COOLFluiD_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation)
+ MAKE_DIRECTORY(${coolfluid_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation)
  STRING(ASCII 35 POUND)
  WRITE_FILE(
-   ${COOLFluiD_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation/CMakeLists.txt
+   ${coolfluid_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation/CMakeLists.txt
    "PROJECT(EXPLICIT)\n"
    "ADD_LIBRARY(A A.cxx)\n"
    "ADD_EXECUTABLE(B B.cxx)\n"
    "TARGET_LINK_LIBRARIES(B A)\n"
    )
  WRITE_FILE(
-   ${COOLFluiD_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation/A.h
+   ${coolfluid_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation/A.h
    "${POUND}ifndef A_h\n"
    "${POUND}define A_h\n"
    "template <class T> class A { public: static T Method(); };\n"
    "${POUND}endif\n"
    )
  WRITE_FILE(
-   ${COOLFluiD_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation/A.cxx
+   ${coolfluid_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation/A.cxx
    "${POUND}include \"A.h\"\n"
    "template <class T> T A<T>::Method() { return 0; }\n"
    "template class A<int>;"
    )
  WRITE_FILE(
-   ${COOLFluiD_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation/B.cxx
+   ${coolfluid_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation/B.cxx
    "${POUND}include \"A.h\"\n"
    "int main() { return A<int>::Method(); }\n"
    )
  TRY_COMPILE( CF_CXX_SUPPORTS_EXPLICIT_TEMPLATES
-   ${COOLFluiD_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation/Build
-   ${COOLFluiD_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation
+   ${coolfluid_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation/Build
+   ${coolfluid_BINARY_DIR}/CMakeTmp/CheckExplicitInstantiation
    EXPLICIT OUTPUT_VARIABLE OUTPUT
    )
  
