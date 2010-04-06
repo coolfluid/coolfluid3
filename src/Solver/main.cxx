b091ea2012593fec2e4fe8009b657ac0bd41f9d0
gquentin <gquentin@gmail.com>
2010-04-06 10:19:38 +0000

    QG: removed other dummy things
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@19 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Solver/main.cxx
+++ src/Solver/main.cxx
@@ -1,18 +1,11 @@
 #include <iostream>
 
-#include "Common/DummyCommon.hh"
-#include "Math/DummyMath.hh"
-
-using namespace COOLFluiD::Common;
-using namespace COOLFluiD::Math;
+using namespace CF::Common;
+using namespace CF::Math;
 
 int main(int argc, char * argv[])
 {
- DummyCommon dc;
- DummyMath dm;
-    
- std::cout << "The string is : " << dc.getString() << std::endl;
- std::cout << "The answer is : " << dm.getAnswer() << std::endl;
+ std::cout << "Welcome to the COOLFluiD K3 solver!" << std::endl;
     
  return 0;
 }
