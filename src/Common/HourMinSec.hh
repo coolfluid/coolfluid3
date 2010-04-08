2848f9ae2d8385a84e2044b6a5be4dd917143b45
gquentin <gquentin@gmail.com>
2010-04-08 15:14:54 +0000

    QG: * imported HourMinSec, Stopwatch and TimePolicies classes * moved all the contents of Stopwatch.ci file to the end of Stopwatch.hh file
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@53 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ src/Common/HourMinSec.hh
@@ -0,0 +1,64 @@
+#ifndef COOLFluiD_Common_HourMinSec_hh
+#define COOLFluiD_Common_HourMinSec_hh
+
+#include "CF.hh"
+#include "CommonAPI.hh"
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace CF {
+
+  namespace Common {
+
+//////////////////////////////////////////////////////////////////////////////
+
+/// This class represents a place holder for some timing data.
+/// @author Andrea Lani
+/// @author Tiago Quintino
+class Common_API HourMinSec {
+public:
+
+  /// Default constructor without arguments
+  HourMinSec();
+
+  /// Constructor with nb of seconds.
+  /// @param sec number of seconds to initializes
+  HourMinSec(const CF::Real& sec);
+
+  /// Default destructor
+  ~HourMinSec();
+
+  /// Conversion to std::string
+  std::string str() const;
+
+  /// Set the values corresponding to the given time (in sec)
+  void set(const CF::Real& total);
+
+public:
+  /// number of seconds per hour
+  static const CF::Real secPerHour;
+  /// number of seconds per minute
+  static const CF::Real secPerMin;
+  /// number of micro seconds per second
+  static const CF::Real usecPerSec;
+
+private:
+  /// number of hours
+  CF::Real m_h;
+  /// number of minutes
+  CF::Real m_m;
+  /// number of seconds
+  CF::Real m_s;
+
+}; // end HourMinSec
+
+//////////////////////////////////////////////////////////////////////////////
+
+  } // namespace Common
+
+} // namespace CF
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif // COOLFluiD_Common_HourMinSec_hh
+
