94913a32a5fb6f98910f801329d6256d7f7eb56b
tlmquintino <tlmquintino@gmail.com>
2010-04-15 18:22:31 +0000

    TQ: renamed file
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@120 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Solver/coolfluid-solver.cpp
+++ src/Solver/coolfluid-solver.cpp
@@ -1,13 +1,15 @@
 #include "Common/Log.hpp"
 #include "Common/Component.hpp"
-#include "Common/StringOps.hpp"
 
 using namespace CF;
 using namespace CF::Common;
 
 int main(int argc, char * argv[])
 {
-CFinfo << "Welcome to the COOLFLUID K3 solver!\n" << CFendl;
+  CFinfo << "Welcome to the COOLFLUID K3 solver!\n" << CFendl;
 
-return 0;
+
+
+
+  return 0;
 }
