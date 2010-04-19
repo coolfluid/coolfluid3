c46ac068020cf81efee1dd5cefa0b2b493d967a7
wdeconinck <ir.willem.deconinck@gmail.com>
2010-04-19 14:51:54 +0000

    WD: Adding some VERY basic files to Mesh
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@136 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ src/Mesh/Mesh.hpp
@@ -0,0 +1,31 @@
+#ifndef CF_Mesh_HH
+#define CF_Mesh_HH
+
+////////////////////////////////////////////////////////////////////////////////
+
+// #include "Common/ExportAPI.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+
+/// Define the macro Math_API
+/// @note build system defines Math_EXPORTS when compiling MathTools files
+// #ifdef Math_EXPORTS
+// #   define Math_API      CF_EXPORT_API
+// #   define Math_TEMPLATE
+// #else
+// #   define Math_API      CF_IMPORT_API
+// #   define Math_TEMPLATE CF_TEMPLATE_EXTERN
+// #endif
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace CF {
+
+  /// Basic Classes for Mathematical applications used by the CF
+  namespace Mesh {}
+
+} // namespace Math
+
+////////////////////////////////////////////////////////////////////////////////
+
+#endif // CF_Mesh_HH
