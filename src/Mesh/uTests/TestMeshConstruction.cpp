902ff5fbbb0ea9b8baf864e09629dc26faefe21f
wdeconinck <ir.willem.deconinck@gmail.com>
2010-04-25 21:16:45 +0000

    WD: triag/quad hybrid mesh "hand created "
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@188 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Mesh/uTests/TestMeshConstruction.cpp
+++ src/Mesh/uTests/TestMeshConstruction.cpp
@@ -48,6 +48,14 @@ struct MeshConstruction_Fixture
     quadVec.assign(quad,quad+4);
     return quadVec;
   }
+  
+  std::vector<Uint> create_triag(const Uint& A, const Uint& B, const Uint& C) {
+    Uint triag[] = {A,B,C};
+    std::vector<Uint> triagVec;
+    triagVec.assign(triag,triag+3);
+    return triagVec;
+  }
+  
   /// common values accessed by all tests goes here
 
 };
@@ -76,13 +84,17 @@ BOOST_AUTO_TEST_CASE( MeshConstruction )
   p_mesh->get_component("superRegion").d_castTo<CRegion>()->create_region("quads");
   p_mesh->get_component("superRegion").d_castTo<CRegion>()->create_region("triags");
   
-  // create a pointer to the quads region
+  // create a pointer to the quads and triag region
   SafePtr<CRegion> quadRegion = 
     p_mesh->get_component("superRegion")->get_component("quads").d_castTo<CRegion>(); 
-  
-  // create connectivity table and element type in the quads region
+  SafePtr<CRegion> triagRegion = 
+  p_mesh->get_component("superRegion")->get_component("triags").d_castTo<CRegion>(); 
+
+  // create connectivity table and element type in the quads and triags region
   quadRegion->create_connectivityTable("table");
   quadRegion->create_elementType("type");
+  triagRegion->create_connectivityTable("table");
+  triagRegion->create_elementType("type");
   
   // create a coordinates array in the mesh component
   p_mesh->create_array("coordinates");
@@ -90,18 +102,26 @@ BOOST_AUTO_TEST_CASE( MeshConstruction )
   // create pointers to the coordinates array and connectivity table
   SafePtr<CArray::Array> coordinates = (&p_mesh->get_component("coordinates").d_castTo<CArray>()->getArray());
   SafePtr<CTable::ConnectivityTable> qTable = (&quadRegion->get_component("table").d_castTo<CTable>()->getTable());
-  
-  // initialize the coordinates array and connectivity table
+  SafePtr<CTable::ConnectivityTable> tTable = (&triagRegion->get_component("table").d_castTo<CTable>()->getTable());
+
+  // initialize the coordinates array and connectivity tables
   coordinates->initialize(2,20);
   qTable->initialize(4,10);
+  tTable->initialize(3,10);
   
-  //  Mesh of quads with node numbering:
+  //  Mesh of quads and triangles with node numbering:
   //
-  //    5----4 
-  //    |    | 
-  //    3----2 
-  //    |    | 
-  //    0----1   
+  //    5----4----6    
+  //    |    |\   |
+  //    |    | \  |
+  //    |    |  \ |
+  //    |    |   \|
+  //    3----2----7
+  //    |    |\   |
+  //    |    | \  |
+  //    |    |  \ |
+  //    |    |   \|
+  //    0----1----8   
   
   // fill coordinates in the buffer
   coordinates->add_row(create_coord( 0.0 , 0.0 ));  // 0
@@ -110,15 +130,25 @@ BOOST_AUTO_TEST_CASE( MeshConstruction )
   coordinates->add_row(create_coord( 0.0 , 1.0 ));  // 3
   coordinates->add_row(create_coord( 0.0 , 2.0 ));  // 4
   coordinates->add_row(create_coord( 1.0 , 1.0 ));  // 5
+  coordinates->add_row(create_coord( 2.0 , 2.0 ));  // 6
+  coordinates->add_row(create_coord( 2.0 , 1.0 ));  // 7
+  coordinates->add_row(create_coord( 2.0 , 0.0 ));  // 8
+
   
   // fill connectivity in the buffer
   qTable->add_row(create_quad( 0 , 1 , 2 , 3 ));
   qTable->add_row(create_quad( 3 , 2 , 4 , 5 ));
+  
+  tTable->add_row(create_triag( 1, 8 , 2 ));
+  tTable->add_row(create_triag( 8, 7 , 2 ));
+  tTable->add_row(create_triag( 2, 7 , 4 ));
+  tTable->add_row(create_triag( 7, 6 , 4 ));
 
   // flush buffers into the table. 
   // This causes the table and array to be resized and filled.
   coordinates->flush();
   qTable->flush();
+  tTable->flush();
   
 }
 
