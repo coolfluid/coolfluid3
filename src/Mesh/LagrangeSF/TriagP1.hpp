7da18c6a863df81774897a8424b0e94bab73c6f1
barche <bart.janssens@lid.kviv.be>
2010-04-27 21:02:03 +0000

    BJ: Add computeJacobianDeterminant test for TriagP1 shapefunction
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@207 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Mesh/LagrangeSF/TriagP1.hpp
+++ src/Mesh/LagrangeSF/TriagP1.hpp
@@ -8,6 +8,9 @@ namespace CF {
 namespace Mesh {
 namespace LagrangeSF {
 
+/// Storage for element nodes
+typedef std::vector<RealVector*> NodesT;
+
 /// This class provides the lagrangian shape function describing the
 /// representation of the solution and/or the geometry in a P1 (linear)
 /// triangular element.
@@ -24,18 +27,28 @@ public:
 /// @param shapeFunc Vector storing the result
 static void computeShapeFunction(const RealVector& mappedCoord, RealVector& shapeFunc) {
   cf_assert(shapeFunc.size() == 3);
+  cf_assert(mappedCoord.size() == 2);
   shapeFunc[0] = 1.0 - mappedCoord[0] - mappedCoord[1];
   shapeFunc[1] = mappedCoord[0];
   shapeFunc[2] = mappedCoord[1];
 }
 
+inline static Real computeJacobianDeterminant(const RealVector& mappedCoord, const NodesT& nodes) {
+  return   (nodes[1]->at(XX) - nodes[0]->at(XX)) * (nodes[2]->at(YY) - nodes[0]->at(YY))
+         - (nodes[2]->at(XX) - nodes[0]->at(XX)) * (nodes[1]->at(YY) - nodes[0]->at(YY));
+}
+
 /// Compute Mapped Coordinates
 /// @param coord contains the coordinates to be mapped
 /// @param nodes contains the nodes
-/// @return RealVector containing the Mapped Coordinates
-//static RealVector computeMappedCoordinates(const RealVector& coord, const std::vector<Framework::Node*>& nodes) {
-//
-//}
+/// @param mappedCoord Store the output mapped coordinates
+static void computeMappedCoordinates(const RealVector& coord, const NodesT& nodes, RealVector& mappedCoord) {
+  cf_assert(coord.size() == 2);
+  cf_assert(mappedCoord.size() == 2);
+  cf_assert(nodes.size() == 3);
+}
+
+
 
 private:
 /// Cannot be instantiated
---
+++ src/Mesh/uTests/uTestLagrangeSFTriagP1.cpp
@@ -0,0 +1,85 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "Test module for CF::Mesh::LagrangeSF"
+
+#include <boost/assign/list_of.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "Common/Log.hpp"
+#include "Common/CRoot.hpp"
+#include "Mesh/LagrangeSF/TriagP1.hpp"
+#include "Mesh/P1/Triag2D.hpp"
+#include "Mesh/uTests/difference.hpp"
+
+using namespace boost::assign; // bring 'operator+=()' into scope
+using namespace CF::Mesh::LagrangeSF;
+
+//////////////////////////////////////////////////////////////////////////////
+
+struct LagrangeSFTriagP1_Fixture
+{
+  /// common setup for each test case
+  LagrangeSFTriagP1_Fixture() : mapped_coords(init_mapped_coords()), nodes(init_nodes())
+  {
+     // uncomment if you want to use arguments to the test executable
+     //int*    argc = &boost::unit_test::framework::master_test_suite().argc;
+     //char*** argv = &boost::unit_test::framework::master_test_suite().argv;
+
+    //mapped_coords += 0.1, 0.8;
+  }
+
+  /// common tear-down for each test case
+  ~LagrangeSFTriagP1_Fixture()
+  {
+  }
+  /// common values accessed by all tests goes here
+
+  const CF::RealVector mapped_coords;
+  const NodesT nodes;
+
+private:
+  /// Workaround for boost:assign ambiguity
+  CF::RealVector init_mapped_coords()
+  {
+    return list_of(0.1)(0.8);
+  }
+
+  /// Workaround for boost:assign ambiguity
+  NodesT init_nodes()
+  {
+    static const CF::RealVector c1 = list_of(1.1)(1.2);
+    static const CF::RealVector c2 = list_of(2.3)(1.4);
+    static const CF::RealVector c3 = list_of(1.5)(2.1);
+    return list_of(&c1)(&c2)(&c3);
+  }
+
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_SUITE( LagrangeSFTriagP1, LagrangeSFTriagP1_Fixture )
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE( computeShapeFunction )
+{
+  const CF::RealVector reference_result = list_of(0.1)(0.1)(0.8);
+  CF::RealVector result(3);
+  TriagP1::computeShapeFunction(mapped_coords, result);
+  CF::difference::accumulator accumulator;
+  CF::difference::vector_test(result, reference_result, accumulator);
+  BOOST_CHECK_LT(boost::accumulators::max(accumulator.ulps), 10);
+}
+
+BOOST_AUTO_TEST_CASE( computeJacobianDeterminant )
+{
+  CF::Mesh::P1::Triag2D triag_element; // todo: why is computeVolume not static?
+  // Shapefunction determinant should be double the volume for triangles
+  BOOST_CHECK_LT(boost::accumulators::max(CF::difference::test(0.5*TriagP1::computeJacobianDeterminant(mapped_coords, nodes), triag_element.computeVolume(nodes)).ulps), 1);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_SUITE_END()
+
+////////////////////////////////////////////////////////////////////////////////
+
