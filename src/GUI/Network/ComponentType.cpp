275a1ba50291bdbfe370cc9319d08a6b3586dd64
gquentin <gquentin@gmail.com>
2010-04-29 10:49:19 +0000

    QG: forgog to commit files
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@224 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ src/GUI/Network/ComponentType.cpp
@@ -0,0 +1,40 @@
+#include <boost/assign/list_of.hpp> // for map_list_of
+
+#include "GUI/Network/ComponentType.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+
+using namespace CF::GUI::Network;
+
+////////////////////////////////////////////////////////////////////////////////
+
+ComponentType::Convert::FwdMap_t ComponentType::Convert::all_fwd = boost::assign::map_list_of
+( ComponentType::INVALID, "INVALID" )
+( ComponentType::ROOT,    "Root")
+( ComponentType::GROUP,   "Group")
+( ComponentType::LINK,    "Link");
+
+ComponentType::Convert::BwdMap_t ComponentType::Convert::all_rev = boost::assign::map_list_of
+( "INVALID", ComponentType::INVALID )
+( "Root",    ComponentType::ROOT)
+( "Group",   ComponentType::GROUP)
+( "Link",    ComponentType::LINK);
+
+////////////////////////////////////////////////////////////////////////////////
+
+std::ostream& operator<< ( std::ostream& os, const ComponentType::Type& in )
+{
+  os << ComponentType::Convert::to_str(in);
+  return os;
+}
+
+std::istream& operator>> (std::istream& is, ComponentType::Type& in )
+{
+  std::string tmp;
+  is >> tmp;
+  in = ComponentType::Convert::to_enum(tmp);
+  return is;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
---
+++ src/GUI/Network/ComponentType.hpp
@@ -0,0 +1,52 @@
+#ifndef CF_GUI_Network_ComponentType_hh
+#define CF_GUI_Network_ComponentType_hh
+
+////////////////////////////////////////////////////////////////////////////////
+
+#include "Common/EnumT.hpp"
+#include "GUI/Network/NetworkAPI.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace CF {
+namespace GUI {
+namespace Network {
+
+////////////////////////////////////////////////////////////////////////////////
+    
+  class Network_API ComponentType
+  {
+  public:
+    
+    /// Enumeration of the worker statuses recognized in CF
+    enum Type  { INVALID     =-1,
+      ROOT  = 0,
+      GROUP = 1,
+      LINK  = 2};
+    
+    typedef Common::EnumT< ComponentType > ConverterBase;
+    
+    struct Network_API Convert : public ConverterBase
+    {
+      /// storage of the enum forward map
+      static ConverterBase::FwdMap_t all_fwd;
+      /// storage of the enum reverse map
+      static ConverterBase::BwdMap_t all_rev;
+    };
+    
+  }; // class StatusWorker
+  
+////////////////////////////////////////////////////////////////////////////////
+  
+  Network_API std::ostream& operator<< ( std::ostream& os, const ComponentType::Type& in );
+  Network_API std::istream& operator>> ( std::istream& is, ComponentType::Type& in );
+  
+////////////////////////////////////////////////////////////////////////////////
+  
+} // namespace Network
+} // namespace GUI
+} // namespace CF
+
+////////////////////////////////////////////////////////////////////////////////
+
+#endif // CF_GUI_Network_ComponentType_hh
