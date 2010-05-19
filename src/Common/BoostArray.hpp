6a703fe83f7f424732cc9e2f57c70a4474693d3f
wdeconinck <ir.willem.deconinck@gmail.com>
2010-05-19 21:17:55 +0000

    [WARNINGFIX] include Common/BoostArray.hpp instead of boost/multi_array.hpp
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@310 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ src/Common/BoostArray.hpp
@@ -0,0 +1,8 @@
+#ifndef CF_Common_BOOSTARRAY_hpp
+#define CF_Common_BOOSTARRAY_hpp
+
+#define BOOST_MULTI_ARRAY_NO_GENERATORS false
+#include <boost/multi_array.hpp> 
+#undef BOOST_MULTI_ARRAY_NO_GENERATORS
+
+#endif // CF_Common_BOOSTARRAY_hpp
