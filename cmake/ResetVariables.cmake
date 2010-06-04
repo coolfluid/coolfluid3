abc239d5557cf14276780676821ff83232379935
gquentin <gquentin@gmail.com>
2010-06-04 08:55:03 +0000

    [DEV] added DEPS_ROOT variable to cmake. This allows to define the path for all deps only once (instead of giving it for each dependency)
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@391 4892e656-8fb9-448a-b5bc-b1919152596f

--- cmake/ResetVariables.cmake
+++ cmake/ResetVariables.cmake
@@ -4,5 +4,28 @@ SET ( CF_PROJECT_FILES "" CACHE INTERNAL "" FORCE )
 # reset the list of orphan files
 SET ( CF_ORPHAN_FILES  "" CACHE INTERNAL "" FORCE )
 
+# user define that affects many search paths simultaneously
+if ( DEFINED DEPS_ROOT )
 
+  if ( NOT DEFINED BOOST_ROOT )
+    SET ( BOOST_ROOT  ${DEPS_ROOT} CACHE INTERNAL "" )
+  endif()
+
+  if ( NOT DEFINED MPI_ROOT )
+    SET ( MPI_ROOT  ${DEPS_ROOT} CACHE INTERNAL "" )
+  endif()
+
+  if ( NOT DEFINED PARMETIS_ROOT )
+    SET ( PARMETIS_ROOT  ${DEPS_ROOT} CACHE INTERNAL "" )
+  endif()
+
+  if ( NOT DEFINED CGNS_HOME )
+    SET ( CGNS_HOME  ${DEPS_ROOT} CACHE INTERNAL "" )
+  endif()
+
+  if ( NOT DEFINED GOOGLE_PERFTOOLS_ROOT )
+    SET ( GOOGLE_PERFTOOLS_ROOT  ${DEPS_ROOT} CACHE INTERNAL "" )
+  endif()
+
+endif()
 
