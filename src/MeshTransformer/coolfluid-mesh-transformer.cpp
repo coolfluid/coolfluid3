40a3dd5565470bae7a27bfd7861a7c5db2f4b541
wdeconinck <ir.willem.deconinck@gmail.com>
2010-06-18 17:04:59 +0000

    [FEATURE] mesh-transformer using boost::program_options
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@448 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/MeshTransformer/coolfluid-mesh-transformer.cpp
+++ src/MeshTransformer/coolfluid-mesh-transformer.cpp
@@ -1,6 +1,6 @@
 #include <boost/regex.hpp>
+#include <boost/program_options.hpp>
 
-#include "Common/GetPot.hpp"
 #include "Common/Factory.hpp"
 #include "Common/Log.hpp"
 #include "Common/CoreEnv.hpp"
@@ -11,6 +11,7 @@
 #include "Mesh/CMeshWriter.hpp"
 
 using namespace boost;
+using namespace boost::program_options;
 using namespace CF;
 using namespace CF::Common;
 using namespace CF::Mesh;
@@ -49,24 +50,26 @@ int main(int argc, char * argv[])
       extensions_to_writers[extension].push_back(writer);
   }
 
-  // options, formats and transformations
-  GetPot o(argc,argv);
+  options_description desc("General Options");
+   desc.add_options()
+       ("help,h", "this help")
+       ("input,i" , value<std::vector<std::string> >()->multitoken(), "input file(s)")
+       ("output,o", value<std::vector<std::string> >()->multitoken(), "output file(s)")
+       ("dryrun,d", "dry run")
+       ("version,v", "show version")
+       ;
+   variables_map vm;
+   parsed_options parsed = parse_command_line(argc, argv, desc);
+   store(parsed, vm);
+   notify(vm);
 
-  // help (options and descriptions)
-   if (o.search(2,"-h","--help") || o.size()<=1) {
 
-     // set keys and descriptions
+
+   if (vm.count("help") || vm.size()==0)
+   {
+     CFinfo << CFendl << "Usage: " << argv[0] << " [options]" << CFendl << CFendl;
+     CFinfo << desc << CFendl;
      std::vector< std::string > vk, vt;
-     vk.push_back("");                      vt.push_back("");
-     vk.push_back("General options:");      vt.push_back("");
-     vk.push_back("  -h [ --help ]  ");      vt.push_back("this help");
-     vk.push_back("  -i [ --input ] arg  ");  vt.push_back("input file(s)");
-     vk.push_back("  -o [ --output ] arg  ");  vt.push_back("output file(s)");
-     vk.push_back("  -f [ --format ] arg  ");  vt.push_back("format of the file specified in previous arg");
-     vk.push_back("  -t[...]  ");             vt.push_back("transformations");
-     vk.push_back("  -v [ --version ]  ");        vt.push_back("display version");
-     vk.push_back("  -d [ --dryrun ]  ");        vt.push_back("dry run");
-     vk.push_back("");                      vt.push_back("");
      vk.push_back("Input formats:");        vt.push_back("");
      BOOST_FOREACH(const CMeshReader::Ptr& reader, readers)
      {
@@ -87,7 +90,7 @@ int main(int argc, char * argv[])
        vt.push_back(extensions);
      }     vk.push_back("");                      vt.push_back("");
      vk.push_back("Transformations:");      vt.push_back("");
-     //ft->getkeys(vk);                     ft->gettxts(vt);
+     vk.push_back("  no transformations implemented");    vt.push_back("");
 
      // output with keys strings adjusted to the same length
      unsigned l = 0;
@@ -97,181 +100,116 @@ int main(int argc, char * argv[])
      for (unsigned i=0; i<vk.size(); ++i)
        vk[i].insert(vk[i].end(),l-vk[i].length(),' ');
 
-     CFinfo << CFendl << "Usage: " << o[0] << " [options]" << CFendl;
      for (unsigned i=0; i<vk.size(); ++i)
        CFinfo << vk[i] << vt[i] << CFendl;
      exit(0);
-
-   }
-   else if (o.search(2,"-v","--version")) {
-
-     CFinfo << CFendl
-          << "Version " << 1.0 << CFendl;
-
-     CFinfo << "mfinput keys:";
-     std::vector< std::string > vk;
-     BOOST_FOREACH(const extensions_to_readers_pair_t& p, extensions_to_readers)
-       vk.push_back(p.first);
-     //fi->getkeys(vk);
-     for (unsigned i=0; i<vk.size(); ++i)
-       if (vk[i].length())
-         CFinfo << ' ' << vk[i];
-     CFinfo << CFendl;
-
-     CFinfo << "mfoutput keys:";
-     vk.clear();
-     BOOST_FOREACH(const extensions_to_writers_pair_t& p, extensions_to_writers)
-       vk.push_back(p.first);
-     for (unsigned i=0; i<vk.size(); ++i)
-       if (vk[i].length())
-         CFinfo << ' ' << vk[i];
-     CFinfo << CFendl;
-
-     CFinfo << "mtransform keys:";
-     
-     vk.clear();
-     //ft->getkeys(vk);
-     for (unsigned i=0; i<vk.size(); ++i)
-       if (vk[i].length())
-         CFinfo << ' ' << vk[i];
-     CFinfo << CFendl;
-
-     if (o.search("--version"))
-       exit(0);
-
    }
 
-   bool dryrun = false;
-   if (o.search(2,"-d","--dryrun"))
+   bool dryrun=false;
+   if (vm.count("dryrun"))
    {
-     dryrun = true;
      CFinfo << "\nThis is what would happen without the dryrun option:" << CFendl << CFendl;
+     dryrun=true;
+   }
+
+   if (vm.count("version"))
+   {
+     CFinfo << "Version: " << 1 << CFendl;
    }
 
    // create mesh object
   CRoot::Ptr root = CRoot::create("root");
   CMesh::Ptr mesh = root->create_component_type<CMesh>("mesh");
 
-   // process command line arguments in order
-   for (unsigned i=1; i<o.size(); ++i) {
-     const std::string arg(o[i]);
-     const std::string val(o[i+1]);
-     o.set_cursor(i);
-     if (arg=="-i") {
 
-       // multiple input merges another mesh into current
-       filesystem::path inputfile (val);
-       const std::string ext = inputfile.extension();
-       CMeshReader::Ptr reader;
-       if (extensions_to_readers.find(ext)==extensions_to_readers.end())
+   typedef basic_option<char> Option;
+   typedef std::basic_string<char> OptionValue;
+
+   BOOST_FOREACH(Option option, parsed.options)
+   {
+
+     // parse
+     if (option.string_key=="input")
+     {
+       BOOST_FOREACH(OptionValue value, option.value)
        {
-         Uint selection = 0;
-         CFinfo << inputfile << " has ambiguous extension " << ext << CFendl;
-         BOOST_FOREACH(const CMeshReader::Ptr selectreader , readers)
-           CFinfo << "  [" << selection++ +1 << "]  " << selectreader->get_format() << CFendl;
-         CFinfo << "Select the correct reader: " << CFflush;
-         std::cin >> selection;
-         reader = readers[--selection];
-       }
-       else
-       {
-         Uint selection = 0;
-         if (extensions_to_readers[ext].size()>1)
+         filesystem::path inputfile (value);
+         const std::string ext = inputfile.extension();
+         CMeshReader::Ptr reader;
+         if (!extensions_to_readers.count(ext))
          {
-           CFinfo << inputfile << " with extension " << ext << " has multiple readers: " << CFendl;
-           BOOST_FOREACH(const CMeshReader::Ptr selectreader , extensions_to_readers[ext])
+           Uint selection = 0;
+           CFinfo << inputfile << " has ambiguous extension " << ext << CFendl;
+           BOOST_FOREACH(const CMeshReader::Ptr selectreader , readers)
              CFinfo << "  [" << selection++ +1 << "]  " << selectreader->get_format() << CFendl;
            CFinfo << "Select the correct reader: " << CFflush;
            std::cin >> selection;
-           --selection;
+           reader = readers[--selection];
          }
-         reader = extensions_to_readers[ext][selection];
+         else
+         {
+           Uint selection = 0;
+           if (extensions_to_readers[ext].size()>1)
+           {
+             CFinfo << inputfile << " with extension " << ext << " has multiple readers: " << CFendl;
+             BOOST_FOREACH(const CMeshReader::Ptr selectreader , extensions_to_readers[ext])
+               CFinfo << "  [" << selection++ +1 << "]  " << selectreader->get_format() << CFendl;
+             CFinfo << "Select the correct reader: " << CFflush;
+             std::cin >> selection;
+             --selection;
+           }
+           reader = extensions_to_readers[ext][selection];
+         }
+
+         CFinfo << "Reading " << inputfile << " with " << reader->get_format() << CFendl;
+
+         if (!dryrun) reader->read_from_to(inputfile,mesh);
        }
-
-       CFinfo << "Reading file with " << reader->get_format() << CFendl;
-
-       if (!dryrun) reader->read_from_to(inputfile,mesh);
-//       if (fi->search(key.c_str())) {
-//         CFinfo << "::read \"" << val << "\"..." << CFendl;
-//         mfinput* p = fi->Create(key);
-//         if (m.v()) {
-//           mmesh m2;
-//           p->read(o,m2);
-//           CFinfo << "::merge [d/n/e]: " << m.d() << "+" << m2.d()
-//                              << " / " << m.n() << "+" << m2.n()
-//                              << " / " << m.e() << "+" << m2.e() << "..." << CFendl;
-//           m.merge(m2);
-//           CFinfo << "::merge [d/n/e]: " << m.d()
-//                              << " / " << m.n()
-//                              << " / " << m.e() << "." << CFendl;
-//         }
-//         else {
-//           p->read(o,m);
-//         }
-//         delete p;
-//         CFinfo << "::read \"" << val << "\"." << CFendl;
-//       }
+     }
+     else if (option.string_key=="t")
+     {
 
      }
-     else if (arg.find("-t")==0) {
+  else if (option.string_key=="output")
+  {
+    BOOST_FOREACH(OptionValue value, option.value)
+    {
+     filesystem::path outputfile (value);
+     const std::string ext = outputfile.extension();
 
-//       const string key(arg);
-//       if (ft->search(key.c_str())) {
-//         CFinfo << "::transform \"" << key << "\"..." << CFendl;
-//         mtransform* p = ft->Create(key);
-//         p->transform(o,m);
-//         delete p;
-//         CFinfo << "::transform \"" << key << "\"." << CFendl;
-//       }
-
+     CMeshWriter::Ptr writer;
+     if (!extensions_to_writers.count(ext))
+     {
+       Uint selection = 0;
+       CFinfo << outputfile << " has ambiguous extension " << ext << CFendl;
+       BOOST_FOREACH(const CMeshWriter::Ptr selectwriter , writers)
+         CFinfo << "  [" << selection++ +1 << "]  " << selectwriter->get_format() << CFendl;
+       CFinfo << "Select the correct writer: " << CFflush;
+       std::cin >> selection;
+       writer = writers[--selection];
      }
-     else if (arg=="-o") {
-
-       // write current mesh
-       filesystem::path outputfile (val);
-       const std::string ext = outputfile.extension();
-
-       CMeshWriter::Ptr writer;
-       if (extensions_to_writers.find(ext)==extensions_to_writers.end())
+     else
+     {
+       Uint selection = 0;
+       if (extensions_to_writers[ext].size()>1)
        {
-         Uint selection = 0;
-         CFinfo << outputfile << " has ambiguous extension " << ext << CFendl;
-         BOOST_FOREACH(const CMeshWriter::Ptr selectwriter , writers)
+         CFinfo << outputfile << " with extension " << ext << " has multiple writers: " << CFendl;
+         BOOST_FOREACH(const CMeshWriter::Ptr selectwriter , extensions_to_writers[ext])
            CFinfo << "  [" << selection++ +1 << "]  " << selectwriter->get_format() << CFendl;
          CFinfo << "Select the correct writer: " << CFflush;
          std::cin >> selection;
-         writer = writers[--selection];
+         --selection;
        }
-       else
-       {
-         Uint selection = 0;
-         if (extensions_to_writers[ext].size()>1)
-         {
-           CFinfo << outputfile << " with extension " << ext << " has multiple writers: " << CFendl;
-           BOOST_FOREACH(const CMeshWriter::Ptr selectwriter , extensions_to_writers[ext])
-             CFinfo << "  [" << selection++ +1 << "]  " << selectwriter->get_format() << CFendl;
-           CFinfo << "Select the correct writer: " << CFflush;
-           std::cin >> selection;
-           --selection;
-         }
-         writer = extensions_to_writers[ext][selection];
-       }
-
-       CFinfo << "Writing file with " << writer->get_format() << CFendl;
-
-       if (!dryrun) writer->write_from_to(mesh,outputfile);
-
-       //       if (fo->search(key.c_str())) {
-//         CFinfo << "::write \"" << val << "\"..." << CFendl;
-//         mfoutput* p = fo->Create(key);
-//         p->write(o,m);
-//         delete p;
-//         CFinfo << "::write \"" << val << "\"." << CFendl;
-//       }
-
+       writer = extensions_to_writers[ext][selection];
      }
-   }
 
+     CFinfo << "Writing " << outputfile << " with " << writer->get_format() << CFendl;
+
+     if (!dryrun) writer->write_from_to(mesh,outputfile);
+    }
+  }
+
+  }
 
   //CoreEnv::getInstance().terminate();
 
