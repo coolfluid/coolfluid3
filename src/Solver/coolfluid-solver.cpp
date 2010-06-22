4a82efdb2a3def7dcdfeeb19a7d072f0a8f2600c
wdeconinck <ir.willem.deconinck@gmail.com>
2010-06-22 09:18:56 +0000

    [DEV] coolfluid-solver loads a small mesh
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@468 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Solver/coolfluid-solver.cpp
+++ src/Solver/coolfluid-solver.cpp
@@ -1,7 +1,13 @@
 #include "Common/Log.hpp"
 #include "Common/CoreEnv.hpp"
+#include "Common/Component.hpp"
+#include "Common/CRoot.hpp"
+
+#include "Mesh/CMeshReader.hpp"
+#include "Mesh/CMesh.hpp"
 
 using namespace CF::Common;
+using namespace CF::Mesh;
 
 int main(int argc, char * argv[])
 {
@@ -9,6 +15,27 @@ int main(int argc, char * argv[])
 
   CoreEnv::instance().initiate(argc, argv);
 
+  
+  
+  
+  CMeshReader::Ptr meshreader = create_component_abstract_type<CMeshReader>("Neu","meshreader");
+    
+  // the file to read from
+  boost::filesystem::path inputfile ("quadtriag.neu");
+  
+  // the mesh to store in
+  CRoot::Ptr root = CRoot::create("root");
+  CMesh::Ptr mesh = meshreader->create_mesh_from(inputfile);
+  root->add_component(mesh);
+  
+  mesh->print_tree();
+  
+  
+  
+  
+  
+  
+  
   CoreEnv::instance().terminate();
 
   return 0;
