8c3c9f85ac40eb75d6cba1d3c7cd9fb5c6ecac2d
wdeconinck <ir.willem.deconinck@gmail.com>
2010-06-22 11:33:34 +0000

    [DEV] looping over the mesh in coolfluid-solver
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@471 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Solver/coolfluid-solver.cpp
+++ src/Solver/coolfluid-solver.cpp
@@ -1,13 +1,60 @@
 #include "Common/Log.hpp"
 #include "Common/CoreEnv.hpp"
-#include "Common/Component.hpp"
+#include "Common/ComponentPredicates.hpp"
 #include "Common/CRoot.hpp"
 
 #include "Mesh/CMeshReader.hpp"
 #include "Mesh/CMesh.hpp"
+#include "Mesh/CArray.hpp"
+#include "Mesh/CTable.hpp"
+#include "Mesh/CElements.hpp"
+#include "Mesh/CRegion.hpp"
 
+using namespace CF;
 using namespace CF::Common;
 using namespace CF::Mesh;
+
+namespace CF
+{
+  namespace Mesh
+  {
+    class IsLeaf
+    {
+    public:
+      IsLeaf () {}
+      
+      bool operator()(const Component& component)
+      {
+        return !range_typed<CTable>(component).empty() && !range_typed<CElements>(component).empty();
+      }
+      
+    }; // IsLeaf
+    
+    class IsLeafWithDimensionality
+    {
+    public:
+      IsLeafWithDimensionality (Uint dim) : m_dim(dim) {}
+      
+      bool operator()(const Component& component)
+      {
+        if (m_isLeaf(component))
+        {
+          return (m_dim == dynamic_cast<const CRegion*>(&component)->get_elementType()->getDimensionality());
+        }
+        else
+        {
+          return false;
+        }
+      }
+      
+    private:
+      IsLeaf m_isLeaf;
+      Uint m_dim;
+      
+    }; // IsLeaf
+  }
+}
+
 
 int main(int argc, char * argv[])
 {
@@ -28,12 +75,60 @@ int main(int argc, char * argv[])
   CMesh::Ptr mesh = meshreader->create_mesh_from(inputfile);
   root->add_component(mesh);
   
-  mesh->print_tree();
+  //mesh->print_tree();
+  
+  
+  /// @todo automated way
+  Uint volume_dim = 2;
+  Uint surface_dim = volume_dim-1;
+  
+  CArray& coordinates = get_named_component_typed<CArray>(*mesh, "coordinates");
+  
+  // loop for all volume regions
+  // loop on element types
+  BOOST_FOREACH(CRegion& region, recursive_filtered_range_typed<CRegion>(*mesh,IsLeafWithDimensionality(volume_dim)))
+  {
+    CArray::Ptr centres = region.create_component_type<CArray>("centres");
+    centres->initialize(volume_dim);
+    CArray::Buffer buffer = centres->create_buffer();
+    buffer.increase_array_size(region.elements_count());
+    
+    // loop on elements
+    BOOST_FOREACH(const CTable::ConstRow& elem, region.get_connectivityTable()->get_table())
+    {
+      
+      // compute centre
+      RealVector centre(0.0,volume_dim);
+      BOOST_FOREACH(const Uint node, elem)
+      centre += RealVector( coordinates.get_array()[node] );
+      buffer.add_row_directly(centre);
+    }
+  }
   
   
   
+  // loop for all surface regions
+  // loop on element types
+  BOOST_FOREACH(CRegion& region, recursive_filtered_range_typed<CRegion>(*mesh,IsLeafWithDimensionality(surface_dim)))
+  {
+    CArray::Ptr centres = region.create_component_type<CArray>("centres");
+    centres->initialize(volume_dim);
+    CArray::Buffer buffer = centres->create_buffer();
+    buffer.increase_array_size(region.elements_count());
+
+    // loop on elements
+    BOOST_FOREACH(const CTable::ConstRow& elem, region.get_connectivityTable()->get_table())
+    {
+      
+      // compute centre
+      RealVector centre(0.0,volume_dim);
+      BOOST_FOREACH(const Uint node, elem)
+        centre += RealVector( coordinates.get_array()[node] );
+      buffer.add_row_directly(centre);
+    }
+  }
   
-  
+  root->print_tree();
   
   
   CoreEnv::instance().terminate();
