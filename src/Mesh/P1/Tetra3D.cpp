fa9fa53c0abaafae6db165d4574b0e492134e366
wdeconinck <ir.willem.deconinck@gmail.com>
2010-06-26 15:15:36 +0000

    [DEV] P1::Tetra3D faces set
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@514 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Mesh/P1/Tetra3D.cpp
+++ src/Mesh/P1/Tetra3D.cpp
@@ -28,8 +28,16 @@ Tetra3D::Tetra3D()
   m_dimension=3;
   m_dimensionality=3;
   m_nbEdges=6;
-  
-  // TODO:set faces
+    
+  // set faces
+  m_faces.reserve(4);
+  boost::shared_ptr<ElementType> triag(new Triag3D);
+  std::vector<Uint> nodes(3);
+
+  nodes[0]=0;   nodes[1]=2;   nodes[2]=1;    m_faces.push_back( Face(triag,nodes));
+  nodes[0]=0;   nodes[1]=1;   nodes[2]=3;    m_faces.push_back( Face(triag,nodes));
+  nodes[0]=1;   nodes[1]=2;   nodes[2]=3;    m_faces.push_back( Face(triag,nodes));
+  nodes[0]=0;   nodes[1]=3;   nodes[2]=2;    m_faces.push_back( Face(triag,nodes));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
