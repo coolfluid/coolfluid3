286eee53133b99606ec0d31d663d5e71bbc8d09d
barche <bart.janssens@lid.kviv.be>
2010-07-15 05:54:32 +0000

    [ENH] Add Quad3D shape function
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@589 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ src/Mesh/Elements/SF/Quad3DLagrangeP1.hpp
@@ -0,0 +1,177 @@
+#ifndef CF_Mesh_SF_Quad3DLagrangeP1_hpp
+#define CF_Mesh_SF_Quad3DLagrangeP1_hpp
+
+#include "Common/CF.hpp"
+#include "Math/RealMatrix.hpp"
+#include "Mesh/GeoShape.hpp"
+
+#include "Mesh/Elements/Quad3D.hpp"
+
+#include "Quad2DLagrangeP1.hpp"
+
+namespace CF {
+namespace Mesh {
+namespace SF {
+
+/// This class provides the lagrangian shape function describing the
+/// representation of the solution and/or the geometry in a P1 (bilinear)
+/// quadrilateral element that lives in 3D space
+/// @author Andrea Lani
+/// @author Geoffrey Deliege
+/// @author Tiago Quintino
+/// @author Bart Janssens
+struct Quad3DLagrangeP1  : public Quad3D
+{
+  Quad3DLagrangeP1();
+
+  /// Compute the shape functions corresponding to the given
+  /// mapped coordinates
+  /// @param mappedCoord The mapped coordinates
+  /// @param shapeFunc Vector storing the result
+  static void shape_function(const RealVector& mapped_coord, RealVector& shape_func)
+  {
+    Quad2DLagrangeP1::shape_function(mapped_coord, shape_func);
+  }
+
+  /// Compute the gradient with respect to mapped coordinates, i.e. parial derivatives are in terms of the
+  /// mapped coordinates.
+  /// @param mappedCoord The mapped coordinates where the gradient should be calculated
+  /// @param result Storage for the resulting gradient matrix
+  static void mapped_gradient(const RealVector& mapped_coord, RealMatrix& result)
+  {
+    Quad2DLagrangeP1::mapped_gradient(mapped_coord, result);
+  }
+
+  /// Compute the Jacobian matrix
+  /// In the case of the Quad3D element, this is the vector corresponding to the line segment
+  /// @param mappedCoord The mapped coordinates where the Jacobian should be calculated
+  /// @param result Storage for the resulting Jacobian matrix
+  template<typename NodesT>
+  static void jacobian(const RealVector& mappedCoord, const NodesT& nodes, RealMatrix& result)
+  {
+    cf_assert(result.nbRows() == dimensionality);
+    cf_assert(result.nbCols() == dimension);
+
+    JacobianCoefficients jc(nodes);
+
+    const Real xi = mappedCoord[KSI];
+    const Real eta = mappedCoord[ETA];
+
+    result(KSI,XX) = jc.bx + jc.dx*eta;
+    result(KSI,YY) = jc.by + jc.dy*eta;
+    result(KSI,ZZ) = jc.bz + jc.dz*eta;
+
+    result(ETA,XX) = jc.cx + jc.dx*xi;
+    result(ETA,YY) = jc.cy + jc.dy*xi;
+    result(ETA,ZZ) = jc.cz + jc.dz*xi;
+  }
+
+  /// Normal vector to the surface.
+  /// @param mappedCoord The mapped coordinates where the Jacobian should be calculated
+  /// @param result Storage for the resulting Jacobian matrix
+  template<typename NodesT>
+  static void normal(const RealVector& mappedCoord, const NodesT& nodes, RealVector& result)
+  {
+    cf_assert(result.size() == dimension);
+    RealMatrix jac(dimensionality, dimension);
+    jacobian(mappedCoord, nodes, jac);
+
+    result[XX] = jac(KSI,YY)*jac(ETA,ZZ) - jac(KSI,ZZ)*jac(ETA,YY);
+    result[YY] = jac(KSI,ZZ)*jac(ETA,XX) - jac(KSI,XX)*jac(ETA,ZZ);
+    result[ZZ] = jac(KSI,XX)*jac(ETA,YY) - jac(KSI,YY)*jac(ETA,XX);
+  }
+
+  /// Volume of the cell. 0 in case of elements with a dimensionality that is less than
+  /// the dimension of the problem
+  template<typename NodesT>
+  static Real volume(const NodesT& nodes)
+  {
+    return 0.;
+  }
+
+  /// The area of an element that represents a surface in the solution space, i.e.
+  /// 1D elements in 2D space or 2D elements in 3D space
+  template<typename NodesT>
+  static Real area(const NodesT& nodes)
+  {
+    RealVector n(dimension);
+    RealVector center(0., 2);
+    normal(center, nodes, n);
+    return 4.*n.norm2();
+  }
+
+  /// Number of nodes
+  static const Uint nb_nodes = 4;
+
+  /// Order of the shape function
+  static const Uint order = 1;
+
+  /// Given nodal values, write the interpolation
+  template<typename NodalValuesT, typename ValueT>
+  void operator()(const RealVector& mapped_coord, const NodalValuesT& nodal_values, ValueT& interpolation) const
+  {
+    cf_assert(mapped_coord.size() == dimensionality);
+    cf_assert(nodal_values.size() == nb_nodes);
+
+    RealVector sf(nb_nodes);
+    shape_function(mapped_coord, sf);
+
+    interpolation = sf[0]*nodal_values[0] + sf[1]*nodal_values[1] + sf[2]*nodal_values[2] + sf[3]*nodal_values[3];
+  }
+
+  virtual std::string getElementTypeName() const;
+
+  /// The volume of an element with a dimensionality that is less than
+  /// the dimension of the problem is 0.
+  virtual Real computeVolume(const NodesT& coord) const;
+
+private:
+  /// Convenience struct to easily access the elements that make up the jacobian
+  struct JacobianCoefficients
+  {
+    Real ax, bx, cx, dx;
+    Real ay, by, cy, dy;
+    Real az, bz, cz, dz;
+    template<typename NodesT>
+    JacobianCoefficients(const NodesT& nodes)
+    {
+      const Real x0 = nodes[0][XX];
+      const Real y0 = nodes[0][YY];
+      const Real z0 = nodes[0][ZZ];
+
+      const Real x1 = nodes[1][XX];
+      const Real y1 = nodes[1][YY];
+      const Real z1 = nodes[1][ZZ];
+
+      const Real x2 = nodes[2][XX];
+      const Real y2 = nodes[2][YY];
+      const Real z2 = nodes[2][ZZ];
+
+      const Real x3 = nodes[3][XX];
+      const Real y3 = nodes[3][YY];
+      const Real z3 = nodes[3][ZZ];
+
+      ax = 0.25*( x0 + x1 + x2 + x3);
+      bx = 0.25*(-x0 + x1 + x2 - x3);
+      cx = 0.25*(-x0 - x1 + x2 + x3);
+      dx = 0.25*( x0 - x1 + x2 - x3);
+
+      ay = 0.25*( y0 + y1 + y2 + y3);
+      by = 0.25*(-y0 + y1 + y2 - y3);
+      cy = 0.25*(-y0 - y1 + y2 + y3);
+      dy = 0.25*( y0 - y1 + y2 - y3);
+
+      az = 0.25*( z0 + z1 + z2 + z3);
+      bz = 0.25*(-z0 + z1 + z2 - z3);
+      cz = 0.25*(-z0 - z1 + z2 + z3);
+      dz = 0.25*( z0 - z1 + z2 - z3);
+    }
+  };
+
+};
+
+} // namespace SF
+} // namespace Mesh
+} // namespace CF
+
+#endif /* CF_Mesh_SF_Quad3DLagrangeP1 */
---
+++ src/Mesh/uTests/uTestQuad3DLagrangeP1.cpp
@@ -0,0 +1,414 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "Test module for the Quad3DLagrangeP1 shapefunction"
+
+#include <boost/assign/list_of.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "Common/Log.hpp"
+#include "Common/CRoot.hpp"
+
+#include "Math/MathConsts.hpp"
+
+#include "Mesh/CArray.hpp"
+#include "Mesh/CTable.hpp"
+#include "Mesh/ElementNodes.hpp"
+#include "Mesh/Integrators/Gauss.hpp"
+#include "Mesh/Elements/SF/Quad3DLagrangeP1.hpp"
+
+
+
+#include "Tools/Testing/Difference.hpp"
+
+using namespace boost::assign;
+using namespace CF;
+using namespace CF::Math;
+using namespace CF::Mesh;
+using namespace CF::Mesh::Integrators;
+using namespace CF::Mesh::SF;
+using namespace CF::Tools::Testing;
+
+//////////////////////////////////////////////////////////////////////////////
+
+struct Quad3DLagrangeP1Fixture
+{
+  typedef std::vector<RealVector> NodesT;
+  /// common setup for each test case
+  Quad3DLagrangeP1Fixture() : mapped_coords(init_mapped_coords()), nodes(init_nodes())
+  {
+  }
+
+  /// common tear-down for each test case
+  ~Quad3DLagrangeP1Fixture()
+  {
+  }
+
+  /// Fills the given coordinate and connectivity data to create a cylinder along the Z-axis, consisting of Quad3DLagrangeP1 elements
+  void create_cylinder(CArray& coordinates, CTable& connectivity, const Real radius, const Uint u_segments, const Uint v_segments, const Real height, const Real start_angle = 0., const Real end_angle = 2.*MathConsts::RealPi())
+  {
+    const Uint dim = Quad3DLagrangeP1::dimension;
+    const Uint nb_nodes = Quad3DLagrangeP1::nb_nodes;
+    const bool closed = std::abs(std::abs(end_angle - start_angle) - 2.0*MathConsts::RealPi()) < MathConsts::RealEps();
+
+    coordinates.initialize(dim);
+    CArray::Array& coord_array = coordinates.array();
+    coord_array.resize(boost::extents[(u_segments + (!closed)) * (v_segments+1)][dim]);
+
+    connectivity.initialize(nb_nodes);
+    CTable::ConnectivityTable& conn_array = connectivity.table();
+    conn_array.resize(boost::extents[u_segments * v_segments][nb_nodes]);
+    const Real v_step = height / v_segments;
+
+    if(!closed)
+    {
+      for(Uint v = 0; v <= v_segments; ++v)
+      {
+        const Real z_coord = v_step * static_cast<Real>(v);
+        for(Uint u = 0; u <= u_segments; ++u)
+        {
+          const Real theta = start_angle + (end_angle - start_angle) * (static_cast<Real>(u) / static_cast<Real>(u_segments));
+          CArray::Row coord_row = coord_array[v*(u_segments+1) + u];
+
+          coord_row[XX] = radius * cos(theta);
+          coord_row[YY] = radius * sin(theta);
+          coord_row[ZZ] = z_coord;
+        }
+      }
+
+      for(Uint v = 0; v != v_segments; ++v)
+      {
+        const Real z_coord = v_step * static_cast<Real>(v);
+        for(Uint u = 0; u != u_segments; ++u)
+        {
+          CTable::Row nodes = conn_array[v*u_segments + u];
+          nodes[0] = v*(u_segments+1) + u;
+          nodes[1] = nodes[0] + 1;
+          nodes[3] = (v+1)*(u_segments+1) + u;
+          nodes[2] = nodes[3] + 1;
+        }
+      }
+    }
+    else // closed loop
+    {
+      for(Uint v = 0; v <= v_segments; ++v)
+      {
+        const Real z_coord = v_step * static_cast<Real>(v);
+        for(Uint u = 0; u != u_segments; ++u)
+        {
+          const Real theta = start_angle + (end_angle - start_angle) * (static_cast<Real>(u) / static_cast<Real>(u_segments));
+          CArray::Row coord_row = coord_array[v*u_segments + u];
+
+          coord_row[XX] = radius * cos(theta);
+          coord_row[YY] = radius * sin(theta);
+          coord_row[ZZ] = z_coord;
+        }
+      }
+
+      for(Uint v = 0; v != v_segments; ++v)
+      {
+        const Real z_coord = v_step * static_cast<Real>(v);
+        for(Uint u = 0; u != u_segments; ++u)
+        {
+          CTable::Row nodes = conn_array[v*u_segments + u];
+          nodes[0] = v*u_segments + u;
+          nodes[1] = nodes[0] + 1;
+          nodes[3] = (v+1)*u_segments + u;
+          nodes[2] = nodes[3] + 1;
+        }
+        conn_array[v*u_segments + u_segments-1][1] = v*u_segments;
+        conn_array[v*u_segments + u_segments-1][2] = (v+1)*u_segments;
+      }
+    }
+  }
+
+  const RealVector mapped_coords;
+  const NodesT nodes;
+
+  struct ConstFunctor
+  {
+    ConstFunctor(const NodesT& node_list) : m_nodes(node_list) {}
+    template<typename GeoSF, typename SolSF>
+    Real valTimesDetJacobian(const RealVector& mappedCoords)
+    {
+      RealVector result(GeoSF::dimension);
+      GeoSF::normal(mappedCoords, m_nodes, result);
+      return result.norm2();
+    }
+  private:
+    const NodesT& m_nodes;
+  };
+
+  /// Returns the norm of the normal vector to the curve or surface element (equal to tangent in the case of Line2D)
+  struct NormalVectorNorm {
+
+    template<typename NodesT, typename GeoSF>
+    Real operator()(const RealVector& mapped_coords, const NodesT& nodes, const GeoSF& sf)
+    {
+      RealVector result(GeoSF::dimension);
+      GeoSF::normal(mapped_coords, nodes, result);
+      return result.norm2();
+    }
+
+  };
+
+  /// Returns the scalar product of a constant vector field and the local element normal
+  struct ConstVectorField {
+
+    ConstVectorField(const RealVector& vector) : m_vector(vector) {}
+
+    template<typename NodesT, typename GeoSF>
+    Real operator()(const RealVector& mapped_coords, const NodesT& nodes, const GeoSF& sf)
+    {
+      RealVector normal(GeoSF::dimension);
+      GeoSF::normal(mapped_coords, nodes, normal);
+      return MathFunctions::innerProd(normal, m_vector);
+    }
+
+  private:
+    const RealVector m_vector;
+
+  };
+
+  /// Returns the static pressure around a rotating cylinder in a horizontal,
+  /// uniform velocity field U, multiplied with the local normal vector
+  struct RotatingCylinderPressure {
+
+    RotatingCylinderPressure(const Real radius, const Real circulation, const Real U) :
+      m_radius(radius), m_circulation(circulation), m_u(U) {}
+
+    template<typename NodesT, typename GeoSF>
+    RealVector operator()(const RealVector& mapped_coords, const NodesT& nodes, const GeoSF& sf)
+    {
+      // The pressures to interpolate
+      RealVector nodal_p(GeoSF::nb_nodes);
+      for(Uint i = 0; i != GeoSF::nb_nodes; ++i)
+        nodal_p[i] = pressure(theta(nodes[i]));
+
+      // The local normal
+      RealVector normal(GeoSF::dimension);
+      GeoSF::normal(mapped_coords, nodes, normal);
+
+      // Interpolate the pressure
+      Real p;
+      sf(mapped_coords, nodal_p, p);
+
+      return normal * p;
+    }
+
+  private:
+    const Real m_radius;
+    const Real m_circulation;
+    const Real m_u;
+    static const Real m_rho = 1.225;
+
+    // Reconstruct the value of theta, based on the coordinates
+    Real theta(const RealVector& coords)
+    {
+      return atan2(coords[YY], coords[XX]);
+    }
+
+    // Pressure in function of theta
+    Real pressure(const Real theta)
+    {
+      Real tmp = (2. * m_u * sin(theta) + m_circulation / (2. * MathConsts::RealPi() * m_radius));
+      return 0.5 * m_rho * tmp * tmp;
+    }
+
+  };
+
+private:
+  /// Workaround for boost:assign ambiguity
+  RealVector init_mapped_coords()
+  {
+    return list_of(0.1)(0.8);
+  }
+
+  /// Workaround for boost:assign ambiguity
+  NodesT init_nodes()
+  {
+    const CF::RealVector c0 = list_of(0.5)(0.3)(-1.);
+    const CF::RealVector c1 = list_of(1.1)(1.2)(0.);
+    const CF::RealVector c2 = list_of(1.35)(1.9)(1.);
+    const CF::RealVector c3 = list_of(0.8)(2.1)(0.);
+    return list_of(c0)(c1)(c2)(c3);
+  }
+};
+
+/// Mimic a possible new integration interface
+template<typename ResultT, typename FunctorT, typename GeoSF>
+void integrate_region(ResultT& result, FunctorT functor, const CArray& coordinates, const CTable& connectivity, const GeoSF& geo_sf)
+{
+  const Uint nb_elems = connectivity.table().size();
+  for(Uint elem_idx = 0; elem_idx != nb_elems; ++ elem_idx)
+  {
+    const ConstElementNodeView nodes(coordinates, connectivity.table()[elem_idx]);
+    integrate_element(result, functor, nodes, geo_sf);
+  }
+}
+
+/// Mimic a possible new integration interface. Integration over a single element
+template<typename ResultT, typename FunctorT, typename NodesT, typename GeoSF>
+void integrate_element(ResultT& result, FunctorT functor, const NodesT& nodes, const GeoSF& sf)
+{
+  static const double mu = 0.;
+  static const double w = 4.;
+  static const RealVector mapped_coords = boost::assign::list_of(mu)(mu);
+  result += w * functor(mapped_coords, nodes, sf);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_SUITE( Quad3DLagrangeP1Suite, Quad3DLagrangeP1Fixture )
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE( ShapeFunction )
+{
+  const CF::RealVector reference_result = list_of(0.045)(0.055)(0.495)(0.405);
+  CF::RealVector result(4);
+  Quad3DLagrangeP1::shape_function(mapped_coords, result);
+  CF::Tools::Testing::Accumulator accumulator;
+  CF::Tools::Testing::vector_test(result, reference_result, accumulator);
+  BOOST_CHECK_LT(boost::accumulators::max(accumulator.ulps), 10); // Maximal difference can't be greater than 10 times the least representable unit
+}
+
+BOOST_AUTO_TEST_CASE( MappedGradient )
+{
+  CF::RealMatrix expected(4, 2);
+  const CF::Real ksi  = mapped_coords[0];
+  const CF::Real eta = mapped_coords[1];
+  expected(0, 0) = 0.25 * (-1 + eta);
+  expected(0, 1) = 0.25 * (-1 + ksi);
+  expected(1, 0) = 0.25 * ( 1 - eta);
+  expected(1, 1) = 0.25 * (-1 - ksi);
+  expected(2, 0) = 0.25 * ( 1 + eta);
+  expected(2, 1) = 0.25 * ( 1 + ksi);
+  expected(3, 0) = 0.25 * (-1 - eta);
+  expected(3, 1) = 0.25 * ( 1 - ksi);
+  CF::RealMatrix result(4, 2);
+  Quad3DLagrangeP1::mapped_gradient(mapped_coords, result);
+  CF::Tools::Testing::Accumulator accumulator;
+  CF::Tools::Testing::vector_test(result, expected, accumulator);
+  BOOST_CHECK_LT(boost::accumulators::max(accumulator.ulps), 2);
+}
+
+BOOST_AUTO_TEST_CASE( Jacobian )
+{
+  CF::RealMatrix expected(Quad3DLagrangeP1::dimensionality, Quad3DLagrangeP1::dimension);
+  expected(KSI,XX) = 0.2775;
+  expected(KSI,YY) = -0.045;
+  expected(KSI,ZZ) = 0.5;
+
+  expected(ETA,XX) = 0.13625;
+  expected(ETA,YY) = 0.5975;
+  expected(ETA,ZZ) = 0.5;
+
+  CF::RealMatrix result(Quad3DLagrangeP1::dimensionality, Quad3DLagrangeP1::dimension);
+  Quad3DLagrangeP1::jacobian(mapped_coords, nodes, result);
+  CF::Tools::Testing::Accumulator accumulator;
+  CF::Tools::Testing::vector_test(result, expected, accumulator);
+  BOOST_CHECK_LT(boost::accumulators::max(accumulator.ulps), 15);
+}
+
+BOOST_AUTO_TEST_CASE( IntegrateConst )
+{
+  ConstFunctor ftor(nodes);
+  const Real area = Quad3DLagrangeP1::area(nodes);
+
+  Real result1 = 0.0;
+  Real result2 = 0.0;
+  Real result4 = 0.0;
+  Real result8 = 0.0;
+  Real result16 = 0.0;
+  Real result32 = 0.0;
+
+  Gauss<Quad3DLagrangeP1>::integrateElement(ftor, result1);
+  Gauss<Quad3DLagrangeP1, Quad3DLagrangeP1, 2>::integrateElement(ftor, result2);
+  Gauss<Quad3DLagrangeP1, Quad3DLagrangeP1, 4>::integrateElement(ftor, result4);
+  Gauss<Quad3DLagrangeP1, Quad3DLagrangeP1, 8>::integrateElement(ftor, result8);
+  Gauss<Quad3DLagrangeP1, Quad3DLagrangeP1, 16>::integrateElement(ftor, result16);
+  Gauss<Quad3DLagrangeP1, Quad3DLagrangeP1, 32>::integrateElement(ftor, result32);
+
+  BOOST_CHECK_CLOSE(result1, area, 0.001);
+  // TODO: Computed area is approximate, higher order integration is actually more accurate than what is returned by area()
+//  BOOST_CHECK_CLOSE(result2, area, 0.001);
+//  BOOST_CHECK_CLOSE(result4, area, 0.001);
+//  BOOST_CHECK_CLOSE(result8, area, 0.001);
+//  BOOST_CHECK_CLOSE(result16, area, 0.001);
+//  BOOST_CHECK_CLOSE(result32, area, 0.001);
+}
+
+/// Surface integrals can be done in 2D by making use of the normal vector to a curve.
+/// The components of this vector are the elements of the jacobian. For scalar functions, we
+/// need to integrate the function multiplied with the norm of the normal vector, for vector
+/// fields we integrate the scalar product of the function and the normal vector
+BOOST_AUTO_TEST_CASE( SurfaceIntegral )
+{
+  // Create an approximation of a circle
+  const Real radius = 1.;
+  const Uint u_segments = 100;
+  const Uint v_segments = 24;
+  const Real height = 3.;
+
+  // complete circle
+  CArray coordinates("coordinates");
+  CTable connectivity("connectivity");
+  create_cylinder(coordinates, connectivity, radius, u_segments, v_segments, height);
+
+  Quad3DLagrangeP1 aSF; /* temporary solution, on Mac it tries to use copy-constructor */
+
+  // Check the area
+  Real area = 0.;
+  integrate_region(area, NormalVectorNorm(), coordinates, connectivity, aSF);
+  BOOST_CHECK_CLOSE(area, 2.*MathConsts::RealPi()*radius*height, 0.1);
+
+  // Flux from a constant vector field through a closed surface should be 0
+  Real zero_flux = 0.;
+  const RealVector field_vector = boost::assign::list_of(0.35)(1.25)(0.);
+  integrate_region(zero_flux, ConstVectorField(field_vector), coordinates, connectivity, aSF);
+  BOOST_CHECK_SMALL(zero_flux, 1e-14);
+}
+
+BOOST_AUTO_TEST_CASE( ArcIntegral )
+{
+  // half cylinder arc
+  CArray arc_coordinates("coordinates");
+  CTable arc_connectivity("connectivity");
+  create_cylinder(arc_coordinates, arc_connectivity, 1., 100, 24, 3., 0., MathConsts::RealPi());
+  Real arc_flux = 0.;
+  const RealVector y_vector = boost::assign::list_of(0.)(1.)(0.);
+  Quad3DLagrangeP1 aSF; /* temporary solution, on Mac it tries to use copy-constructor */
+  integrate_region(arc_flux, ConstVectorField(y_vector), arc_coordinates, arc_connectivity, aSF);
+  BOOST_CHECK_CLOSE(arc_flux, 6., 0.01);
+}
+
+/// Lift produced by a rotating cylinder
+BOOST_AUTO_TEST_CASE( RotatingCylinder )
+{
+  // Create an approximation of a circle
+  const Real radius = 1.;
+  const Uint u_segments = 1000;
+  const Uint v_segments = 100;
+  const Real height = 3.;
+
+  // complete cylinder
+  CArray coordinates("coordinates");
+  CTable connectivity("connectivity");
+  create_cylinder(coordinates, connectivity, radius, u_segments, v_segments, height);
+
+  // Rotating cylinder in uniform flow
+  const Real u = 300.;
+  const Real circulation = 975.;
+  RealVector force(0.,3);
+  Quad3DLagrangeP1 aSF; /* temporary solution, on Mac it tries to use copy-constructor */
+  integrate_region(force, RotatingCylinderPressure(radius, circulation, u), coordinates, connectivity, aSF);
+  BOOST_CHECK_CLOSE(force[YY], height * 1.225*u*circulation, 0.001); // lift according to theory
+  BOOST_CHECK_SMALL(force[XX], 1e-8); // Drag should be zero
+  BOOST_CHECK_SMALL(force[ZZ], 1e-8);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_SUITE_END()
+
+////////////////////////////////////////////////////////////////////////////////
+
