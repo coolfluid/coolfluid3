d1e1e87c401f47fd4f16993dbea362678e58449e
gquentin <gquentin@gmail.com>
2010-07-19 14:39:09 +0000

    [DOC] added documentation for CNode class
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@596 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/GUI/Client/CNode.hpp
+++ src/GUI/Client/CNode.hpp
@@ -28,17 +28,26 @@ namespace Client {
 
   ////////////////////////////////////////////////////////////////////////////
 
+	/// @brief Structure that handles node options
+	
   struct NodeOption
   {
+		
+		/// @brief Option name
     QString m_paramName;
 
+		/// @brief Option type
     OptionType::Type m_paramType;
 
+		/// @brief Option value
     QString m_paramValue;
 
+		/// @brief Option description
     QString m_paramDescr;
 
+		/// @brief If @c true, the option is advanced. Otherwise, it is not.
     bool m_paramAdv;
+		
   }; // struct NodeParams
 
   ////////////////////////////////////////////////////////////////////////////
@@ -55,29 +64,38 @@ namespace Client {
     typedef boost::shared_ptr<CNode> Ptr;
     typedef boost::shared_ptr<CNode const> ConstPtr;
 
+		/// @brief Available sub-node types
     enum Type
     {
+			/// @brief Root node
       ROOT_NODE,
 
+			/// @brief Group node
       GROUP_NODE,
 
+			/// @brief Link node
       LINK_NODE,
 
+			/// @brief Mesh node
       MESH_NODE,
 
+			/// @brief Method node
       METHOD_NODE,
 
+			/// @brief Log node
       LOG_NODE,
 
+			/// @brief Tree node
       TREE_NODE,
 
+			/// @brief Browser node
       BROWSER_NODE
+			
     }; // enum Type
 
     ////////////////////////////////////////////
 
     /// @brief Constructor.
-
     /// @param name Component name.
     /// @param componentType Corresponding component type name
     /// (on the simulator side)
@@ -92,26 +110,46 @@ namespace Client {
 
     /// @brief Gives the icon associated to this node
     /// @return Returns the icon associated to this node
-    /// @note This method should be reimplemented by all subclasses.
     virtual QIcon getIcon() const = 0;
 
+		/// @brief Gives the node tooltip.
+		/// @return Returns the tooltip text.
     virtual QString getToolTip() const = 0;
 
+		/// @brief Indicates whether this node is a client component or not.
+		/// @return Returns @c true if this node is a client component.
     virtual bool isClientComponent() const = 0;
 
+		/// @brief Gives the node type
+		/// @return Returns the node type
     CNode::Type getType() const;
 
+		/// @brief Checks whether this node is of the provided type.
+		
+		/// Doing @code node->checkType(type) @endcode is equivalent to
+		/// @code node->getType() == type @endcode.
+		/// @param type The type to compare to.
+		/// @return Returns @c true is this node type is the same as @c type
     inline bool checkType(CNode::Type type) const
     {
       return m_type == type;
     }
 
-    void setTextData(const QString & text);
-
+		/// @brief Sets node options
+		/// @param node Note containing the options
     void setOptions(const CF::Common::XmlNode & node);
 
+		/// @brief Modifies options
+		
+		/// If at least on option has been modified, a @c configure signal is sent
+		/// to the corresponding component on the server.
+		/// @param options Map of options to modify. The key is the option name.
+		/// The value is the new option value, in string format.
     void modifyOptions(const QHash<QString, QString> options);
 
+		/// @brief Gives options
+		/// @param options Reference to a list where options will be put. The list
+		/// cleared before first use.
     virtual void getOptions(QList<NodeOption> & options) const = 0;
 
     /// @brief Creates an object tree from a given node
@@ -121,7 +159,7 @@ namespace Client {
     /// @throw XmlError If the tree could not be built.
     static CNode::Ptr createFromXml(CF::Common::XmlNode & node);
 
-    static CNode::Ptr createFromXml(const CF::Common::XmlNode & node);
+//    static CNode::Ptr createFromXml(const CF::Common::XmlNode & node);
 
     QMenu * getContextMenu() const;
 
@@ -134,8 +172,6 @@ namespace Client {
     }
 
   protected:
-
-    QString m_textData;
 
     QMenu * m_contextMenu;
 
