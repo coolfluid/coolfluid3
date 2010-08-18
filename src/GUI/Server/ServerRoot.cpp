a12c221ba25381d45527f21b4a7317c44a3a97f7
gquentin <gquentin@gmail.com>
2010-08-18 13:57:34 +0000

    [CLEANING] removed commented code
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@706 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/GUI/Server/ServerRoot.cpp
+++ src/GUI/Server/ServerRoot.cpp
@@ -34,31 +34,6 @@ CRoot::Ptr & ServerRoot::getRoot()
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-//void ServerRoot::processSignal(const QDomDocument & signal)
-//{
-//  boost::shared_ptr<XmlDoc> xmldoc = XmlOps::parse ( signal.toString().toStdString() );
-
-//  XmlNode& nodedoc = *XmlOps::goto_doc_node(*xmldoc.get());
-
-//  std::string type = nodedoc.first_node()->first_attribute("target")->value();
-//  std::string receiver = nodedoc.first_node()->first_attribute("receiver")->value();
-
-//  try
-//  {
-//    getRoot()->access_component(receiver)->call_signal( type, *nodedoc.first_node() );
-
-//    std::string str;
-//    XmlOps::xml_to_string(*xmldoc.get(), str);
-//    qDebug() << "Sending back " <<  str.c_str() << CFendl;
-
-//    getCore()->sendSignal(*xmldoc.get());
-//  }
-//  catch(Exception e)
-//  {
-//    CFerror << e.what() << CFendl;
-//  }
-//}
-
 void ServerRoot::processSignal(const std::string & target,
                                const std::string & receiver,
                                const std::string & clientid,
