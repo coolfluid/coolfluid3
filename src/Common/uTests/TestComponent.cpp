aad1c6df941e83f2d00bb40d8e90c4cbfbe5bfb3
tlmquintino <tlmquintino@gmail.com>
2010-09-04 10:18:01 +0000

    [FIX] removed unused header
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@798 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Common/uTests/TestComponent.cpp
+++ src/Common/uTests/TestComponent.cpp
@@ -1,4 +1,5 @@
 #define BOOST_TEST_DYN_LINK
+
 #include <boost/test/unit_test.hpp>
 #include <boost/foreach.hpp>
 #include <boost/iterator.hpp>
--- src/Common/uTests/TestLogStampFilter.cpp
+++ src/Common/uTests/TestLogStampFilter.cpp
@@ -1,6 +1,6 @@
 #define BOOST_TEST_DYN_LINK
+
 #include <boost/test/unit_test.hpp>
-#include <boost/test/unit_test_suite.hpp>
 #include <boost/iostreams/device/back_inserter.hpp>
 
 #include "Common/LogStampFilter.hpp"
@@ -10,10 +10,12 @@ using namespace boost;
 using namespace CF;
 using namespace CF::Common;
 
-struct LogStampFilterFixture
+////////////////////////////////////////////////////////////////////////////////
+
+struct LogStampFilter_Fixture
 {
   /// common setup for each test case
-  LogStampFilterFixture() :
+  LogStampFilter_Fixture() :
   m_buffer(),
   m_sink(iostreams::back_inserter(m_buffer))
   { 
@@ -21,7 +23,7 @@ struct LogStampFilterFixture
   }
 
   /// common tear-down for each test case
-  ~LogStampFilterFixture() { delete m_filter; }
+  ~LogStampFilter_Fixture() { delete m_filter; }
 
   /// possibly common functions used on the tests below
 
@@ -33,69 +35,67 @@ struct LogStampFilterFixture
   iostreams::back_insert_device<string> m_sink;
 };
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+////////////////////////////////////////////////////////////////////////////////
 
-BOOST_FIXTURE_TEST_SUITE(LogStampFilterTestSuite,LogStampFilterFixture)
+BOOST_FIXTURE_TEST_SUITE( LogStampFilter_TestSuite, LogStampFilter_Fixture )
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+////////////////////////////////////////////////////////////////////////////////
+
 
 BOOST_AUTO_TEST_CASE( write )
 {
   /// test whether the string is forwarded
-  LogStampFilterFixture f;
+  LogStampFilter_Fixture f;
   string str = "Hello world!";
-  unsigned int writtenBytes;
+  Uint nbbytes_wrtitten;
   
-  writtenBytes = f.m_filter->write(f.m_sink, str.c_str(), str.length());
+  nbbytes_wrtitten = f.m_filter->write(f.m_sink, str.c_str(), str.length());
   f.m_filter->endMessage();
   BOOST_CHECK_EQUAL(str, f.m_buffer);
-  BOOST_CHECK_EQUAL(writtenBytes, f.m_buffer.length());
+  BOOST_CHECK_EQUAL(nbbytes_wrtitten, f.m_buffer.length());
   
   f.m_buffer.clear();
   
   // test the stamps ("TestStream" is the name of the filter created by
-  // LogStampFilterFixture class)
+  // LogStampFilter_Fixture class)
   f.m_filter->setStamp("<%type%> ");
-  writtenBytes = f.m_filter->write(f.m_sink, str.c_str(), str.length());
+  nbbytes_wrtitten = f.m_filter->write(f.m_sink, str.c_str(), str.length());
   f.m_filter->endMessage(); 
   BOOST_CHECK_EQUAL(string("<TestStream> ") + str, f.m_buffer);
   // below : +13 because of the stamp size
-  BOOST_CHECK_EQUAL(writtenBytes, str.length() + 13); 
+  BOOST_CHECK_EQUAL(nbbytes_wrtitten, str.length() + 13);
   
   f.m_buffer.clear();
 
   // test the stamps ("TestStream" is the name of the filter created by
-  // LogStampFilterFixture class)
+  // LogStampFilter_Fixture class)
   f.m_filter->setStamp("<%tpye%> ");
-  writtenBytes = f.m_filter->write(f.m_sink, str.c_str(), str.length());
+  nbbytes_wrtitten = f.m_filter->write(f.m_sink, str.c_str(), str.length());
   f.m_filter->endMessage(); 
   BOOST_CHECK_EQUAL(string("<%tpye%> ") + str, f.m_buffer);
   // below : +9 because of the stamp size
-  BOOST_CHECK_EQUAL(writtenBytes, str.length() + 9); 
+  BOOST_CHECK_EQUAL(nbbytes_wrtitten, str.length() + 9);
+
 }
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+////////////////////////////////////////////////////////////////////////////////
+
 
 BOOST_AUTO_TEST_CASE( setPlace )
 {
-  LogStampFilterFixture f;
+  LogStampFilter_Fixture f;
   CodeLocation cl = FromHere();
   string str = "Hello world!";
-  unsigned int writtenBytes;
+  unsigned int nbbytes_wrtitten;
   
   f.m_filter->setStamp("--%place%-- ");
   f.m_filter->setPlace(cl);
-  writtenBytes = f.m_filter->write(f.m_sink, str.c_str(), str.length());
+  nbbytes_wrtitten = f.m_filter->write(f.m_sink, str.c_str(), str.length());
   f.m_filter->endMessage();
   BOOST_CHECK_EQUAL(string("--") + cl.short_str() + string("-- ") + str, f.m_buffer);
-  BOOST_CHECK_EQUAL(writtenBytes, str.length() + cl.short_str().length() + 5); 
+  BOOST_CHECK_EQUAL(nbbytes_wrtitten, str.length() + cl.short_str().length() + 5);
 }
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+////////////////////////////////////////////////////////////////////////////////
 
 BOOST_AUTO_TEST_SUITE_END()
