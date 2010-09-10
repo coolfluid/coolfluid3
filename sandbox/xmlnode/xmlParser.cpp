c7be9831f0145f7f646fdc90e537d0586d1dd519
wdeconinck <ir.willem.deconinck@gmail.com>
2010-09-10 14:13:35 +0000

    [COSM] warning fixed
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@839 4892e656-8fb9-448a-b5bc-b1919152596f

--- sandbox/xmlnode/xmlParser.cpp
+++ sandbox/xmlnode/xmlParser.cpp
@@ -737,7 +737,11 @@ ToXMLStringTool::~ToXMLStringTool(){ freeBuffer(); }
 void ToXMLStringTool::freeBuffer(){ if (buf) free(buf); buf=NULL; buflen=0; }
 XMLSTR ToXMLStringTool::toXML(XMLCSTR source)
 {
-    if (!source) return _CXML("");
+    if (!source) 
+    { 
+      XMLCSTR empty = _CXML("");
+      return empty;
+    }
     int l=lengthXMLString(source)+1;
     if (l>buflen) { buflen=l; buf=(XMLSTR)realloc(buf,l*sizeof(XMLCHAR)); }
     return toXMLUnSafe(buf,source);
