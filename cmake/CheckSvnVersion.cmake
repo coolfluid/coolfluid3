454560b990f264e412cb7095e9e12bf111150c21
wdeconinck <ir.willem.deconinck@gmail.com>
2010-09-10 12:18:17 +0000

    [FIX] Find svn revision when using a git repository
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@835 4892e656-8fb9-448a-b5bc-b1919152596f

--- cmake/CheckSvnVersion.cmake
+++ cmake/CheckSvnVersion.cmake
@@ -33,10 +33,30 @@ IF(Subversion_WC)
 
     ENDIF()
 ELSE()
-    IF(Subversion_FOUND)
-        LOG("Subversion svn command was found, but not svnversion.")
-    ENDIF()
-    SET(coolfluid_svnversion "NOVERSION-FOUND")
     
+    # Check if this is a git repository, and get revision number through "git-svn"
+    FIND_PACKAGE(Git QUIET)
+    SET(Git_WC FALSE)
+    
+    IF(Git_FOUND)
+        EXECUTE_PROCESS(COMMAND ${Git_EXECUTABLE} svn info
+          WORKING_DIRECTORY ${coolfluid_SOURCE_DIR}
+          OUTPUT_VARIABLE Git_svn_WC_INFO
+          ERROR_VARIABLE Git_svn_info_error
+          RESULT_VARIABLE Git_svn_info_result
+          OUTPUT_STRIP_TRAILING_WHITESPACE)
+        IF(${Git_svn_info_result} EQUAL 0)
+          # This means it is a git repository
+          EXECUTE_PROCESS(COMMAND ${Git_EXECUTABLE} svn find-rev HEAD^
+              WORKING_DIRECTORY ${coolfluid_SOURCE_DIR}
+              OUTPUT_VARIABLE coolfluid_svnversion
+              OUTPUT_STRIP_TRAILING_WHITESPACE)
+        ENDIF()
+    ELSE()
+        SET(coolfluid_svnversion "NOVERSION-FOUND")
+        IF(Subversion_FOUND && Git_FOUND)
+            LOG("svn or git command was found, but this is not a subversion or git repository.")
+        ENDIF()
+    ENDIF()
 ENDIF()    
 
