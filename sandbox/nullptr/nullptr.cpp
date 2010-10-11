f7f6d8f35c5a897c980de0565bc40ef49f81a65d
tlmquintino <tlmquintino@gmail.com>
2010-10-11 20:03:00 +0000

    [ENH] sandbox for nullptr idiom
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@1064 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ sandbox/nullptr/nullptr.cpp
@@ -0,0 +1,72 @@
+#include <typeinfo>
+ 
+const // It is a const object...
+class nullptr_t 
+{
+  public:
+    template<class T>
+      operator T*() const // convertible to any type of null non-member pointer...
+    { return 0; }
+ 
+    template<class C, class T>
+    operator T C::*() const   // or any type of null member pointer...
+    { return 0; }
+ 
+  private:
+    void operator&() const;  // Can't take address of nullptr
+ 
+} nullptr = {};
+ 
+struct C
+{
+  void func();
+};
+ 
+template<typename T> 
+void g( T* t ) {}
+ 
+template<typename T> 
+void h( T t ) {}
+ 
+void func (double *) {}
+void func (int) {}
+ 
+int main(void)
+{
+  char * ch = nullptr;        // ok
+  func (nullptr);             // Calls func(double *)
+  func (0);                   // Calls func(int)
+  void (C::*pmf2)() = 0;      // ok
+  void (C::*pmf)() = nullptr; // ok
+  nullptr_t n1, n2;
+  n1 = n2;
+  
+  // nullptr_t *null = &n1;    // Address can't be taken.
+  if (nullptr == ch) {}       // ok
+  if (nullptr == pmf) {}      // Valid statement; but fails on g++ 4.1.1 due to bug #33990
+  
+  const int n = 0;
+  if (nullptr == n) {}        // Should not compile; but only Comeau shows an error.
+  
+  //int p = 0;
+  //if (nullptr == p) {}      // not ok
+  //g (nullptr);              // Can't deduce T
+  
+  int expr = 0;
+  char* ch3 = expr ? nullptr : nullptr; // ch1 is the null pointer value
+  //char* ch4 = expr ? 0 : nullptr;     // error, types are not compatible
+  //int n3 = expr ? nullptr : nullptr;  // error, nullptr can’t be converted to int
+  //int n4 = expr ? 0 : nullptr;        // error, types are not compatible
+ 
+  h( 0 );                // deduces T = int
+  h( nullptr );          // deduces T = nullptr_t
+  h( (float*) nullptr ); // deduces T = float*
+ 
+  sizeof( nullptr );     // ok
+  typeid( nullptr );     // ok
+  try{
+      throw nullptr;         // ok
+  } catch (...) {};
+
+  return 0;
+}
