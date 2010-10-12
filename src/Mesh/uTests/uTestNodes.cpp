5e47df2d2064cebfb50e4248a6997023e6226c31
barche <bart.janssens@lid.kviv.be>
2010-10-12 14:43:40 +0000

    [ENH] Test ElementNodeValues in uTestNodes
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@1106 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Mesh/uTests/uTestNodes.cpp
+++ src/Mesh/uTests/uTestNodes.cpp
@@ -21,6 +21,7 @@
 #include "Mesh/CMeshReader.hpp"
 #include "Mesh/CMeshWriter.hpp"
 #include "Mesh/ElementNodes.hpp"
+#include "Mesh/ElementData.hpp"
 
 #include "Mesh/Integrators/Gauss.hpp"
 
@@ -133,6 +134,33 @@ BOOST_AUTO_TEST_CASE( ElementNodeTest )
     BOOST_CHECK_EQUAL(nodes[0][XX], 13);
     BOOST_CHECK_EQUAL(nodes[0][YY], 14);
     CFinfo << "nodes after mod: " << nodes << CFendl;
+  }
+}
+
+BOOST_AUTO_TEST_CASE( ElementNodeValuesTest )
+{
+  const CElements& firstRegion = get_first_region();
+  const CArray& coords = firstRegion.coordinates();
+  const CTable& conn = firstRegion.connectivity_table();
+  const Uint element_count = conn.size();
+  ElementNodeValues<4, 2> nodes;
+  for(Uint element = 0; element != element_count; ++element)
+  {
+    nodes.fill(coords, conn[element]);
+    const ConstElementNodeView node_view(coords, conn[element]);
+    for(Uint node_idx = 0; node_idx != conn.row_size(); ++node_idx)
+    {
+      for(Uint xyz = 0; xyz != coords.row_size(); ++xyz)
+      {
+        BOOST_CHECK_EQUAL(nodes[node_idx][xyz], node_view[node_idx][xyz]);
+      }
+    }
+    RealVector& node0 = nodes[0];
+    BOOST_CHECK(!node0.isOwner());
+    node0[XX] = 13;
+    node0[YY] = 14;
+    BOOST_CHECK_EQUAL(nodes[0][XX], 13);
+    BOOST_CHECK_EQUAL(nodes[0][YY], 14);
   }
 }
 
