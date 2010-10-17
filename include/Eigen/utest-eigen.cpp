1fed54fc08621f168721e766fe58d87de4ef0510
tlmquintino <tlmquintino@gmail.com>
2010-10-17 09:18:25 +0000

    [ENH] started adding test to Eigen, still not compared to MatrixT
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@1148 4892e656-8fb9-448a-b5bc-b1919152596f

--- include/Eigen/utest-eigen.cpp
+++ include/Eigen/utest-eigen.cpp
@@ -9,10 +9,46 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <iostream>
+
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
 /// fixture for each test of Eigen
 struct EigenFixture
 {
-  EigenFixture() {}
+
+  EigenFixture()
+  {
+    va.resize(5);
+    va << 1.0, 2.0, 3.0, 5.0, 7.0;
+
+    vb = VectorXd::LinSpaced(2.0, 10.0, 5);
+
+    vc = VectorXd::Constant(5, 7.0);
+
+    ma.resize(5,5);
+    ma << 1.0 , 2.5 , 3.0 , 4.5 , 5.0 ,
+          3.0 , 4.5 , 5.0 , 6.5 , 7.0 ,
+          5.0 , 6.5 , 7.0 , 8.5 , 5.0 ,
+          7.0 , 8.5 , 9.0 ,10.5 , 5.0 ,
+          9.0 ,10.5 ,11.0 ,12.5 ,13.0 ;
+
+    mb = MatrixXd::Identity(5,5);
+
+    mc = MatrixXd::Zero(5,5);
+  }
+
+  VectorXd va;
+  VectorXd vb;
+  VectorXd vc;
+
+  MatrixXd ma;
+  MatrixXd mb;
+  MatrixXd mc;
+
 };
 
 //////////////////////////////////////////////////////////////////////////////
@@ -21,12 +57,34 @@ BOOST_FIXTURE_TEST_SUITE( TestEigen, EigenFixture )
 
 //////////////////////////////////////////////////////////////////////////////
 
-BOOST_AUTO_TEST_CASE( Matrix )
-{
 //  BOOST_CHECK( bool );
 //  BOOST_CHECK_EQUAL(str, f.m_buffer);
 //  BOOST_CHECK_CLOSE( v1, v2, 0.0001 );
+
+BOOST_AUTO_TEST_CASE( VectorCoeffWiseOps )
+{
+  vc = va.array() * vb.array();
+  cout << vc << endl;
+  vc = va.array() / vb.array();
+  cout << vc << endl;
 }
+
+BOOST_AUTO_TEST_CASE( VectorVectorOps )
+{
+  vc = va + vb;
+  cout << vc << endl;
+  vc += vb;
+  cout << vc << endl;
+}
+
+BOOST_AUTO_TEST_CASE( MatrixMatrixOps )
+{
+  mc = ma * mb;
+  cout << mc << endl;
+  mc += mb;
+  cout << mc << endl;
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 
