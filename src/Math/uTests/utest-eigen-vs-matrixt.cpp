a34093052feb5b7bd1ea3d95318154f1602e9959
tlmquintino <tlmquintino@gmail.com>
2010-10-17 22:18:05 +0000

    [ENH] completed dgemm test w comparison to native
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@1154 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Math/uTests/utest-eigen-vs-matrixt.cpp
+++ src/Math/uTests/utest-eigen-vs-matrixt.cpp
@@ -24,11 +24,13 @@ using namespace CF::Math;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-/// @todo
-///  * check validity of the results
+struct nat
+{
+  double * cf_restrict data;
+};
 
 #define MSIZE 100000
-#define LSIZE 5
+#define LSIZE 4
 
 BOOST_AUTO_TEST_SUITE( VectorBenchmarkSuite )
 
@@ -39,17 +41,17 @@ BOOST_AUTO_TEST_CASE( timed_dgemv_eigen_dynamic )
   std::vector< MatrixXd > ma;
   ma.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    ma[i] = MatrixXd::Random( LSIZE, LSIZE);
+    ma[i] = MatrixXd::Constant( LSIZE, LSIZE, 2.0);
 
   std::vector< VectorXd > vb;
   vb.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    vb[i] = VectorXd::Random( LSIZE );
+    vb[i] = VectorXd::Constant( LSIZE, 5.0);
 
   std::vector< VectorXd > vc;
   vc.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    vc[i] = VectorXd::Random( LSIZE );
+    vc[i] = VectorXd::Constant( LSIZE, 0.0);
 
   boost::timer mtimer;
 
@@ -57,6 +59,7 @@ BOOST_AUTO_TEST_CASE( timed_dgemv_eigen_dynamic )
     vc[i].noalias() = ma[i] * vb[i];
 
   cout << "[Eigen:Dyn]   dgemv elapsed time: " << mtimer.elapsed() << " seconds" << endl;
+//  cout << vc[0] << endl;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -69,17 +72,17 @@ BOOST_AUTO_TEST_CASE( timed_dgemv_eigen_fixed )
   std::vector< MatrixSd > ma;
   ma.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    ma[i] = MatrixSd::Random( LSIZE, LSIZE);
+    ma[i] = MatrixSd::Constant( LSIZE, LSIZE, 2.0);
 
   std::vector< VectorSd > vb;
   vb.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    vb[i] = VectorSd::Random( LSIZE );
+    vb[i] = VectorSd::Constant( LSIZE, 5.0 );
 
   std::vector< VectorSd > vc;
   vc.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    vc[i] = VectorSd::Random( LSIZE );
+    vc[i] = VectorSd::Constant( LSIZE, 0.0 );
 
   boost::timer mtimer;
 
@@ -87,6 +90,7 @@ BOOST_AUTO_TEST_CASE( timed_dgemv_eigen_fixed )
     vc[i].noalias() = ma[i] * vb[i];
 
   cout << "[Eigen:Fixed] dgemv elapsed time: " << mtimer.elapsed() << " seconds" << endl;
+//  cout << vc[0] << endl;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -96,18 +100,26 @@ BOOST_AUTO_TEST_CASE( timed_dgemv_matrixt )
   std::vector< RealMatrix > ma;
   ma.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
+  {
     ma[i].resize( LSIZE, LSIZE );
-
+    ma[i] = 2.0;
+  }
 
   std::vector< RealVector > vb;
   vb.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
+  {
     vb[i].resize( LSIZE );
+    vb[i] = 5.0;
+  }
 
   std::vector< RealVector > vc;
   vc.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
+  {
     vc[i].resize( LSIZE );
+    vc[i] = 0.0;
+  }
 
   boost::timer mtimer;
 
@@ -115,26 +127,72 @@ BOOST_AUTO_TEST_CASE( timed_dgemv_matrixt )
     vc[i] = ma[i] * vb[i];
 
   cout << "[MatrixT]     dgemv elapsed time: " << mtimer.elapsed() << " seconds" << endl;
+//  cout << vc[0] << endl;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE( timed_dgemv_native )
+{
+  std::vector< nat > ma;
+  ma.resize( MSIZE );
+  for ( int i = 0; i < MSIZE; ++i )
+  {
+    ma[i].data = new double [ LSIZE * LSIZE ];
+    for ( int j = 0; j < LSIZE*LSIZE; ++j ) ma[i].data[j] = 2.0;
+  }
+
+  std::vector< nat > vb;
+  vb.resize( MSIZE );
+  for ( int i = 0; i < MSIZE; ++i )
+  {
+    vb[i].data = new double [ LSIZE ];
+    for ( int j = 0; j < LSIZE; ++j ) vb[i].data[j] = 5.0;
+  }
+
+  std::vector< nat > vc;
+  vc.resize( MSIZE );
+  for ( int i = 0; i < MSIZE; ++i )
+  {
+      vc[i].data = new double [ LSIZE ];
+      for ( int j = 0; j < LSIZE; ++j ) vc[i].data[j] = 0.0;
+  }
+
+  boost::timer mtimer;
+
+  for ( int e = 0; e < MSIZE; ++e )
+    for ( int i = 0; i < LSIZE; ++i )
+    {
+      const unsigned n = LSIZE;
+      for (Uint j = 0, k = i*n; j < n; ++j, ++k)
+        vc[e].data[i] += ma[e].data[k] * vb[e].data[j];
+    }
+
+  cout << "[native]      dgemv elapsed time: " << mtimer.elapsed() << " seconds" << endl;
+//  for ( int i = 0; i < LSIZE; ++i )
+//    cout << vc[0].data[i] << endl;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 BOOST_AUTO_TEST_CASE( timed_dgemm_eigen_dynamic )
 {
+  cout << "-------------------------" << endl;
+
   std::vector< MatrixXd > ma;
   ma.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    ma[i] = MatrixXd::Random( LSIZE, LSIZE);
+    ma[i] = MatrixXd::Constant( LSIZE, LSIZE, 2.0);
 
   std::vector< MatrixXd > mb;
   mb.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    mb[i] = MatrixXd::Random( LSIZE, LSIZE);
+    mb[i] = MatrixXd::Constant( LSIZE, LSIZE, 7.0);
 
   std::vector< MatrixXd > mc;
   mc.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    mc[i] = MatrixXd::Random( LSIZE, LSIZE);
+    mc[i] = MatrixXd::Constant( LSIZE, LSIZE, 0.0);
 
   boost::timer mtimer;
 
@@ -142,6 +200,7 @@ BOOST_AUTO_TEST_CASE( timed_dgemm_eigen_dynamic )
     mc[i].noalias() = ma[i] * mb[i];
 
   cout << "[Eigen:Dyn]   dgemm elapsed time: " << mtimer.elapsed() << " seconds" << endl;
+//  cout << mc[0] << endl;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -153,17 +212,17 @@ BOOST_AUTO_TEST_CASE( timed_dgemm_eigen_fixed )
   std::vector< MatrixSd > ma;
   ma.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    ma[i] = MatrixSd::Random( LSIZE, LSIZE);
+    ma[i] = MatrixSd::Constant( LSIZE, LSIZE, 2.0);
 
   std::vector< MatrixSd > mb;
   mb.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    mb[i] = MatrixSd::Random( LSIZE, LSIZE);
+    mb[i] = MatrixSd::Constant( LSIZE, LSIZE, 7.0);
 
   std::vector< MatrixSd > mc;
   mc.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    mc[i] = MatrixSd::Random( LSIZE, LSIZE);
+    mc[i] = MatrixSd::Constant( LSIZE, LSIZE, 0.0);
 
   boost::timer mtimer;
 
@@ -171,6 +230,7 @@ BOOST_AUTO_TEST_CASE( timed_dgemm_eigen_fixed )
     mc[i].noalias() = ma[i] * mb[i];
 
   cout << "[Eigen:Fixed] dgemm elapsed time: " << mtimer.elapsed() << " seconds" << endl;
+//  cout << mc[0] << endl;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -180,17 +240,26 @@ BOOST_AUTO_TEST_CASE( timed_dgemm_matrixt )
   std::vector< RealMatrix > ma;
   ma.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    ma[i].resize( LSIZE, LSIZE);
+  {
+    ma[i].resize( LSIZE, LSIZE );
+    ma[i] = 2.0;
+  }
 
   std::vector< RealMatrix > mb;
   mb.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    mb[i].resize( LSIZE, LSIZE);
+  {
+    mb[i].resize( LSIZE, LSIZE );
+    mb[i] = 7.0;
+  }
 
   std::vector< RealMatrix > mc;
   mc.resize( MSIZE );
   for ( int i = 0; i < MSIZE; ++i )
-    mc[i].resize( LSIZE, LSIZE);
+  {
+    mc[i].resize( LSIZE, LSIZE );
+    mc[i] = 0.0;
+  }
 
   boost::timer mtimer;
 
@@ -198,6 +267,77 @@ BOOST_AUTO_TEST_CASE( timed_dgemm_matrixt )
     mc[i] = ma[i] * mb[i];
 
   cout << "[MatrixT]     dgemm elapsed time: " << mtimer.elapsed() << " seconds" << endl;
+//  cout << mc[0];
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE( timed_dgemm_native )
+{
+  std::vector< nat > ma;
+  ma.resize( MSIZE );
+  for ( int i = 0; i < MSIZE; ++i )
+  {
+    ma[i].data = new double [ LSIZE * LSIZE ];
+    for ( int j = 0; j < LSIZE*LSIZE; ++j ) ma[i].data[j] = 2.0;
+  }
+
+  std::vector< nat > mb;
+  mb.resize( MSIZE );
+  for ( int i = 0; i < MSIZE; ++i )
+  {
+    mb[i].data = new double [ LSIZE * LSIZE ];
+    for ( int j = 0; j < LSIZE*LSIZE; ++j ) mb[i].data[j] = 7.0;
+  }
+
+  std::vector< nat > mc;
+  mc.resize( MSIZE );
+  for ( int i = 0; i < MSIZE; ++i )
+  {
+    mc[i].data = new double [ LSIZE * LSIZE ];
+    for ( int j = 0; j < LSIZE*LSIZE; ++j ) mc[i].data[j] = 0.0;
+  }
+
+  boost::timer mtimer;
+
+  for ( int e = 0; e < MSIZE; ++e )
+  {
+    const size_t nc = LSIZE;
+    const size_t m  = LSIZE;
+    const size_t n  = LSIZE;
+
+    const nat& A = ma[e];
+    const nat& B = mb[e];
+          nat& C = mc[e];
+
+    for (size_t i = 0; i < m; ++i)
+    {
+      const size_t jstart = i*n;
+      const size_t jmax = jstart + n;
+      const size_t kstart = i*nc;
+      size_t count = 0;
+      size_t k = 0;
+      while(count < nc)
+      {
+        double sum = 0.;
+        for(size_t j = jstart; j < jmax; ++j)
+        {
+          sum += A.data[j] * B.data[k];
+          k += nc;
+        }
+        C.data[kstart + count] = sum;
+        k = ++count;
+      }
+    }
+  }
+
+  cout << "[native]      dgemv elapsed time: " << mtimer.elapsed() << " seconds" << endl;
+//  for ( int i = 0; i < LSIZE; ++i )
+//  {
+//    for ( int j = 0; j < LSIZE; ++j )
+//      cout << mc[0].data[i*LSIZE + j] << " ";
+//    cout << endl;
+//  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
