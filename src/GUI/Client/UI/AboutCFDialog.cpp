d7e99b5b47a32dfd8e4184a53abff38ef0b974a9
gquentin <gquentin@gmail.com>
2010-10-19 09:55:55 +0000

    [DEV] added Qt version to client "About" dialog.
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@1177 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/GUI/Client/UI/AboutCFDialog.cpp
+++ src/GUI/Client/UI/AboutCFDialog.cpp
@@ -30,6 +30,7 @@ AboutCFDialog::AboutCFDialog(QWidget * parent)
   m_infoList << new CFInfo("Kernel version:", CF_KERNEL_VERSION_STR, m_infoLayout);
   m_infoList << new CFInfo("Build operating system:", QString("%1 [%2bits]").arg(CF_OS_LONGNAME).arg(CF_OS_BITS), m_infoLayout);
   m_infoList << new CFInfo("Build processor:", CF_BUILD_PROCESSOR, m_infoLayout);
+  m_infoList << new CFInfo("Qt version:", QT_VERSION_STR, m_infoLayout);
 
   m_mainLayout->addLayout(m_infoLayout);
   m_mainLayout->addWidget(m_btOK);
@@ -42,15 +43,16 @@ AboutCFDialog::AboutCFDialog(QWidget * parent)
 
 AboutCFDialog::~AboutCFDialog()
 {
-  //  delete m_btOK;
-  //  delete m_mainLayout;
-  //  delete m_infoLayout;
+  delete m_btOK;
+  delete m_infoLayout;
+  delete m_mainLayout;
 }
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-AboutCFDialog::CFInfo::CFInfo(const QString & name, const QString & value, QFormLayout * parent)
+AboutCFDialog::CFInfo::CFInfo(const QString & name, const QString & value,
+                              QFormLayout * parent)
 {
   this->labName = new QLabel(name);
   this->labValue = new QLabel(value);
