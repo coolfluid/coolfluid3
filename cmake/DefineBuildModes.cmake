80a9921cca8d92258cbe6ca7f2dfc4da5b674c7e
tlmquintino <tlmquintino@gmail.com>
2010-10-25 20:08:42 +0000

    [ENH] insure that build tyep belongs to either  Debug Release RelWithDebInfo MinSizeRel
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@1222 4892e656-8fb9-448a-b5bc-b1919152596f

--- cmake/DefineBuildModes.cmake
+++ cmake/DefineBuildModes.cmake
@@ -1,116 +1,96 @@
-# set the default build to be RelWithDebInfo
+# always define a build - default is RelWithDebInfo
 if( NOT CMAKE_BUILD_TYPE )
-  set( CMAKE_BUILD_TYPE RelWithDebInfo CACHE STRING "Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel." FORCE)
+  set( CMAKE_BUILD_TYPE RelWithDebInfo CACHE STRING "Choose the type of build, options are: Debug Release RelWithDebInfo MinSizeRel." FORCE)
 endif()
+
+# capitalize the build type
+string( TOUPPER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE_CAPS )
+set(CF_BUILD_TYPE_OK OFF)
 
 #######################################################
 ### DEBUG mode
 
-if(UNIX)
-  if(CMAKE_COMPILER_IS_GNUCC)
-    list( APPEND CF_C_FLAGS_DEBUG       " -g -O1" )
-    list( APPEND CF_CXX_FLAGS_DEBUG     " -g -O1" )
-    if(CF_ENABLE_STDDEBUG)
-      list( APPEND CF_CXX_FLAGS_DEBUG   " -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC" )
-    endif()
-    list( APPEND CF_Fortran_FLAGS_DEBUG " -g -O1" )
-  endif()
-endif()
+if(CMAKE_BUILD_TYPE_CAPS MATCHES "DEBUG")
 
-### TESTING THIS MODIFICATION
-if( WIN32 )
+  set(CF_BUILD_TYPE_OK ON)
+
+  if( CMAKE_COMPILER_IS_GNUCC AND CF_ENABLE_STDDEBUG )
+      list( APPEND CF_CXX_FLAGS_DEBUG   " -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC" )
+      mark_as_advanced( CF_CXX_FLAGS_DEBUG )
+  endif()
+
+  if( WIN32 )
     list( APPEND CF_C_FLAGS_DEBUG       "/Zi" )
     list( APPEND CF_CXX_FLAGS_DEBUG     "/Zi" )
-    list( APPEND CF_Fortran_FLAGS_DEBUG "" )
-endif()
+    mark_as_advanced( CF_C_FLAGS_DEBUG  CF_CXX_FLAGS_DEBUG )
+  endif()
 
-if(CMAKE_BUILD_TYPE MATCHES "[Dd][Ee][Bb][Uu][Gg]")
+
   coolfluid_set_if_not_defined( CF_ENABLE_ASSERTIONS   ON   )
   coolfluid_set_if_not_defined( CF_ENABLE_TRACE        ON   )
   coolfluid_set_if_not_defined( CF_ENABLE_LOGALL       ON   )
   coolfluid_set_if_not_defined( CF_ENABLE_LOGDEBUG     ON   )
   coolfluid_set_if_not_defined( CF_ENABLE_DEBUG_MACROS ON   )
   coolfluid_set_if_not_defined( CF_ENABLE_STATIC       OFF  )
+
 endif()
 
 #######################################################
-### OPTIM mode
+### RELWITHDEBINFO mode
 
-if(UNIX)
-  if(CMAKE_COMPILER_IS_GNUCC)
-    list( APPEND CF_C_FLAGS_OPTIM       "-g -O2" )
-    list( APPEND CF_CXX_FLAGS_OPTIM     "-g -O2" )
-    list( APPEND CF_Fortran_FLAGS_OPTIM "-g -O2" )
-  endif(CMAKE_COMPILER_IS_GNUCC)
-endif(UNIX)
+if(CMAKE_BUILD_TYPE_CAPS MATCHES "RELWITHDEBINFO")
 
+  set(CF_BUILD_TYPE_OK ON)
 
-mark_as_advanced( CF_CMAKE_CXX_FLAGS_OPTIM  CF_CMAKE_C_FLAGS_OPTIM CF_CMAKE_Fortran_FLAGS_OPTIM )
-
-if(CMAKE_BUILD_TYPE MATCHES "[Oo][Pp][Tt][Ii][Mm]")
   coolfluid_set_if_not_defined( CF_ENABLE_ASSERTIONS   ON   )
   coolfluid_set_if_not_defined( CF_ENABLE_TRACE        ON  )
   coolfluid_set_if_not_defined( CF_ENABLE_LOGALL       ON   )
   coolfluid_set_if_not_defined( CF_ENABLE_LOGOPTIM     ON   )
   coolfluid_set_if_not_defined( CF_ENABLE_OPTIM_MACROS ON   )
   coolfluid_set_if_not_defined( CF_ENABLE_STATIC       OFF  )
-endif()
 
-#######################################################
-### RelWithDebInfo mode
-
-if(UNIX)
-  if(CMAKE_COMPILER_IS_GNUCC)
-    list( APPEND CF_C_FLAGS_RelWithDebInfo       "-g -O2" )
-    list( APPEND CF_CXX_FLAGS_RelWithDebInfo     "-g -O2" )
-    list( APPEND CF_Fortran_FLAGS_RelWithDebInfo "-g -O2" )
-  endif(CMAKE_COMPILER_IS_GNUCC)
-endif(UNIX)
-
-
-mark_as_advanced( CF_CMAKE_CXX_FLAGS_RelWithDebInfo  CF_CMAKE_C_FLAGS_RelWithDebInfo CF_CMAKE_Fortran_FLAGS_RelWithDebInfo )
-
-if(CMAKE_BUILD_TYPE MATCHES "[Re][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo]")
-  coolfluid_set_if_not_defined( CF_ENABLE_ASSERTIONS   ON   )
-  coolfluid_set_if_not_defined( CF_ENABLE_TRACE        ON  )
-  coolfluid_set_if_not_defined( CF_ENABLE_LOGALL       ON   )
-  coolfluid_set_if_not_defined( CF_ENABLE_LOGOPTIM     ON   )
-  coolfluid_set_if_not_defined( CF_ENABLE_OPTIM_MACROS ON   )
-  coolfluid_set_if_not_defined( CF_ENABLE_STATIC       OFF  )
 endif()
 
 #######################################################
 ### RELEASE mode
 
-if(UNIX)
+if(CMAKE_BUILD_TYPE_CAPS MATCHES "RELEASE")
+
+  set(CF_BUILD_TYPE_OK ON)
+
   if(CMAKE_COMPILER_IS_GNUCC)
     list( APPEND CF_C_FLAGS_RELEASE       "-O3 -fomit-frame-pointer" )
     list( APPEND CF_CXX_FLAGS_RELEASE     "-O3 -fomit-frame-pointer" )
     list( APPEND CF_Fortran_FLAGS_RELEASE "-O3 -fomit-frame-pointer" )
-  endif(CMAKE_COMPILER_IS_GNUCC)
-endif(UNIX)
+    mark_as_advanced( CF_CMAKE_CXX_FLAGS_RELEASE  CF_CMAKE_C_FLAGS_RELEASE CF_CMAKE_Fortran_FLAGS_RELEASE )
+  endif()
 
-
-mark_as_advanced( CF_CMAKE_CXX_FLAGS_RELEASE  CF_CMAKE_C_FLAGS_RELEASE CF_CMAKE_Fortran_FLAGS_RELEASE )
-
-if(CMAKE_BUILD_TYPE MATCHES "[Re][Ee][Ll][Ee][Aa][Ss][Ee]")
   coolfluid_set_if_not_defined( CF_ENABLE_ASSERTIONS   OFF  )
   coolfluid_set_if_not_defined( CF_ENABLE_TRACE        OFF  )
   coolfluid_set_if_not_defined( CF_ENABLE_LOGALL       ON   )
   coolfluid_set_if_not_defined( CF_ENABLE_LOGDEBUG     OFF  )
   coolfluid_set_if_not_defined( CF_ENABLE_DEBUG_MACROS OFF  )
   coolfluid_set_if_not_defined( CF_ENABLE_STATIC       OFF  )
+
 endif()
 
 #######################################################
 
-if( DEFINED CF_C_FLAGS_${CMAKE_BUILD_TYPE} )
-	set( CMAKE_C_FLAGS_${CMAKE_BUILD_TYPE}  "${CF_C_FLAGS_${CMAKE_BUILD_TYPE}}"  CACHE STRING "Flags used by the C compiler during ${CMAKE_BUILD_TYPE} builds." FORCE )
+# check that the build type belongs to the list [ Debug Release RelWithDebInfo MinSizeRel ]
+if( NOT CF_BUILD_TYPE_OK )
+  message( FATAL_ERROR "Build type (case insensitive) [${CMAKE_BUILD_TYPE_CAPS}] is not one of [ Debug Release RelWithDebInfo MinSizeRel ]" )
 endif()
-if( DEFINED CF_CXX_FLAGS_${CMAKE_BUILD_TYPE} )
-	set( CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}  "${CF_CXX_FLAGS_${CMAKE_BUILD_TYPE}}"  CACHE STRING "Flags used by the C++ compiler during ${CMAKE_BUILD_TYPE} builds." FORCE )
+
+#######################################################
+
+# apply additions to flags
+if( DEFINED CF_C_FLAGS_${CMAKE_BUILD_TYPE_CAPS} )
+  set( CMAKE_C_FLAGS_${CMAKE_BUILD_TYPE_CAPS}  "${CMAKE_C_FLAGS_${CMAKE_BUILD_TYPE_CAPS}} ${CF_C_FLAGS_${CMAKE_BUILD_TYPE_CAPS}}"  CACHE STRING "Flags used by the C compiler during ${CMAKE_BUILD_TYPE} builds." FORCE )
 endif()
-if( DEFINED CF_Fortran_FLAGS_${CMAKE_BUILD_TYPE} )
-	set( CMAKE_Fortran_FLAGS_${CMAKE_BUILD_TYPE}  "${CF_Fortran_FLAGS_${CMAKE_BUILD_TYPE}}"  CACHE STRING "Flags used by the C compiler during ${CMAKE_BUILD_TYPE} builds." FORCE )
+if( DEFINED CF_CXX_FLAGS_${CMAKE_BUILD_TYPE_CAPS} )
+  set( CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE_CAPS}  "${CMAKE_C_FLAGS_${CMAKE_BUILD_TYPE_CAPS}} ${CF_CXX_FLAGS_${CMAKE_BUILD_TYPE_CAPS}}"  CACHE STRING "Flags used by the C++ compiler during ${CMAKE_BUILD_TYPE} builds." FORCE )
+endif()
+if( DEFINED CF_Fortran_FLAGS_${CMAKE_BUILD_TYPE_CAPS} )
+  set( CMAKE_Fortran_FLAGS_${CMAKE_BUILD_TYPE_CAPS}  "${CF_Fortran_FLAGS_${CMAKE_BUILD_TYPE_CAPS}}"  CACHE STRING "Flags used by the C compiler during ${CMAKE_BUILD_TYPE} builds." FORCE )
 endif()
 
