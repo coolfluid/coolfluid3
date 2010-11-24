85408fa3b348ce5544ff91a6b1436e0f8d3fbe3d
wdeconinck <ir.willem.deconinck@gmail.com>
2010-11-24 12:46:56 +0000

    [ENH] add lower-case boost_foreach, plus additional index_foreach, foreach_container. See utest-foreach for usage
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@1404 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ src/Common/Foreach.hpp
@@ -0,0 +1,58 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#ifndef CF_Common_FOREACH_hpp
+#define CF_Common_FOREACH_hpp
+
+#include <boost/foreach.hpp>
+#include <boost/fusion/include/at_c.hpp>
+#include <boost/fusion/include/std_pair.hpp>
+#include <boost/preprocessor/seq/for_each_i.hpp>
+
+/// lowercase version of BOOST_FOREACH
+#define boost_foreach BOOST_FOREACH 
+
+/// boost_foreach version that also increments an index
+#define index_foreach(index,a,b)                            \
+    for(unsigned int index = static_cast<unsigned int>(-1); \
+        index == static_cast<unsigned int>(-1);)            \
+            BOOST_FOREACH(a,b) if(++index,true) 
+
+
+// Following is a feature request in boost
+// It will be used for access to container variables:
+//
+// example 1:
+// std::map<int, int> my_map;
+// foreach_container((int key)(int value), my_map)
+//   std::cout << key << " : " << value << "\n";
+//
+// example 2: 
+// std::vector<boost::fusion::vector<int, std::string, int> > my_complex_type;
+// foreach_container((int i)(std::string const& j)(int& k), my_complex_type)
+//   std::cout << "line: " << i << ", " << j << ", " << ++k;
+
+#define BOOST_FOREACH_ASSIGN_VAR(R, ROW, I, VAR)                                                                  \
+  for (VAR = boost::fusion::at_c<I>(ROW); !BOOST_FOREACH_ID(_foreach_leave_outerloop);                            \
+      BOOST_FOREACH_ID(_foreach_leave_outerloop) = true)
+
+/// boost_foreach version that allows to use std::map's, boost::tuple's, std::pair's internal variables
+#define foreach_container(VARS, COL)                                                                                  \
+    BOOST_FOREACH_PREAMBLE()                                                                                      \
+    if (boost::foreach_detail_::auto_any_t BOOST_FOREACH_ID(_foreach_col) = BOOST_FOREACH_CONTAIN(COL)) {} else   \
+    if (boost::foreach_detail_::auto_any_t BOOST_FOREACH_ID(_foreach_cur) = BOOST_FOREACH_BEGIN(COL)) {} else     \
+    if (boost::foreach_detail_::auto_any_t BOOST_FOREACH_ID(_foreach_end) = BOOST_FOREACH_END(COL)) {} else       \
+    for (bool BOOST_FOREACH_ID(_foreach_continue) = true,                                                         \
+        BOOST_FOREACH_ID(_foreach_leave_outerloop) = true;                                                        \
+        BOOST_FOREACH_ID(_foreach_continue) && !BOOST_FOREACH_DONE(COL);                                          \
+        BOOST_FOREACH_ID(_foreach_continue) ? BOOST_FOREACH_NEXT(COL) : (void)0)                                  \
+        if  (boost::foreach_detail_::set_false(BOOST_FOREACH_ID(_foreach_continue))) {} else                      \
+        if  (boost::foreach_detail_::set_false(BOOST_FOREACH_ID(_foreach_leave_outerloop))) {} else               \
+        BOOST_PP_SEQ_FOR_EACH_I(BOOST_FOREACH_ASSIGN_VAR, BOOST_FOREACH_DEREF(COL), VARS)                         \
+        for (; !BOOST_FOREACH_ID(_foreach_continue); BOOST_FOREACH_ID(_foreach_continue) = true)
+
+
+#endif // CF_Common_FOREACH_hpp
---
+++ src/Common/uTests/utest-foreach.cpp
@@ -0,0 +1,82 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "Test module for simplifying for loops"
+
+#include <boost/test/unit_test.hpp>
+
+#include "Common/CF.hpp"
+#include "Common/Foreach.hpp"
+
+//////////////////////////////////////////////////////////////////////////////
+
+struct ForeachFixture
+{
+  /// common setup for each test case
+  ForeachFixture()
+  {    
+  }
+
+  /// common tear-down for each test case
+  ~ForeachFixture()
+  {
+  }
+
+private:
+
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_SUITE( ForeachTests, ForeachFixture )
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE ( test_boost_foreach )
+{
+  std::vector<int> vec(5,1.0);
+
+  boost_foreach(int value, vec)
+    BOOST_CHECK_EQUAL(value,1.0);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE ( test_index_foreach )
+{
+  std::vector<int> vec(5,1.0);
+
+  index_foreach(i, int value, vec)
+    BOOST_CHECK_EQUAL(value,vec[i]); 
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE ( test_foreach_container )
+{
+  std::map<std::string,int> my_map;
+  my_map["first"] = 1;
+  my_map["second"] = 2;
+
+  foreach_container((const std::string& key)(const int value), my_map)
+  {
+    if (key == "first")
+      BOOST_CHECK_EQUAL(value,1);
+    else if (key == "second")
+      BOOST_CHECK_EQUAL(value,2);
+    else
+      BOOST_CHECK(false);
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+////////////////////////////////////////////////////////////////////////////////
+
