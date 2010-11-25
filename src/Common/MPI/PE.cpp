36aef3b43efefffc6eec43022e7660351a811cfa
tlmquintino <tlmquintino@gmail.com>
2010-11-25 14:08:42 +0000

    [EHN] use of delete_ptr function
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@1441 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Common/MPI/PE.cpp
+++ src/Common/MPI/PE.cpp
@@ -10,7 +10,7 @@
 using namespace boost;
 
 namespace CF {
-  namespace Common  {
+namespace Common  {
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -18,16 +18,16 @@ PE::PE(int argc, char** args):
 	boost::mpi::communicator()
 {
   m_environment=new mpi::environment(argc,args);
-	m_current_status=WorkerStatus::NOT_RUNNING;
+  m_current_status=WorkerStatus::NOT_RUNNING;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-PE::PE() :
+PE::PE(): 
 	boost::mpi::communicator()
 {
-  m_environment=0;
-  m_current_status=WorkerStatus::NOT_RUNNING;
+  m_environment = 0;
+  m_current_status = WorkerStatus::NOT_RUNNING;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -53,34 +53,39 @@ void PE::init(int argc, char** args)
 // (which it may modify) in your main program. The creation of this object
 // initializes MPI, and its destruction will finalize MPI.
 	
-  if (m_environment!=0) delete(m_environment);
-  m_environment=new mpi::environment(argc,args);
+  if (!m_environment)
+    delete_ptr(m_environment);
+  m_environment = new mpi::environment(argc,args);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-bool PE::is_init() const {
-  if (m_environment==0) return false;
+bool PE::is_init() const
+{
+  if ( m_environment == 0 ) return false;
   return m_environment->initialized();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void PE::finalize() {
-	if (is_init())
+void PE::finalize()
+{
+	if ( is_init() )
 	{
 		barrier();
-		delete(m_environment);
-		m_environment=0;
-		/// TODO: communicator has no destructor, see boost/mpi/communicator.hpp, this is sort of dangerous
-		/// needs to be checked if it somehow realizes that
+        delete_ptr(m_environment);
+		
+		/// @TODO Communicator has no destructor, see boost/mpi/communicator.hpp, 
+		///       this is sort of dangerous.
+		///       Needs to be checked if it somehow realizes that
 	}	
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-Uint PE::rank() const {
-  if (!is_init()) return 0;
+Uint PE::rank() const 
+{
+  if ( !is_init() ) return 0;
   return static_cast<Uint>(mpi::communicator::rank());
 }
 
@@ -88,21 +93,23 @@ Uint PE::rank() const {
 
 Uint PE::size() const
 {
-  if (!is_init()) return 1;
+  if ( !is_init() ) return 1;
   return static_cast<Uint>(mpi::communicator::size());
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void PE::change_status(WorkerStatus::Type status) {
+void PE::change_status(WorkerStatus::Type status) 
+{
   cf_assert ( WorkerStatus::Convert::is_valid(status) );
   m_current_status = status;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-WorkerStatus::Type PE::status() {
+WorkerStatus::Type PE::status() 
+{
   return m_current_status;
 }
 
