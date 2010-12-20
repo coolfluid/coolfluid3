8b46e4499365275602f02d7a590ffbe3c57b0361
tlmquintino <tlmquintino@gmail.com>
2010-12-20 14:26:11 +0000

    [ENH] implemennted unsteady CModel simulate
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@1688 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Solver/CModelUnsteady.cpp
+++ src/Solver/CModelUnsteady.cpp
@@ -5,7 +5,11 @@
 // See doc/lgpl.txt and doc/gpl.txt for the license text.
 
 #include "Common/CBuilder.hpp"
+#include "Common/Foreach.hpp"
+#include "Common/ComponentPredicates.hpp"
+
 #include "Solver/CModelUnsteady.hpp"
+#include "Solver/CIterativeSolver.hpp"
 
 namespace CF {
 namespace Solver {
@@ -32,6 +36,25 @@ CModelUnsteady::~CModelUnsteady()
 
 void CModelUnsteady::simulate ()
 {
+  Real ti  = 0.0;
+  Real dt  = 0.1;
+  Real ct  = 0.0;
+  Real tf  = 1.0;
+
+  // loop over time
+  while( ct < tf )
+  {
+    // compute which dt to take
+    if( ct + dt > tf ) dt = tf - ct;
+
+    // call all (non-linear) iterative solvers
+    // to solve this dt step
+    boost_foreach(CIterativeSolver& is, find_components<CIterativeSolver>(*this))
+      is.solve();
+
+    ct += dt; // update time
+
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
