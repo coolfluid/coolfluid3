181b4bc91a3be1badd2ab5218c6b910fec28aee0
wdeconinck <ir.willem.deconinck@gmail.com>
2010-12-28 20:06:50 +0000

    [ENH] macro to create component classes that wrap any kind of data
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@1729 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ src/Common/CreateComponentDataType.hpp
@@ -0,0 +1,55 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#ifndef CF_Common_CreateComponentDataType_hpp
+#define CF_Common_CreateComponentDataType_hpp
+
+////////////////////////////////////////////////////////////////////////////////
+
+#include "Common/Component.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+
+/// @define create_component_data_type
+/// This macro creates a component class that wraps any given data type that has
+/// a default constructor.
+/// The data can be accessed directly through its memberfunction data()
+/// @param data_type        The data type to wrap in the component class
+/// @param API              The API of the library this class is created in
+/// @param CName            The typename of the component class
+/// @param CName_type_name  The name of the class as a std::string
+#define create_component_data_type( data_type , API , CName , Cname_type_name) \
+class API CName : public Common::Component                                     \
+{                                                                              \
+public:                                                                        \
+                                                                               \
+  typedef boost::shared_ptr<CName> Ptr;                                        \
+  typedef boost::shared_ptr<CName const> ConstPtr;                             \
+                                                                               \
+public:                                                                        \
+                                                                               \
+  CName ( const std::string& name ) : Common::Component(name)                  \
+  {                                                                            \
+    regist_typeinfo(this);                                                     \
+  }                                                                            \
+                                                                               \
+  virtual ~CName() {}                                                          \
+                                                                               \
+  static std::string type_name () { return Cname_type_name; }                  \
+                                                                               \
+  const data_type & data() const { return m_data; }                            \
+                                                                               \
+  data_type & data() { return m_data; }                                        \
+                                                                               \
+private:                                                                       \
+                                                                               \
+  data_type m_data;                                                            \
+                                                                               \
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+#endif // CF_Common_CreateComponentDataType_hpp
---
+++ test/Common/utest-create-component-datatype.cpp
@@ -0,0 +1,63 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "Test module for DataComponentWrapper.hpp"
+
+#include <set>
+#include <boost/test/unit_test.hpp>
+
+#include "Common/CF.hpp"
+#include "Common/CreateComponentDataType.hpp"
+#include "Common/Log.hpp"
+#include "Common/Foreach.hpp"
+
+//////////////////////////////////////////////////////////////////////////////
+
+using namespace CF;
+using namespace CF::Common;
+
+struct DataComponentWrapperFixture
+{
+  /// common setup for each test case
+  DataComponentWrapperFixture()
+  {    
+  }
+
+  /// common tear-down for each test case
+  ~DataComponentWrapperFixture()
+  {
+  }
+    
+  create_component_data_type( std::set<int> , Common_API, Component_std_set_int, "Component_std_set<integer>");
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_SUITE( DataComponentWrapperTests, DataComponentWrapperFixture )
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE ( test_DataComponentWrapper )
+{
+  Component_std_set_int::Ptr comp_set = allocate_component<Component_std_set_int>("set");
+  
+  std::set<int>& wrapped_set = comp_set->data();
+
+  BOOST_CHECK_EQUAL(comp_set->data().size() , 0);
+  
+  wrapped_set.insert(5);
+  
+  BOOST_CHECK_EQUAL(comp_set->data().size() , 1);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+////////////////////////////////////////////////////////////////////////////////
+
