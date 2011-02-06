d9a8c12912a2069913259f4a5a81ed120d84d4fb
wdeconinck <ir.willem.deconinck@gmail.com>
2011-02-06 10:01:56 +0000

    [ENH] compiler version available as CF_CXX_COMPILER_VERSION
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@1992 4892e656-8fb9-448a-b5bc-b1919152596f

--- cmake/CheckCompilerFeatures.cmake
+++ cmake/CheckCompilerFeatures.cmake
@@ -4,6 +4,17 @@ coolfluid_log( "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++" )
 
 #######################################################################################
 
+  exec_program(${CMAKE_CXX_COMPILER}
+               ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpversion
+               OUTPUT_VARIABLE CF_CXX_COMPILER_VERSION)
+               
+  string(REGEX REPLACE "([0-9])\\.([0-9])(\\.[0-9])?" "\\1.\\2"
+         CF_CXX_COMPILER_VERSION ${CF_CXX_COMPILER_VERSION})
+
+  coolfluid_log( "+++++  Checking C++ compiler version  -- ${CMAKE_CXX_COMPILER_ID} ${CF_CXX_COMPILER_VERSION}" )
+  
+#######################################################################################
+
   coolfluid_log( "+++++  Checking sizes of types" )
   CHECK_TYPE_SIZE("void *"       CF_SIZEOF_PTR)
   CHECK_TYPE_SIZE(char           CF_SIZEOF_CHAR)
