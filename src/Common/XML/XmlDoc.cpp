ac8444efc79cbd758675ba67951004f613333586
martinv <martin.vymazal@gmail.com>
2011-02-16 14:45:41 +0000

    [FIX] missing headers for linux compilation
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2092 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Common/XML/XmlDoc.cpp
+++ src/Common/XML/XmlDoc.cpp
@@ -4,6 +4,8 @@
 // GNU Lesser General Public License version 3 (LGPLv3).
 // See doc/lgpl.txt and doc/gpl.txt for the license text.
 
+#include <cstdio>
+
 #include "rapidxml/rapidxml.hpp"
 
 #include "Common/BasicExceptions.hpp"
