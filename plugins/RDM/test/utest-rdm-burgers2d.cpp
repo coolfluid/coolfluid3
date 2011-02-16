b83130070dd633137c8d702c87cfb4a5b8e52920
tlmquintino <tlmquintino@gmail.com>
2011-02-16 20:09:02 +0000

    [FIX] RD test cases
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2099 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ plugins/RDM/test/utest-rdm-burgers2d.cpp
@@ -0,0 +1,276 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "Test module for CF::RDM::ScalarAdvection"
+
+#include <boost/test/unit_test.hpp>
+#include <boost/filesystem/path.hpp>
+
+#include "Common/CreateComponent.hpp"
+#include "Common/ComponentPredicates.hpp"
+#include "Common/Log.hpp"
+#include "Common/CLink.hpp"
+#include "Common/Foreach.hpp"
+
+#include "Solver/CIterativeSolver.hpp"
+#include "Solver/CModel.hpp"
+#include "Solver/CDiscretization.hpp"
+#include "Solver/CPhysicalModel.hpp"
+#include "Solver/Actions/CLoop.hpp"
+
+#include "Mesh/LoadMesh.hpp"
+#include "Mesh/CCells.hpp"
+#include "Mesh/CMeshReader.hpp"
+#include "Mesh/CMeshWriter.hpp"
+#include "Mesh/CDomain.hpp"
+#include "Mesh/CRegion.hpp"
+#include "Mesh/Actions/CBubbleEnrich.hpp"
+#include "Mesh/Actions/CBubbleRemove.hpp"
+
+#include "RDM/ScalarAdvection.hpp"
+#include "RDM/ResidualDistribution.hpp"
+
+using namespace CF;
+using namespace CF::Common;
+using namespace CF::Mesh;
+using namespace CF::Solver;
+using namespace CF::Solver::Actions;
+using namespace CF::RDM;
+
+//#define BUBBLE
+
+struct burgers2d_global_fixture
+{
+  burgers2d_global_fixture()
+  {
+    burgers2d_wizard = allocate_component<ScalarAdvection>("mymodel");
+
+    boost::shared_ptr<XmlDoc> doc = XmlOps::create_doc();
+    XmlNode& node  = *XmlOps::goto_doc_node(*doc.get());
+    XmlParams p(node);
+    p.add_option<std::string>("Model name","mymodel");
+
+    burgers2d_wizard->create_model(node);
+  }
+
+  ScalarAdvection::Ptr burgers2d_wizard;
+
+};
+
+struct burgers2d_local_fixture
+{
+  burgers2d_local_fixture() :
+    model  ( * Core::instance().root()->get_child("mymodel")->as_type<CModel>() ),
+    domain ( find_component_recursively<CDomain>(model)  ),
+    solver ( find_component_recursively<CIterativeSolver>(model) ),
+    discretization( find_component_recursively<CDiscretization>(solver) )
+
+  {}
+
+  CModel& model;
+  CDomain& domain;
+  CIterativeSolver& solver;
+  CDiscretization& discretization;
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_GLOBAL_FIXTURE( burgers2d_global_fixture )
+
+BOOST_AUTO_TEST_SUITE( burgers2d_test_suite )
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( check_tree , burgers2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  boost::shared_ptr<XmlDoc> doc = XmlOps::create_doc();
+  XmlNode& tree_node  = *XmlOps::goto_doc_node(*doc.get());
+
+  Core::instance().root()->list_tree(tree_node);
+
+//  CFinfo << model.tree() << CFendl;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( read_mesh , burgers2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  // create the xml parameters for the read mesh signal
+
+  boost::shared_ptr<XmlDoc> doc = XmlOps::create_doc();
+  XmlNode& node  = *XmlOps::goto_doc_node(*doc.get());
+  XmlParams xmlp (node);
+
+  BOOST_CHECK(true);
+
+  std::vector<URI> files;
+
+//  files.push_back( "file:square1x1-tg-p1.msh" );
+  files.push_back( "file:square1x1-tg-p2.msh" );
+//  files.push_back( "file:rotation-tg-p1.neu" );
+//  files.push_back( "file:rotation-qd-p1.neu" );
+//  files.push_back( "file:advection-tg-p2.msh" );
+//  files.push_back( "file:advection-qd-p2.msh" );
+//  files.push_back( "file:rotation-tg-p3.msh" );
+
+  xmlp.add_option<URI>("Parent Component", URI( domain.full_path().string()) );
+  xmlp.add_array("Files", files);
+
+  // get the generic mesh loader from the Tools
+
+  LoadMesh::Ptr load_mesh = Core::instance().root()->get_child("Tools")->get_child<LoadMesh>("LoadMesh");
+  cf_assert( is_not_null(load_mesh) );
+
+  load_mesh->signal_load_mesh( node );
+
+  BOOST_CHECK_NE( domain.get_child_count(), (Uint) 0);
+
+#ifdef BUBBLE // enrich the mesh with bubble functions
+  CMeshTransformer::Ptr enricher =
+      create_component_abstract_type<CMeshTransformer>("CF.Mesh.Actions.CBubbleEnrich","enricher");
+
+  domain.add_component( enricher );
+
+  CMesh::Ptr mesh = find_component_ptr<CMesh>(domain);
+
+  enricher->transform( mesh );
+#endif
+
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( setup_iterative_solver , burgers2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  solver.configure_property("Domain",URI("cpath:../Domain"));
+  solver.configure_property("Number of Iterations", 1500u);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( create_boundary_term , burgers2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  boost::shared_ptr<XmlDoc> doc = XmlOps::create_doc();
+  XmlNode& node  = *XmlOps::goto_doc_node(*doc.get());
+  XmlParams p(node);
+
+  std::vector<URI> bc_regions;
+  boost_foreach( const CRegion& region, find_components_recursively_with_name<CRegion>(domain,"inlet"))
+    bc_regions.push_back( region.full_path() );
+  boost_foreach( const CRegion& region, find_components_recursively_with_name<CRegion>(domain,"left"))
+    bc_regions.push_back( region.full_path() );
+  boost_foreach( const CRegion& region, find_components_recursively_with_name<CRegion>(domain,"right"))
+    bc_regions.push_back( region.full_path() );
+
+  BOOST_CHECK_EQUAL( bc_regions.size() , 3u);
+
+
+  std::string name = "INLET";
+
+  p.add_option<std::string>("Name",name);
+  p.add_option<std::string>("Type","CF.RDM.BcDirichlet");
+  p.add_array("Regions", bc_regions);
+
+  discretization.as_type<ResidualDistribution>()->create_boundary_term(node);
+
+  Component::Ptr inletbc = find_component_ptr_recursively_with_name( discretization, name );
+  cf_assert( is_not_null(inletbc) );
+
+  inletbc->get_child("action")->configure_property("Function", std::string("1.5-2.0*x") );
+
+//  CFinfo << find_component_recursively<CModel>(*Core::instance().root()).tree() << CFendl;
+
+  BOOST_CHECK(true);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( create_domain_term , burgers2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  CMesh::Ptr mesh = find_component_ptr<CMesh>(domain);
+
+  boost::shared_ptr<XmlDoc> doc = XmlOps::create_doc();
+  XmlNode& node  = *XmlOps::goto_doc_node(*doc.get());
+  XmlParams p(node);
+
+  std::vector<URI> bc_regions;
+  boost_foreach( const CRegion& region, find_components_recursively_with_name<CRegion>(*mesh,"topology"))
+    bc_regions.push_back( region.full_path() );
+
+  BOOST_CHECK_EQUAL( bc_regions.size() , 1u);
+
+  p.add_option<std::string>("Name","INTERNAL");
+  p.add_option<std::string>("Type","CF.RDM.CLDA<Burgers2D>");
+  p.add_array("Regions", bc_regions);
+
+  discretization.as_type<ResidualDistribution>()->create_domain_term(node);
+
+//  CFinfo << find_component_recursively<CModel>(*Core::instance().root()).tree() << CFendl;
+
+  BOOST_CHECK(true);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( solve , burgers2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+//  CFinfo << model.tree() << CFendl;
+
+  solver.solve();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( output , burgers2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  CMesh::Ptr mesh = find_component_ptr<CMesh>(domain);
+
+#ifdef BUBBLE // remove the bubble functions from the mesh
+  CMeshTransformer::Ptr remover =
+      create_component_abstract_type<CMeshTransformer>("CF.Mesh.Actions.CBubbleRemove","remover");
+
+  domain.add_component( remover );
+  remover->transform( mesh );
+#endif
+
+  BOOST_CHECK(true);
+
+  CMeshWriter::Ptr mesh_writer = create_component_abstract_type<CMeshWriter> ( "CF.Mesh.Gmsh.CWriter", "GmshWriter" );
+  model.add_component(mesh_writer);
+
+  std::vector<URI> fields;
+  boost_foreach(const CField2& field, find_components_recursively<CField2>(*mesh))
+    fields.push_back(field.full_path());
+
+  mesh_writer->configure_property("Fields",fields);
+  mesh_writer->configure_property("File",model.name()+".msh");
+  mesh_writer->configure_property("Mesh",mesh->full_path());
+
+  mesh_writer->write();
+
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_SUITE_END()
+
+////////////////////////////////////////////////////////////////////////////////
+
