f20669adf3ebdc922b9327fee804f35384870b8b
wdeconinck <ir.willem.deconinck@gmail.com>
2011-03-04 12:00:11 +0000

    [FIX] unhandled exception in create_component_abstract_type
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2233 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Common/CreateComponent.hpp
+++ src/Common/CreateComponent.hpp
@@ -23,20 +23,19 @@ namespace Common {
 template < typename ATYPE >
     typename ATYPE::Ptr create_component_abstract_type ( const std::string& builder_name, const std::string& name )
 {
-  CFactories::Ptr factories = Core::instance().root()->get_child_ptr("Factories")->as_ptr< CFactories >();
+  Component::Ptr factories = Core::instance().root()->get_child_ptr("Factories");
   if ( is_null(factories) ) throw ValueNotFound( FromHere(), "CFactories \'Factories\' not found in " + Core::instance().root()->full_path().string() );
 
-  Component::Ptr pfac = factories->get_child_ptr( ATYPE::type_name() );
-  CFactory::Ptr factory = pfac->as_ptr< CFactory >();
+  Component::Ptr factory = factories->get_child_ptr( ATYPE::type_name() );
   if ( is_null(factory) ) throw ValueNotFound( FromHere(), "CFactory \'" + ATYPE::type_name() + "\' not found in " + factories->full_path().string() + ". Probably forgot to load a library." );
 
-  CBuilder::Ptr builder = factory->get_child_ptr( builder_name )->as_ptr< CBuilder >();
+  Component::Ptr builder = factory->get_child_ptr( builder_name );
   if ( is_null(builder) ) throw ValueNotFound( FromHere(), "CBuilder \'" + builder_name + "\' not found in factory \'" + ATYPE::type_name() + "\'. Probably forgot to load a library." );
 
-  Component::Ptr comp = builder->build ( name );
+  Component::Ptr comp = builder->as_type<CBuilder>().build ( name );
   if ( is_null(comp) ) throw NotEnoughMemory ( FromHere(), "CBuilder \'" + builder_name + "\' failed to allocate component with name \'" + name + "\'" );
 
-  typename ATYPE::Ptr ccomp = boost::dynamic_pointer_cast<ATYPE>( comp );
+  typename ATYPE::Ptr ccomp = comp->as_ptr<ATYPE>();
   if ( is_null(ccomp) ) throw CastingFailed ( FromHere(), "Pointer created by CBuilder \'" + builder_name + "\' could not be casted to \'" + ATYPE::type_name() + "\' pointer" );
 
   return ccomp;
