c711099cb3cf54128916a56c9af76062b94d547b
tlmquintino <tlmquintino@gmail.com>
2011-03-05 19:24:35 +0000

    [ENH] made LDA physics independent - chosen at runtime
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2243 4892e656-8fb9-448a-b5bc-b1919152596f

--- plugins/RDM/src/RDM/LDA.cpp
+++ plugins/RDM/src/RDM/LDA.cpp
@@ -4,76 +4,111 @@
 // GNU Lesser General Public License version 3 (LGPLv3).
 // See doc/lgpl.txt and doc/gpl.txt for the license text.
 
+#include <boost/mpl/for_each.hpp>
+
 #include "Common/CBuilder.hpp"
+
+#include "Common/Foreach.hpp"
+#include "Common/ComponentPredicates.hpp"
 
 #include "RDM/LDA.hpp"
 
-// supported physics
+#include "RDM/SupportedTypes.hpp"    // supported elements
 
-#include "RDM/LinearAdv2D.hpp"
-#include "RDM/RotationAdv2D.hpp"
-#include "RDM/Burgers2D.hpp"
+#include "RDM/LinearAdv2D.hpp"       // supported physics
+#include "RDM/RotationAdv2D.hpp"     // supported physics
+#include "RDM/Burgers2D.hpp"         // supported physics
+
+#include "RDM/CSchemeLDAT.hpp"
 
 using namespace CF::Common;
+using namespace CF::Mesh;
 
 namespace CF {
 namespace RDM {
 
-Common::ComponentBuilder < LDA<LinearAdv2D>,   CAction, LibRDM > LDA_LinearAdv2D_Builder;
-Common::ComponentBuilder < LDA<RotationAdv2D>, CAction, LibRDM > LDA_RotationAdv2D_Builder;
-Common::ComponentBuilder < LDA<Burgers2D>,     CAction, LibRDM > LDA_Burgers2D_Builder;
+////////////////////////////////////////////////////////////////////////////////
+
+Common::ComponentBuilder < LDA, CAction, LibRDM > LDA_Builder;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-template < typename PHYS >
-LDA<PHYS>::LDA ( const std::string& name  ): RDM::Action(name)
+/// Looper defines a functor taking the type that boost::mpl::for_each passes.
+/// It is the core of the looping mechanism.
+template < typename PHYS>
+struct ElementLoop
+{
+  /// region to loop on
+  Mesh::CRegion& region;
+  /// component containing the element loop
+  LDA& comp;
+  /// Constructor
+  ElementLoop( LDA& comp_in, Mesh::CRegion& region_in ) : comp(comp_in), region(region_in) {}
+  /// operator needed for the loop over element types (SF)
+  template < typename SF >
+  void operator() ( SF& T )
+  {
+    /// definition of the quadrature type
+    typedef typename RDM::DefaultQuadrature<SF>::type QD;
+    /// parametrization of the numerical scheme
+    typedef CSchemeLDAT< SF, QD, PHYS > SchemeT;
+
+    boost_foreach(Mesh::CElements& elements,
+                  Common::find_components_recursively_with_filter<Mesh::CElements>(region,IsElementType<SF>()))
+    {
+      // get the scheme or create it if does not exist
+      Component::Ptr cscheme = comp.get_child_ptr( SchemeT::type_name() );
+      typename SchemeT::Ptr scheme;
+      if( is_null( cscheme ) )
+        scheme = comp.create_component< SchemeT >( SchemeT::type_name() );
+      else
+        scheme = cscheme->as_ptr_checked<SchemeT>();
+
+      // loop on elements of that type
+      scheme->set_elements(elements);
+
+      const Uint nb_elem = elements.size();
+      for ( Uint elem = 0; elem != nb_elem; ++elem )
+      {
+        scheme->select_loop_idx(elem);
+        scheme->execute();
+      }
+    }
+  }
+
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+LDA::LDA ( const std::string& name  ): RDM::Action(name)
 {
   regist_typeinfo(this);
 }
 
-template < typename PHYS >
-LDA<PHYS>::~LDA() {}
+LDA::~LDA() {}
 
-template < typename PHYS >
-void LDA<PHYS>::execute()
+void LDA::execute()
 {
   boost_foreach(Mesh::CRegion::Ptr& region, m_loop_regions)
   {
-    ElementLoop loop( *this, *region );
+    std::string physics;
 
-    boost::mpl::for_each< RDM::CellTypes >( loop );
-  }
-}
-
-//////////////////////////////////////////////////////////////////////////////
-
-/// Operator needed for the loop over element types, identified by shape functions (SF)
-template < typename PHYS>
-template < typename SF >
-void LDA<PHYS>::ElementLoop::operator() ( SF& T )
-{
-  typedef typename RDM::DefaultQuadrature<SF>::type QD; // create a quadrature for this specific type
-  typedef CSchemeLDAT< SF, QD, PHYS > SchemeT;          // create a scheme for this specific type
-
-  boost_foreach(Mesh::CElements& elements,
-                Common::find_components_recursively_with_filter<Mesh::CElements>(region,IsElementType<SF>()))
-  {
-    // get the scheme
-    Component::Ptr cscheme = comp.get_child_ptr( SchemeT::type_name() );
-    typename SchemeT::Ptr scheme;
-    if( is_null( cscheme ) )
-      scheme = comp.create_component< SchemeT >( SchemeT::type_name() );
-    else
-      scheme = cscheme->as_ptr_checked<SchemeT>();
-
-    // loop on elements of that type
-    scheme->set_elements(elements);
-
-    const Uint nb_elem = elements.size();
-    for ( Uint elem = 0; elem != nb_elem; ++elem )
+    if ( physics == "LinearAdv2D" )
     {
-      scheme->select_loop_idx(elem);
-      scheme->execute();
+      ElementLoop<LinearAdv2D> loop( *this, *region );
+      boost::mpl::for_each< RDM::CellTypes >( loop );
+    }
+
+    if ( physics == "RotationAdv2D" )
+    {
+      ElementLoop<RotationAdv2D> loop( *this, *region );
+      boost::mpl::for_each< RDM::CellTypes >( loop );
+    }
+
+    if ( physics == "Burgers2D" )
+    {
+      ElementLoop<Burgers2D> loop( *this, *region );
+      boost::mpl::for_each< RDM::CellTypes >( loop );
     }
   }
 }
--- plugins/RDM/src/RDM/LDA.hpp
+++ plugins/RDM/src/RDM/LDA.hpp
@@ -7,19 +7,10 @@
 #ifndef CF_RDM_LDA_hpp
 #define CF_RDM_LDA_hpp
 
-#include <boost/mpl/for_each.hpp>
-
-#include "Common/Foreach.hpp"
-#include "Common/ComponentPredicates.hpp"
-
 #include "Mesh/CRegion.hpp"
 
 #include "RDM/LibRDM.hpp"
 #include "RDM/Action.hpp"
-#include "RDM/SupportedTypes.hpp"
-
-#include "RDM/CSchemeLDAT.hpp"
-//#include "RDM/CSchemeN.hpp"
 
 /////////////////////////////////////////////////////////////////////////////////////
 
@@ -28,30 +19,8 @@ namespace RDM {
 
 /////////////////////////////////////////////////////////////////////////////////////
 
-template < typename PHYS >
 class RDM_API LDA : public RDM::Action
 {
-
-  /// Looper defines a functor taking the type that boost::mpl::for_each
-  /// passes. It is the core of the looping mechanism.
-  struct ElementLoop
-  {
-    /// region to loop on
-    Mesh::CRegion& region;
-    /// component containing the element loop
-    LDA& comp;
-
-    public: // functions
-
-      /// Constructor
-      ElementLoop( LDA& comp_in, Mesh::CRegion& region_in ) : comp(comp_in), region(region_in) {}
-
-      /// Operator needed for the loop over element types, identified by shape functions (SF)
-      template < typename SF >
-      void operator() ( SF& T );
-
-
-  }; // ElementLoop
 
 public: // typedefs
 
@@ -68,7 +37,7 @@ public: // functions
   virtual ~LDA();
 
   /// Get the class name
-  static std::string type_name () { return "LDA<" + PHYS::type_name() + ">"; }
+  static std::string type_name () { return "LDA"; }
 
   /// Execute the loop for all elements
   virtual void execute();
