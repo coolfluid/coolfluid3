17dba7da3ab0330cb9fc1086ac4e0248e98cda41
tlmquintino <tlmquintino@gmail.com>
2011-03-08 22:46:48 +0000

    [FIX] corrected setting of norm
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2282 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Solver/Actions/CComputeLNorm.cpp
+++ src/Solver/Actions/CComputeLNorm.cpp
@@ -6,6 +6,8 @@
 
 #include <cmath>
 
+#include "Common/Log.hpp"
+
 #include "Common/CBuilder.hpp"
 #include "Common/OptionT.hpp"
 #include "Common/OptionComponent.hpp"
@@ -42,7 +44,7 @@ CComputeLNorm::CComputeLNorm ( const std::string& name ) : CAction(name)
   // options
 
   m_properties.add_option< OptionT<bool> >("Scale", "Scales (divides) the norm by the number of entries (ignored if order zero)", true);
-  m_properties.add_option< OptionT<Uint> >("Order", "Order of the p-norm, zero if L-inf", 2);
+  m_properties.add_option< OptionT<Uint> >("Order", "Order of the p-norm, zero if L-inf", 2u);
 
   m_properties.add_option(OptionComponent<CField2>::create("Field", "Field for which to compute the norm", &m_field));
 }
@@ -53,40 +55,42 @@ void CComputeLNorm::execute()
 {
   if (m_field.expired()) throw SetupError(FromHere(), "Field was not set");
 
+  CTable<Real>& table = m_field.lock()->data();
+  CTable<Real>::ArrayT& array =  table.array();
 
-  const Uint nbrows = m_field.lock()->data().size();
+  const Uint nbrows = table.size();
 
   if ( !nbrows ) throw SetupError(FromHere(), "Field has empty table");
-
 
   Real norm = 0.;
 
   const Uint order = m_properties.option("Order").value<Uint>();
   switch(order)
   {
-  case 1: // L1
-    boost_foreach(CTable<Real>::ConstRow row, m_field.lock()->data().array())
-        norm += std::abs( row[0] );
-    break;
   case 2: // L2
-    boost_foreach(CTable<Real>::ConstRow row, m_field.lock()->data().array())
+    boost_foreach(CTable<Real>::ConstRow row, array )
         norm += row[0]*row[0];
     norm = std::sqrt(norm);
     break;
+  case 1: // L1
+    boost_foreach(CTable<Real>::ConstRow row, array )
+        norm += std::abs( row[0] );
+    break;
   case 0: // treat as Linf
-    boost_foreach(CTable<Real>::ConstRow row, m_field.lock()->data().array())
+    boost_foreach(CTable<Real>::ConstRow row, array )
         norm += std::max( std::abs(row[0]), norm );
     break;
   default: // Lp
-    boost_foreach(CTable<Real>::ConstRow row, m_field.lock()->data().array())
+    boost_foreach(CTable<Real>::ConstRow row, array )
         norm += std::abs( pow(row[0], order) );
     norm = std::pow(norm, 1./order );
+    break;
   }
 
   if( m_properties.option("Scale").value<bool>() && order )
     norm /= nbrows;
 
-  properties().property("Norm").put_value<Real>(norm);
+  properties().property("Norm").change_value(norm);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
