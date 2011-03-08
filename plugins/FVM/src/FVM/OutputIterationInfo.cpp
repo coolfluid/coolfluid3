6c6fb1c98b9db84922966fd5d58f25b963051252
tlmquintino <tlmquintino@gmail.com>
2011-03-08 22:46:31 +0000

    [FIX] miss header
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2279 4892e656-8fb9-448a-b5bc-b1919152596f

--- plugins/FVM/src/FVM/OutputIterationInfo.cpp
+++ plugins/FVM/src/FVM/OutputIterationInfo.cpp
@@ -5,6 +5,7 @@
 // See doc/lgpl.txt and doc/gpl.txt for the license text.
 #include <iomanip>
 
+#include "Common/Log.hpp"
 #include "Common/CBuilder.hpp"
 #include "Common/OptionComponent.hpp"
 #include "Common/Foreach.hpp"
