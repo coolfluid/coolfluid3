cf08e6756ab450df004c06eb50c5b292148bc748
gquentin <gquentin@gmail.com>
2011-03-10 09:23:58 +0000

    [ENH] sandbox for boost::dynamic_pointer_cast issue
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2299 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ sandbox/boost-shared_ptr/boost-shared_ptr.cpp
@@ -0,0 +1,48 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+
+#include "Common/OptionT.hpp"
+#include "Common/OptionURI.hpp"
+#include "Common/OptionArray.hpp"
+#include "Common/Log.hpp"
+
+using namespace CF::Common;
+
+int main ( int argc, char * argv[])
+{
+   Option * option = new OptionArrayT<bool>("bla", "", std::vector<bool>());
+
+   CFinfo << "pointer\t\t" << option << " " <<
+       static_cast< OptionArray* >(option) << " " <<
+       static_cast< OptionArrayT<bool>* >(option) << CFendl;
+
+   std::vector<bool> vect;
+   Option::Ptr opt(new OptionArrayT<bool>("bla", "", vect));
+
+   CFinfo << "OptionArrayT\t" << opt.get() << " " <<
+       boost::dynamic_pointer_cast<OptionArray>(opt).get() << " " <<
+       boost::dynamic_pointer_cast<OptionArrayT<bool> >(opt).get() << CFendl;
+
+   Property::Ptr optT(new OptionT<bool>("bla", "", true));
+
+   CFinfo << "OptionT\t\t" << optT.get() << " " <<
+       boost::dynamic_pointer_cast<Option>(optT).get() << " " <<
+       boost::dynamic_pointer_cast<OptionT<bool> >(optT).get() << CFendl;
+
+   Property::Ptr optURI(new OptionURI("bla", "", URI()));
+
+   CFinfo << "OptionURI\t" << optURI.get() << " " <<
+       boost::dynamic_pointer_cast<Option>(optURI).get() << " " <<
+       boost::dynamic_pointer_cast<OptionURI >(optURI).get() << CFendl;
+
+//   Component::Ptr cmp(new NLink("link"));
+
+//   CFinfo << "NLink " << cmp.get() << " " << boost::dynamic_pointer_cast<CNode>(cmp).get() << " " << boost::dynamic_pointer_cast<NLink>(cmp).get() << CFendl;
+
+   return 0;
+}
+
