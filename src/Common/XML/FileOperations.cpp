e53eaa96ff253b2a1bc8ffdf07bc7c74e93e3755
tlmquintino <tlmquintino@gmail.com>
2011-03-12 21:53:26 +0000

    [FIX] forgot files
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2324 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ src/Common/XML/FileOperations.cpp
@@ -0,0 +1,100 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include <cstdio>
+#include <fstream>
+
+#include "rapidxml/rapidxml_print.hpp" // includes rapidxml/rapidxml.hpp
+
+#include "Common/CodeLocation.hpp"
+#include "Common/BasicExceptions.hpp"
+
+#include "Common/XML/FileOperations.hpp"
+
+/////////////////////////////////////////////////////////////////////////////
+
+namespace CF {
+namespace Common {
+namespace XML {
+
+/////////////////////////////////////////////////////////////////////////////
+
+XmlDoc::Ptr parse_string ( const std::string& str )
+{
+  using namespace rapidxml;
+
+  xml_document<>* xmldoc = new xml_document<>();
+
+  char* ctext = xmldoc->allocate_string(str.c_str());
+
+  // parser trims and merges whitespaces
+  xmldoc->parse< parse_no_data_nodes |
+                 parse_trim_whitespace /*|
+                 parse_normalize_whitespace*/ >(ctext);
+
+  return boost::shared_ptr<XmlDoc>( new XmlDoc(xmldoc) );
+}
+
+
+XmlDoc::Ptr parse_file ( const boost::filesystem::path& path )
+{
+  using namespace rapidxml;
+
+  xml_document<>* xmldoc = new xml_document<>();
+
+  std::string filepath = path.string();
+  FILE *filep = fopen( filepath.c_str(), "rb" );
+
+  if (filep == NULL)
+    throw FileSystemError(FromHere(), "Unable to open file [" + filepath + "]" );
+
+  fseek(filep,0,SEEK_END);                  // go to end
+  Uint length = ftell(filep);               // get position at end (length)
+
+  if (!length)
+    throw FileSystemError(FromHere(), "File [" + filepath + "] is empty" );
+
+  fseek(filep, 0, SEEK_SET);                  // go to beginning
+
+  char* buffer = xmldoc->allocate_string( 0, length );  // allocate buffer directly inside the xmldoc
+
+  size_t rs = fread(buffer,length, 1, filep);           // read into buffer
+  if (!rs)
+    throw FileSystemError(FromHere(), "Error while reading file [" + filepath + "]" );
+
+  fclose(filep);                             // close file
+
+  // parser trims and merges whitespaces
+  xmldoc->parse< parse_no_data_nodes |
+                 parse_trim_whitespace /*|
+                 parse_normalize_whitespace*/ >(buffer);
+
+  return boost::shared_ptr<XmlDoc>( new XmlDoc(xmldoc) );
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+
+void to_file ( const XmlNode& node, const boost::filesystem::path& fpath )
+{
+  std::ofstream fout ( fpath.string().c_str() );
+
+  std::string xml_as_string;
+
+  XML::to_string( node, xml_as_string );
+
+  fout << xml_as_string << std::endl;
+}
+
+void to_string ( const XmlNode& node, std::string& str )
+{
+  rapidxml::print(std::back_inserter(str), *node.content);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+} // XML
+} // Common
+} // CF
---
+++ src/Common/XML/FileOperations.hpp
@@ -0,0 +1,49 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#ifndef CF_Common_XML_FileOperations_hpp
+#define CF_Common_XML_FileOperations_hpp
+
+////////////////////////////////////////////////////////////////////////////
+
+#include "Common/BoostFilesystem.hpp"
+
+#include "Common/XML/XmlDoc.hpp"
+
+/////////////////////////////////////////////////////////////////////////////
+
+namespace CF {
+namespace Common {
+namespace XML {
+
+/// Parses a XML string
+/// @param str String with the XML contents
+/// @return Returns a shared pointer with the built XML document.
+XmlDoc::Ptr parse_string ( const std::string& str );
+
+/// Parses a XML file
+/// @param fpath Path to file with xml contents
+/// @return Returns a shared pointer with the built XML document.
+/// @throw FileSystemError If the file cannot be read.
+XmlDoc::Ptr parse_file ( const boost::filesystem::path& path );
+
+/// Writes the provided XML node to a file.
+/// @param node The node to write.
+/// @param fpath The file path to which the node has to be written.
+void to_file ( const XmlNode& node, const boost::filesystem::path& fpath);
+
+/// Writes the provided XML node to a string.
+/// @param str The string to which the node has to be written.
+/// @param node The node to write.
+void to_string ( const XmlNode& node, std::string& str );
+
+} // XML
+} // Common
+} // CF
+
+////////////////////////////////////////////////////////////////////////////
+
+#endif // CF_Common_XML_FileOperations_hpp
