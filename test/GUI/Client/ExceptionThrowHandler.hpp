12a20e9061570bdf0a424134a4c3fd278574d959
gquentin <gquentin@gmail.com>
2011-03-16 10:57:23 +0000

    [FIX] improved exception handling macros. The test function now returns on failure.
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2396 4892e656-8fb9-448a-b5bc-b1919152596f

--- test/GUI/Client/ExceptionThrowHandler.hpp
+++ test/GUI/Client/ExceptionThrowHandler.hpp
@@ -20,15 +20,18 @@ catch ( CF::Common::Exception ex) \
 { \
   QFAIL(QString(#the_exception " expected but another CF exception was "\
                 "thrown.\n%1").arg(ex.what()).toStdString().c_str()); \
+  return;\
 } \
 catch ( std::exception stde) \
 { \
   QFAIL(QString(#the_exception " expected but a std exception was "\
                 "thrown.\n%1").arg(stde.what()).toStdString().c_str()); \
+  return;\
 } \
 catch ( ... ) \
 { \
   QFAIL(#the_exception " expected but another one was thrown."); \
+  return;\
 }
 
 // Checks whether a code never throws any exception.
@@ -41,15 +44,18 @@ catch ( CF::Common::Exception ex) \
 { \
   QFAIL(QString("No exception expected but a CF exception was "\
                 "thrown.\n%1").arg(ex.what()).toStdString().c_str()); \
+  return;\
 } \
 catch ( std::exception stde) \
 { \
   QFAIL(QString("No exception expected but a std exception was "\
                 "thrown.\n%1").arg(stde.what()).toStdString().c_str()); \
+  return;\
 } \
 catch ( ... ) \
 { \
   QFAIL("No exception expected but an unknown exception was thrown."); \
+  return;\
 }
 
 #endif // CF_GUI_Client_uTests_ExceptionThrowHandler_hpp
