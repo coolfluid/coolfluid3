6aac2b1b1440fac67bd90e458143295a4adf9fea
gquentin <gquentin@gmail.com>
2011-03-25 14:26:55 +0000

    [ENH] added a sandbox application to test that ParaView is working well with CF
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2536 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ sandbox/paraview-deps/paraview-deps.cpp
@@ -0,0 +1,23 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include <QApplication>
+
+#include <pqApplicationCore.h>
+#include <pqTreeView.h>
+
+int main(int argc, char * argv[])
+{
+  QApplication app(argc, argv);
+
+  pqApplicationCore appCore(argc, argv);
+
+  pqTreeView * tv = new pqTreeView();
+
+  tv->show();
+
+  return app.exec();
+}
