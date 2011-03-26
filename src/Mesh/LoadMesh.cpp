53171744b7799ebd4ab0a541399182b0936fa6de
wdeconinck <ir.willem.deconinck@gmail.com>
2011-03-26 19:18:45 +0000

    [ENH] signal LoadMesh::load_mesh takes MeshName
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2552 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Mesh/LoadMesh.cpp
+++ src/Mesh/LoadMesh.cpp
@@ -154,7 +154,7 @@ void LoadMesh::signal_load_mesh ( Common::SignalArgs& node )
   // create a mesh in the domain
   if( !files.empty() )
   {
-    CMesh::Ptr mesh = parent_component->create_component<CMesh>("Mesh");
+    CMesh::Ptr mesh = parent_component->create_component<CMesh>(options.get_option<std::string>("MeshName"));
 
     // Get the file paths
     boost_foreach(URI file, files)
@@ -210,6 +210,8 @@ void LoadMesh::signature_load_mesh ( Common::SignalArgs& node)
   }
 
   options.set_option<URI>("Parent Component", URI(), "Path to the component to hold the mesh" );
+
+  options.set_option<std::string>("MeshName", std::string("mesh"), "Name of the mesh" );
 
   // create de value and add the restricted list
   XmlNode rdrs_node = options.set_option( "Readers", std::string() , "Available readers" );
