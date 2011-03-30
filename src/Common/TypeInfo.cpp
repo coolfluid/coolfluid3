979cf18d3a1c292b6901dc7bcacd295c1aa6613f
tlmquintino <tlmquintino@gmail.com>
2011-03-30 12:57:08 +0000

    [FIX] miss header
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2609 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Common/TypeInfo.cpp
+++ src/Common/TypeInfo.cpp
@@ -4,6 +4,8 @@
 // GNU Lesser General Public License version 3 (LGPLv3).
 // See doc/lgpl.txt and doc/gpl.txt for the license text.
 
+#include <cstdlib>
+
 #include "Common/TypeInfo.hpp"
 #include "Common/URI.hpp"
 
