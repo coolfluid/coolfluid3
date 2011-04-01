9046d041b0922064c799ef9863f78addd5c02840
tlmquintino <tlmquintino@gmail.com>
2011-04-01 21:00:09 +0000

    [FIX] linking error due to missing assertion function
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2625 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Common/BoostAssertions.cpp
+++ src/Common/BoostAssertions.cpp
@@ -5,6 +5,7 @@
 // See doc/lgpl.txt and doc/gpl.txt for the license text.
 
 #include "Common/BoostAssertions.hpp"
+#include "Common/Assertions.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
 
