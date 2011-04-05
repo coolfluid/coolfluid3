38e45762dec83762a02d8ec3dac80f3a945efcb1
wdeconinck <ir.willem.deconinck@gmail.com>
2011-04-05 12:29:26 +0000

    [FIX] MacOSX Bundle. coolfluid-server starts in separate window and can be manually closed
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2675 4892e656-8fb9-448a-b5bc-b1919152596f

--- tools/MacOSX_Bundle/start_coolfluid.sh
+++ tools/MacOSX_Bundle/start_coolfluid.sh
@@ -14,8 +14,42 @@ echo "CF3_RESOURCES: $CF3_RESOURCES"
 export "DYLD_LIBRARY_PATH=$CF3_RESOURCES/coolfluid-CF_VERSION/lib"
 export "PATH=$CF3_RESOURCES/coolfluid-CF_VERSION/bin:$PATH"
 
-#export
-$CF3_RESOURCES/coolfluid-CF_VERSION/bin/coolfluid-server &
-server_pid=$!
-$CF3_RESOURCES/coolfluid-CF_VERSION/bin/coolfluid-client
-kill $server_pid
+
+SERVICE='coolfluid-server'
+
+if ps ax | grep -v grep | grep $SERVICE > /dev/null
+then
+    echo "$SERVICE service running, everything is fine"
+else
+    echo "$SERVICE is not running"
+    START_SERVER="Yes" #`osascript -e 'tell app "Finder" to return button returned of (display dialog "Start server?" buttons {"No","Yes"} default button 2)'`
+    if [ "$START_SERVER" == "Yes" ]; then
+        echo "Starting server"
+        echo  "export COOLFLUID_PLUGINS=$CF3_RESOURCES/CF_VERSION/lib/libcoolfluid_fvm.dylib:$CF3_RESOURCES/coolfluid-CF_VERSION/lib/libcoolfluid_rdm.dylib" > ~/.start_coolfluid-server.command
+        echo  "export LD_LIBRARY_PATH=$CF3_RESOURCES/coolfluid-CF_VERSION/lib" >> ~/.start_coolfluid-server.command
+        echo  "export DYLD_LIBRARY_PATH=$CF3_RESOURCES/coolfluid-CF_VERSION/lib" >> ~/.start_coolfluid-server.command
+        echo  "export PATH=$CF3_RESOURCES/coolfluid-CF_VERSION/bin:$PATH" >> ~/.start_coolfluid-server.command
+        echo  "$CF3_RESOURCES/coolfluid-CF_VERSION/bin/coolfluid-server" >> ~/.start_coolfluid-server.command
+        chmod 755 ~/.start_coolfluid-server.command
+        open -a Terminal ~/.start_coolfluid-server.command
+    else
+        echo "Not starting server"
+    fi
+fi &
+
+$CF3_RESOURCES/coolfluid-CF_VERSION/bin/coolfluid-gui
+
+# if ps ax | grep -v grep | grep $SERVICE > /dev/null
+# then
+#     echo "$SERVICE service running, everything is maybe not fine"
+#     KILL_SERVER=`osascript -e 'tell app "Finder" to return button returned of (display dialog "Quit server?" buttons {"Yes","No"} default button 2)'`
+#     if [ "$KILL_SERVER" == "Yes" ]; then
+#         echo "Killing server"
+#         killall coolfluid-server
+#     else
+#         echo "Not killing server"
+#     fi
+#     
+# else
+#     echo "$SERVICE is not running"
+# fi
