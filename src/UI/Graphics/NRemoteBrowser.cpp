00ebb35536a950d9e71b9c6b24a7362829eb482e
wertz <USER@DOMAIN>
2011-04-27 10:56:51 +0000

    [FIX] bad connector used
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2803 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/UI/Graphics/NRemoteBrowser.cpp
+++ src/UI/Graphics/NRemoteBrowser.cpp
@@ -242,8 +242,8 @@ QStringList NRemoteBrowser::showMultipleSelect(const QString & startingDir)
   m_listView->setSelectionMode(QAbstractItemView::ExtendedSelection);
   m_listView->clearSelection();
 
-  connect(NLog::globalLog().get(), SIGNAL(newMessage(const QString &, bool)),
-          this, SLOT(error(const QString &, bool)));
+  connect(NLog::globalLog().get(), SIGNAL(newMessage(QString,CF::UI::UICommon::LogMessage::Type)),
+          this, SLOT(message(QString,CF::UI::UICommon::LogMessage::Type)));
 
   this->reinitValues();
 
