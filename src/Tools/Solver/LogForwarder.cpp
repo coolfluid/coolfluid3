551667d9a5ea2d6145b0a87b6d3fc828fb2f0e51
gquentin <gquentin@gmail.com>
2011-05-15 00:58:06 +0000

    [ENH] workers log is now redirected to the manager
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2918 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ src/Tools/Solver/LogForwarder.cpp
@@ -0,0 +1,51 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include "Common/Core.hpp"
+#include "Common/CRoot.hpp"
+
+#include "Common/XML/SignalOptions.hpp"
+
+#include "Tools/Solver/LogForwarder.hpp"
+
+#include "Common/Log.hpp"
+
+using namespace CF::Common;
+using namespace CF::Common::mpi;
+using namespace CF::Common::XML;
+
+////////////////////////////////////////////////////////////////////////////
+
+namespace CF {
+namespace Tools {
+namespace Solver {
+
+////////////////////////////////////////////////////////////////////////////
+
+LogForwarder::LogForwarder()
+{
+  m_manager = Core::instance().root().access_component_ptr_checked("//Root/Tools/PEManager")->as_ptr<CPEManager>();
+}
+
+////////////////////////////////////////////////////////////////////////////
+
+void LogForwarder::message(const std::string &data)
+{
+  std::string path = m_manager->full_path().string();
+  SignalFrame frame("message", path, path);
+  SignalOptions options(frame);
+
+  options.add<std::string>("message", data);
+//  frame.set_option<std::string>("message", data);
+
+  m_manager->send_to_parent( frame );
+}
+
+////////////////////////////////////////////////////////////////////////////
+
+} // Tools
+} // Tools
+} // CF
---
+++ src/Tools/Solver/LogForwarder.hpp
@@ -0,0 +1,48 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#ifndef CF_Tools_Solver_LogForwarder_hpp
+#define CF_Tools_Solver_LogForwarder_hpp
+
+#include "Common/MPI/CPEManager.hpp"
+#include "Common/LogStringForwarder.hpp"
+
+////////////////////////////////////////////////////////////////////////////
+
+namespace CF {
+namespace Tools {
+namespace Solver {
+
+////////////////////////////////////////////////////////////////////////////
+
+  /// Appends LoggingEvents to the remote client log window.
+
+  class LogForwarder : public Common::LogStringForwarder
+  {
+
+  public:
+
+    LogForwarder();
+
+  protected:
+
+    virtual void message(const std::string & data);
+
+  private:
+
+    Common::mpi::CPEManager::Ptr m_manager;
+
+  };
+
+////////////////////////////////////////////////////////////////////////////
+
+} // Tools
+} // Tools
+} // CF
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif // CF_Tools_Solver_LogForwarder_hpp
