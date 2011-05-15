f67e1e1bb70ca21ff5635718ef3b1ec1569cc3cf
gquentin <gquentin@gmail.com>
2011-05-15 00:57:59 +0000

    [FIX] compilation error after rebase
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2914 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Tools/Solver/coolfluid-solver.cpp
+++ src/Tools/Solver/coolfluid-solver.cpp
@@ -4,34 +4,87 @@
 // GNU Lesser General Public License version 3 (LGPLv3).
 // See doc/lgpl.txt and doc/gpl.txt for the license text.
 
-#include <mpi.h>
+#include <boost/program_options/errors.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
 
 #include "Common/Core.hpp"
+#include "Common/CRoot.hpp"
 #include "Common/Log.hpp"
 
+#include "Common/MPI/CPEManager.hpp"
 #include "Common/MPI/PE.hpp"
 #include "Common/MPI/ListeningThread.hpp"
 
+#include "Tools/Solver/LogForwarder.hpp"
+
+using namespace boost;
 using namespace MPI;
+using namespace CF;
 using namespace CF::Common;
 using namespace CF::Common::mpi;
+using namespace CF::Tools::Solver;
 
 int main(int argc, char ** argv)
 {
+  boost::program_options::options_description desc("Allowed options");
+  std::string forward = "none";
+  LogForwarder * forwarder;
+  Communicator parent_comm;
+  int rank;
+
+  desc.add_options()
+      ("forward", program_options::value<std::string>(&forward)->default_value(forward),
+       "Defines whether log should be forwarded to the parent process. Three "
+       "values available: none (no forwarding), rank0 (only rank 0 forwards) or "
+       "all (all ranks forward).");
+
   // initiate the CF core and MPI environment
   Core::instance().initiate(argc, argv);
   PE::instance().init(argc, argv);
-  Logger::instance().getStream( Logger::INFO ).setFilterRankZero(false);
 
-  if( mpi::PE::instance().get_parent() != COMM_NULL )
+
+  parent_comm = PE::instance().get_parent();
+  rank = PE::instance().rank();
+
+  if( parent_comm == MPI_COMM_NULL )
   {
-    CFinfo << "Worker[" << PE::instance().rank() << "] -> I have a parent. " <<
-              "We are " << mpi::PE::instance().size() << " in this universe." << CFendl;
-  }
-  else
-    CFerror << "A worker cannot be run without a manager." << CFendl;
+    CFerror << "This solver cannot run without a manager. Exiting..." << CFendl;
 
+    return 1;
+  }
+
+  // get command line arguments
+//  program_options::variables_map vm;
+//  program_options::store(program_options::parse_command_line(argc, argv, desc), vm);
+//  program_options::notify(vm);
+
+  // create the PE manager
+  CPEManager::Ptr mgr = Core::instance().root().get_child_ptr("Tools")->
+      create_component_ptr<CPEManager>("PEManager");
+
+  mgr->mark_basic();
+
+  // set the forwarder, if needed
+//  if( forward == "all" || (forward == "rank0" && PE::instance().rank() == 0) )
+  {
+    forwarder = new LogForwarder();
+    Logger::instance().getStream(Logger::INFO).addStringForwarder(forwarder);
+  }
+
+  Logger::instance().getStream( Logger::INFO ).setFilterRankZero(false);
+  CFinfo << "Worker[" << rank << "] -> " << "Syncing with the parent..." << CFendl;
+//  PE::instance().get_parent()->barrier();
   PE::instance().barrier();
+  MPI_Barrier( parent_comm );
+  CFinfo << "Worker[" << rank << "] -> " << "Synced with the parent!" << CFendl;
+
+  mgr->listening_thread().join();
+
+//  PE::instance().barrier();
+
+  CFinfo << "Worker[" << rank << "] -> " << "C U..." << CFendl;
 
   // terminate the CF core and MPI environment
   PE::instance().finalize();
