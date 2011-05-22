2a612e6e2b4a2c734b86b5a0af068fdaff1760d6
betak <USER@DOMAIN>
2011-05-22 16:14:56 +0000

    [FIX] GPU scheme for system
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@2983 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ plugins/RDM/test/utest-rdm-euler2d-gpu.cpp
@@ -0,0 +1,334 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "Test module for CF::RDM::Euler2D"
+
+#include <boost/test/unit_test.hpp>
+#include <boost/assign/list_of.hpp>
+
+#include "Common/BoostFilesystem.hpp"
+
+#include "Common/CreateComponent.hpp"
+#include "Common/FindComponents.hpp"
+#include "Common/Log.hpp"
+#include "Common/CLink.hpp"
+#include "Common/Foreach.hpp"
+#include "Common/LibLoader.hpp"
+#include "Common/OSystem.hpp"
+
+#include "Common/XML/SignalOptions.hpp"
+
+#include "Solver/CSolver.hpp"
+#include "Solver/CModel.hpp"
+#include "Solver/CPhysicalModel.hpp"
+#include "Solver/Actions/CLoop.hpp"
+
+#include "Mesh/LoadMesh.hpp"
+#include "Mesh/CCells.hpp"
+#include "Mesh/CMeshReader.hpp"
+#include "Mesh/CMeshWriter.hpp"
+#include "Mesh/CDomain.hpp"
+#include "Mesh/CRegion.hpp"
+#include "Mesh/Actions/CBubbleEnrich.hpp"
+#include "Mesh/Actions/CBubbleRemove.hpp"
+
+#include "RDM/Core/RKRD.hpp"
+#include "RDM/Core/DomainTerm.hpp"
+#include "RDM/Core/SteadyExplicit.hpp"
+
+using namespace CF;
+using namespace CF::Common;
+using namespace CF::Common::XML;
+using namespace CF::Mesh;
+using namespace CF::Solver;
+using namespace CF::Solver::Actions;
+using namespace CF::RDM;
+
+struct euler2d_global_fixture
+{
+  euler2d_global_fixture()
+  {
+    Core::instance().initiate(boost::unit_test::framework::master_test_suite().argc,
+                              boost::unit_test::framework::master_test_suite().argv);
+
+    // Load the required libraries (we assume the working dir is the binary path)
+    LibLoader& loader = *OSystem::instance().lib_loader();
+
+    const std::vector< boost::filesystem::path > lib_paths = boost::assign::list_of
+                                                             ("../../../dso");
+    loader.set_search_paths(lib_paths);
+
+    loader.load_library("coolfluid_mesh_neu");
+    loader.load_library("coolfluid_mesh_gmsh");
+    //loader.load_library("coolfluid_mesh_tecplot");
+    //loader.load_library("coolfluid_mesh_vtklegacy");
+
+    euler2d_wizard = allocate_component<SteadyExplicit>("mymodel");
+
+    SignalFrame frame;
+    SignalOptions options( frame );
+
+    options.add<std::string>("ModelName","mymodel");
+    options.add<std::string>("PhysicalModel","Euler2D");
+
+    euler2d_wizard->signal_create_model(frame);
+  }
+
+//  ~euler2d_global_fixture() { Core::instance().terminate(); }
+
+
+  SteadyExplicit::Ptr euler2d_wizard;
+
+};
+
+struct euler2d_local_fixture
+{
+  euler2d_local_fixture() :
+    model  ( * Core::instance().root().get_child_ptr("mymodel")->as_ptr<CModel>() ),
+    domain ( find_component_recursively<CDomain>(model)  ),
+    solver ( find_component_recursively<CSolver>(model) )
+  {}
+
+  CModel& model;
+  CDomain& domain;
+  CSolver& solver;
+};
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_GLOBAL_FIXTURE( euler2d_global_fixture )
+
+BOOST_AUTO_TEST_SUITE( euler2d_test_suite )
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( test_check_tree , euler2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  SignalFrame frame;
+  SignalOptions options( frame );
+
+  Core::instance().root().signal_list_tree(frame);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( test_read_mesh , euler2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+//  CFinfo << Core::instance().root().tree() << CFendl;
+
+  // create the xml parameters for the read mesh signal
+
+  SignalFrame frame;
+  SignalOptions options( frame );
+
+  BOOST_CHECK(true);
+
+  std::vector<URI> files;
+
+  URI file( "file:euler2d-tg.msh" );     // works
+
+
+  std::vector<URI::Scheme::Type> schemes(1);
+  schemes[0] = URI::Scheme::FILE;
+
+  options.add("file", file );
+  options.add<std::string>("name", std::string("Mesh") );
+
+  std::cout << "opening file: " << file.string() << std::endl;
+
+  domain.signal_load_mesh( frame );
+
+  BOOST_CHECK_NE( domain.count_children(), (Uint) 0);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( test_setup_iterative_solver , euler2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  solver.configure_property("Domain",URI("cpath:../Domain"));
+  solver.get_child("time_stepping").configure_property("cfl", 0.25);;
+  solver.get_child("time_stepping").configure_property("MaxIter", 250u);;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( test_create_boundary_term , euler2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  SignalFrame frame;
+  SignalOptions options( frame );
+
+  std::vector<URI> regions;
+  boost_foreach( const CRegion& region, find_components_recursively_with_name<CRegion>(domain,"bottom"))
+    regions.push_back( region.full_path() );
+  boost_foreach( const CRegion& region, find_components_recursively_with_name<CRegion>(domain,"left"))
+    regions.push_back( region.full_path() );
+  boost_foreach( const CRegion& region, find_components_recursively_with_name<CRegion>(domain,"right"))
+    regions.push_back( region.full_path() );
+
+  BOOST_CHECK_EQUAL( regions.size() , 3u);
+
+  std::string name ("INLET");
+
+  options.add<std::string>("Name",name);
+  options.add<std::string>("Type","CF.RDM.Core.WeakDirichlet");
+  options.add("Regions", regions, " ; ");
+
+  solver.as_ptr<RKRD>()->signal_create_boundary_term(frame);
+
+  Component::Ptr inletbc = find_component_ptr_recursively_with_name( solver, name );
+  cf_assert( is_not_null(inletbc) );
+
+  std::vector<std::string> fns(4);
+
+  fns[0] = "if(x>0.5,0.5,1.)";
+  fns[1] = "0.0";
+  fns[2] = "if(x>0.5,1.67332,2.83972)";
+  fns[3] = "if(x>0.5,3.425,6.532)";
+
+//  fns[0] = "0.5";
+//  fns[1] = "0.0";
+//  fns[2] = "1.67332";
+//  fns[3] = "3.425";
+
+  inletbc->configure_property("Functions", fns);
+
+  BOOST_CHECK(true);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( signal_initialize_solution , euler2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  SignalFrame frame;
+  SignalOptions options( frame );
+
+  std::vector<std::string> fns(4);
+
+
+  fns[0] = "if(x>0.5,0.5,1.)";
+  fns[1] = "0.0";
+  fns[2] = "if(x>0.5,1.67332,2.83972)";
+  fns[3] = "if(x>0.5,3.425,6.532)";
+
+//  fns[0] = "0.5";
+//  fns[1] = "0.0";
+//  fns[2] = "1.67332";
+//  fns[3] = "3.425";
+
+  options.add<std::string>("Functions", fns, " ; ");
+
+  solver.as_type<RKRD>().signal_initialize_solution( frame );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( test_init_output , euler2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  CMesh::Ptr mesh = find_component_ptr<CMesh>(domain);
+
+  CMeshWriter::Ptr gmsh_writer = create_component_abstract_type<CMeshWriter> ( "CF.Mesh.Gmsh.CWriter", "GmshWriter" );
+  model.add_component(gmsh_writer);
+
+  std::vector<URI> fields;
+  boost_foreach(const CField& field, find_components_recursively<CField>(*mesh))
+    fields.push_back(field.full_path());
+
+  gmsh_writer->configure_property("fields",fields);
+  gmsh_writer->configure_property("file",URI(model.name()+"_init.msh"));
+  gmsh_writer->configure_property("mesh",mesh->full_path());
+
+  gmsh_writer->execute();
+
+  model.remove_component("GmshWriter");
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( solve_CSysLDAGPU, euler2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  CFinfo << "solving with LDA scheme" << CFendl;
+
+  // delete previous domain terms
+  Component& domain_terms = solver.get_child("compute_domain_terms");
+  boost_foreach( RDM::DomainTerm& term, find_components_recursively<RDM::DomainTerm>( domain_terms ))
+  {
+    const std::string name = term.name();
+    domain_terms.remove_component( name );
+  }
+
+  BOOST_CHECK( domain_terms.count_children() == 0 );
+
+  CMesh::Ptr mesh = find_component_ptr<CMesh>(domain);
+
+  SignalFrame frame;
+  SignalOptions options( frame );
+
+  std::vector<URI> regions;
+  boost_foreach( const CRegion& region, find_components_recursively_with_name<CRegion>(*mesh,"topology"))
+    regions.push_back( region.full_path() );
+
+  BOOST_CHECK_EQUAL( regions.size() , 1u);
+
+  options.add<std::string>("Name","INTERNAL");
+  options.add<std::string>("Type","CF.RDM.Schemes.CSysLDAGPU");
+  options.add("Regions", regions, " ; ");
+
+  solver.as_ptr<RKRD>()->signal_create_domain_term(frame);
+
+  BOOST_CHECK(true);
+
+  solver.solve();
+
+  BOOST_CHECK(true);
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( test_output , euler2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  CMesh::Ptr mesh = find_component_ptr<CMesh>(domain);
+
+  std::vector<URI> fields;
+  boost_foreach(const CField& field, find_components_recursively<CField>(*mesh))
+    fields.push_back(field.full_path());
+
+  // gmsh writer
+
+  CMeshWriter::Ptr gmsh_writer = create_component_abstract_type<CMeshWriter> ( "CF.Mesh.Gmsh.CWriter", "GmshWriter" );
+  model.add_component(gmsh_writer);
+
+  gmsh_writer->configure_property("fields",fields);
+  gmsh_writer->configure_property("file",URI(model.name()+".msh"));
+  gmsh_writer->configure_property("mesh",mesh->full_path());
+
+  gmsh_writer->execute();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_SUITE_END()
+
+////////////////////////////////////////////////////////////////////////////////
+
--- plugins/RDM/test/utest-rdm-rotationadv2d-gpu.cpp
+++ plugins/RDM/test/utest-rdm-rotationadv2d-gpu.cpp
@@ -5,7 +5,7 @@
 // See doc/lgpl.txt and doc/gpl.txt for the license text.
 
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE "Test module for CF::RDM::ScalarAdvection"
+#define BOOST_TEST_MODULE "Test module for CF::RDM::RotationAdv2D"
 
 #include <boost/test/unit_test.hpp>
 #include <boost/assign/list_of.hpp>
@@ -38,7 +38,7 @@
 
 #include "RDM/Core/RKRD.hpp"
 #include "RDM/Core/DomainTerm.hpp"
-#include "RDM/Core/ScalarAdvection.hpp"
+#include "RDM/Core/SteadyExplicit.hpp"
 
 using namespace CF;
 using namespace CF::Common;
@@ -47,6 +47,8 @@ using namespace CF::Mesh;
 using namespace CF::Solver;
 using namespace CF::Solver::Actions;
 using namespace CF::RDM;
+
+//#define BUBBLE
 
 struct rotationadv2d_global_fixture
 {
@@ -63,7 +65,7 @@ struct rotationadv2d_global_fixture
     //loader.load_library("coolfluid_mesh_tecplot");
     //loader.load_library("coolfluid_mesh_vtklegacy");
 
-    rotationadv2d_wizard = allocate_component<ScalarAdvection>("mymodel");
+    rotationadv2d_wizard = allocate_component<SteadyExplicit>("mymodel");
 
     SignalFrame frame;
     SignalOptions options( frame );
@@ -76,7 +78,7 @@ struct rotationadv2d_global_fixture
 
   ~rotationadv2d_global_fixture() { Core::instance().terminate(); }
 
-  ScalarAdvection::Ptr rotationadv2d_wizard;
+  SteadyExplicit::Ptr rotationadv2d_wizard;
 
 };
 
@@ -128,22 +130,31 @@ BOOST_FIXTURE_TEST_CASE( read_mesh , rotationadv2d_local_fixture )
 
   std::vector<URI> files;
 
-//  URI file ( "file:rotation-tg-p1.msh" );
-//  URI file ( "file:rotation-tg-p2.msh" );
   URI file ( "file:rotation-tg.msh" );
+//  URI file ( "file:rotation-tg-p2.msh" );
 //  URI file ( "file:rotation-tg-p4.msh" );
 //  URI file ( "file:rotation-qd-p1.msh" );
 //  URI file ( "file:rotation-qd-p2.msh" );
 //  URI file ( "file:rotation-qd-p3.msh" );
 //  URI file ( "file:rotation-qd-p4.msh" );
 
-  options.add("File", file );
-  options.add<std::string>("Name", std::string("Mesh") );
+  options.add("file", file );
+  options.add<std::string>("name", std::string("Mesh") );
 
   domain.signal_load_mesh( frame );
 
   BOOST_CHECK_NE( domain.count_children(), (Uint) 0);
 
+#ifdef BUBBLE // enrich the mesh with bubble functions
+  CMeshTransformer::Ptr enricher =
+      create_component_abstract_type<CMeshTransformer>("CF.Mesh.Actions.CBubbleEnrich","enricher");
+
+  domain.add_component( enricher );
+
+  CMesh::Ptr mesh = find_component_ptr<CMesh>(domain);
+
+  enricher->transform( mesh );
+#endif
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -153,8 +164,8 @@ BOOST_FIXTURE_TEST_CASE( setup_iterative_solver , rotationadv2d_local_fixture )
   BOOST_CHECK(true);
 
   solver.configure_property("Domain",URI("cpath:../Domain"));
-  solver.get_child("time_stepping").configure_property("CFL", 1.);
-  solver.get_child("time_stepping").configure_property("MaxIter", 250u);
+  solver.get_child("time_stepping").configure_property("cfl", 1.);
+  solver.get_child("time_stepping").configure_property("MaxIter", 550u);
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -240,6 +251,30 @@ BOOST_FIXTURE_TEST_CASE( signal_initialize_solution , rotationadv2d_local_fixtur
   solver.as_type<RKRD>().signal_initialize_solution( frame );
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_FIXTURE_TEST_CASE( test_init_output , rotationadv2d_local_fixture )
+{
+  BOOST_CHECK(true);
+
+  CMesh::Ptr mesh = find_component_ptr<CMesh>(domain);
+
+  CMeshWriter::Ptr gmsh_writer = create_component_abstract_type<CMeshWriter> ( "CF.Mesh.Gmsh.CWriter", "GmshWriter" );
+  model.add_component(gmsh_writer);
+
+  std::vector<URI> fields;
+  boost_foreach(const CField& field, find_components_recursively<CField>(*mesh))
+    fields.push_back(field.full_path());
+
+  gmsh_writer->configure_property("fields",fields);
+  gmsh_writer->configure_property("file",URI(model.name()+"_init.msh"));
+  gmsh_writer->configure_property("mesh",mesh->full_path());
+
+  gmsh_writer->execute();
+
+  model.remove_component("GmshWriter");
+}
+
 //////////////////////////////////////////////////////////////////////////////
 
 BOOST_FIXTURE_TEST_CASE( solve_lda_gpu , rotationadv2d_local_fixture )
@@ -283,6 +318,91 @@ BOOST_FIXTURE_TEST_CASE( solve_lda_gpu , rotationadv2d_local_fixture )
 
 }
 
+//////////////////////////////////////////////////////////////////////////////
+
+//BOOST_FIXTURE_TEST_CASE( solve_blended , rotationadv2d_local_fixture )
+//{
+//  BOOST_CHECK(true);
+
+//  CFinfo << "solving with Blended scheme" << CFendl;
+
+//  // delete previous domain terms
+//  Component& domain_terms = solver.get_child("compute_domain_terms");
+//  boost_foreach( RDM::DomainTerm& term, find_components_recursively<RDM::DomainTerm>( domain_terms ))
+//  {
+//    const std::string name = term.name();
+//    domain_terms.remove_component( name );
+//  }
+
+//  BOOST_CHECK( domain_terms.count_children() == 0 );
+
+//  CMesh::Ptr mesh = find_component_ptr<CMesh>(domain);
+
+//  SignalFrame frame;
+//  SignalOptions options( frame );
+
+//  std::vector<URI> regions;
+//  boost_foreach( const CRegion& region, find_components_recursively_with_name<CRegion>(*mesh,"topology"))
+//    regions.push_back( region.full_path() );
+
+//  BOOST_CHECK_EQUAL( regions.size() , 1u);
+
+//  options.add<std::string>("Name","INTERNAL");
+//  options.add<std::string>("Type","CF.RDM.Blended");
+//  options.add("Regions", regions, " ; ");
+
+//  solver.as_ptr<RKRD>()->signal_create_domain_term(frame);
+
+//  BOOST_CHECK(true);
+
+//  solver.solve();
+
+//  BOOST_CHECK(true);
+
+//}
+
+//////////////////////////////////////////////////////////////////////////////
+
+//BOOST_FIXTURE_TEST_CASE( solve_SUPG , rotationadv2d_local_fixture )
+//{
+//  BOOST_CHECK(true);
+
+//  CFinfo << "solving with SUPG scheme" << CFendl;
+
+//  // delete previous domain terms
+//  Component& domain_terms = solver.get_child("compute_domain_terms");
+//  boost_foreach( RDM::DomainTerm& term, find_components_recursively<RDM::DomainTerm>( domain_terms ))
+//  {
+//    const std::string name = term.name();
+//    domain_terms.remove_component( name );
+//  }
+
+//  BOOST_CHECK( domain_terms.count_children() == 0 );
+
+//  CMesh::Ptr mesh = find_component_ptr<CMesh>(domain);
+
+//  SignalFrame frame;
+//  SignalOptions options( frame );
+
+//  std::vector<URI> regions;
+//  boost_foreach( const CRegion& region, find_components_recursively_with_name<CRegion>(*mesh,"topology"))
+//    regions.push_back( region.full_path() );
+
+//  BOOST_CHECK_EQUAL( regions.size() , 1u);
+
+//  options.add<std::string>("Name","INTERNAL");
+//  options.add<std::string>("Type","CF.RDM.SUPG");
+//  options.add("Regions", regions, " ; ");
+
+//  solver.as_ptr<RKRD>()->signal_create_domain_term(frame);
+
+//  BOOST_CHECK(true);
+
+//  solver.solve();
+
+//  BOOST_CHECK(true);
+
+//}
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -293,6 +413,14 @@ BOOST_FIXTURE_TEST_CASE( output , rotationadv2d_local_fixture )
 
   CMesh::Ptr mesh = find_component_ptr<CMesh>(domain);
 
+#ifdef BUBBLE // remove the bubble functions from the mesh
+  CMeshTransformer::Ptr remover =
+      create_component_abstract_type<CMeshTransformer>("CF.Mesh.Actions.CBubbleRemove","remover");
+
+  domain.add_component( remover );
+  remover->transform( mesh );
+#endif
+
   BOOST_CHECK(true);
 
   std::vector<URI> fields;
@@ -302,29 +430,29 @@ BOOST_FIXTURE_TEST_CASE( output , rotationadv2d_local_fixture )
   CMeshWriter::Ptr gmsh_writer = create_component_abstract_type<CMeshWriter> ( "CF.Mesh.Gmsh.CWriter", "GmshWriter" );
   model.add_component(gmsh_writer);
 
-  gmsh_writer->configure_property("Fields",fields);
-  gmsh_writer->configure_property("File",model.name()+".msh");
-  gmsh_writer->configure_property("Mesh",mesh->full_path());
+  gmsh_writer->configure_property("fields",fields);
+  gmsh_writer->configure_property("file",URI(model.name()+".msh"));
+  gmsh_writer->configure_property("mesh",mesh->full_path());
 
-  gmsh_writer->write();
+  gmsh_writer->execute();
 
  /* CMeshWriter::Ptr vtk_writer = create_component_abstract_type<CMeshWriter>("CF.Mesh.VTKLegacy.CWriter","VTKWriter");
   model.add_component(vtk_writer);
 
-  vtk_writer->configure_property("Fields",fields);
-  vtk_writer->configure_property("File",model.name()+".vtk");
-  vtk_writer->configure_property("Mesh",mesh->full_path());
+  vtk_writer->configure_property("fields",fields);
+  vtk_writer->configure_property("file",URI(model.name()+".vtk"));
+  vtk_writer->configure_property("mesh",mesh->full_path());
 
-  vtk_writer->write();
-/*
-  CMeshWriter::Ptr tec_writer = create_component_abstract_type<CMeshWriter>("CF.Mesh.Tecplot.CWriter","TecWriter");
+  vtk_writer->execute();*/
+
+ /* CMeshWriter::Ptr tec_writer = create_component_abstract_type<CMeshWriter>("CF.Mesh.Tecplot.CWriter","TecWriter");
   model.add_component(tec_writer);
 
-  tec_writer->configure_property("Fields",fields);
-  tec_writer->configure_property("File",model.name()+".plt");
-  tec_writer->configure_property("Mesh",mesh->full_path());
+  tec_writer->configure_property("fields",fields);
+  tec_writer->configure_property("file",URI(model.name()+".plt"));
+  tec_writer->configure_property("mesh",mesh->full_path());
 
-  tec_writer->write();*/
+  tec_writer->execute();*/
 }
 
 //////////////////////////////////////////////////////////////////////////////
