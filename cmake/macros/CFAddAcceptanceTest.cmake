50d7d2d1e09d1e0a8de2da9521be4c84a03ab0ed
tlmquintino <tlmquintino@gmail.com>
2011-05-27 08:36:57 +0000

    [FIX] atests run on defined directory - no longer depeds on make 2.8.4
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@3044 4892e656-8fb9-448a-b5bc-b1919152596f

--- cmake/macros/CFAddAcceptanceTest.cmake
+++ cmake/macros/CFAddAcceptanceTest.cmake
@@ -8,34 +8,18 @@ macro( coolfluid_add_acceptance_test ATESTNAME ATESTSCRIPT )
 
     # make a diretory to run the test case
 
-    set( TESTDIR ${CMAKE_CURRENT_BINARY_DIR}/${ATESTNAME}.dir )
-    
-    file( MAKE_DIRECTORY ${TESTDIR} )
+#    set( TESTDIR ${CMAKE_CURRENT_BINARY_DIR}/${ATESTNAME}.dir )
+    set( TESTDIR ${CMAKE_CURRENT_BINARY_DIR} )
+
+#    file( MAKE_DIRECTORY ${TESTDIR} )
 
     # put the coolfluid-shell script there
 
     configure_file( ${CMAKE_CURRENT_SOURCE_DIR}/${ATESTSCRIPT} ${TESTDIR}/${ATESTSCRIPT} @ONLY)
 
-
-# copy files to the build directory (not working)
-#    foreach( dfile ${${ATESTNAME}_files} )
-#      add_custom_command(OUTPUT  ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}/${dfile}
-#                         COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/${dfile} ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}
-#                         DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${dfile}
-#                         COMMENT "copying ${dfile}"
-#                        )
-
-#      add_custom_command(TARGET  coolfluid-command
-#                         POST_BUILD
-#                         COMMAND ${CMAKE_COMMAND} -E make_directory    ${CMAKE_CURRENT_SOURCE_DIR}/${dfile} ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}
-#                         COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/${dfile} ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}
-#                         COMMENT "copying ${dfile}"
-#                        )
-#    endforeach()
-
     # Apparently needs cmake 2.8.4 or greater
     add_test( NAME ${ATESTNAME}
-              WORKING_DIRECTORY ${TESTDIR}
+ #             WORKING_DIRECTORY ${TESTDIR}
               COMMAND coolfluid-command -f ${ATESTSCRIPT} )
   endif()
 
