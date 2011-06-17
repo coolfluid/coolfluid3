d35c6e546254d6785230770e72298680cc0428a3
martinv <martin.vymazal@gmail.com>
2011-06-17 12:31:58 +0000

    [ENH] alternative BC for Euler
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@3103 4892e656-8fb9-448a-b5bc-b1919152596f

---
+++ plugins/RDM/RDM/NavierStokes/WallEdwinBc.cpp
@@ -0,0 +1,116 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include "Common/CBuilder.hpp"
+#include "Common/OptionURI.hpp"
+#include "Common/OptionArray.hpp"
+#include "Common/FindComponents.hpp"
+
+
+#include "Mesh/CRegion.hpp"
+#include "Mesh/CField.hpp"
+#include "Mesh/CMesh.hpp"
+#include "Mesh/CElements.hpp"
+
+#include "Solver/CPhysicalModel.hpp"
+
+#include "RDM/Core/FaceLoop.hpp"
+
+#include "RDM/NavierStokes/WallEdwinBc.hpp"
+
+#include "RDM/Core/Euler2D.hpp"
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+using namespace CF::Common;
+using namespace CF::Mesh;
+using namespace CF::Solver;
+
+namespace CF {
+namespace RDM {
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+Common::ComponentBuilder < WallEdwinBc, RDM::BoundaryTerm, LibCore > WallEdwinBc_Builder;
+
+Common::ComponentBuilder < FaceLoopT< WallEdwinBc, Euler2D>, RDM::FaceLoop, LibCore > WallEdwinBc_Euler2D_Builder;
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+WallEdwinBc::WallEdwinBc ( const std::string& name ) :
+  RDM::BoundaryTerm(name)
+{
+  regist_typeinfo(this);
+
+  // options
+
+  m_properties.add_option< OptionURI > ("solution","Solution",
+                                        "Solution field where to apply the boundary condition",
+                                        URI("cpath:"))
+       ->attach_trigger ( boost::bind ( &WallEdwinBc::config_mesh,   this ) )
+       ->mark_basic()
+       ->add_tag("solution");
+
+  m_properties["mesh"].as_option().attach_trigger ( boost::bind ( &WallEdwinBc::config_mesh, this ) );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void WallEdwinBc::config_mesh()
+{
+  cf_assert( is_not_null( m_mesh.lock() ) );
+
+  URI sol_uri  = property("solution").value<URI>();
+  solution = access_component_ptr(sol_uri)->as_ptr<CField>();
+  if( is_null(solution.lock()) )
+    solution = find_component_ptr_with_tag<CField>( *(m_mesh.lock()) , "solution" );
+
+  if( is_null(solution.lock()) )
+    throw CastingFailed (FromHere(),
+                         "Could not find a solution field on mesh "
+                         + m_mesh.lock()->uri().string() );
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+void WallEdwinBc::execute()
+{
+  const std::string physics = physical_model().type();
+
+  // get the element loop or create it if does not exist
+  ElementLoop::Ptr loop;
+  Common::Component::Ptr cloop = get_child_ptr( "LOOP" );
+  if( is_null( cloop ) )
+  {
+    loop = build_component_abstract_type_reduced< FaceLoop >( "FaceLoopT<" + type_name() + "," + physics + ">" , "LOOP");
+    add_component(loop);
+  }
+  else
+    loop = cloop->as_ptr_checked<ElementLoop>();
+
+  // loop on all regions configured by the user
+
+  boost_foreach(Mesh::CRegion::Ptr& region, m_loop_regions)
+  {
+
+//    std::cout << "REGION [" << region->uri().string() << "]" << std::endl;
+
+    loop->select_region( region );
+
+    // loop all elements of this region
+
+    loop->execute();
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+} // RDM
+} // CF
+
+////////////////////////////////////////////////////////////////////////////////////
+
---
+++ plugins/RDM/RDM/NavierStokes/WallEdwinBc.hpp
@@ -0,0 +1,293 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#ifndef CF_RDM_WallEdwinBc_hpp
+#define CF_RDM_WallEdwinBc_hpp
+
+#include <iostream> // to remove
+
+#include "RDM/Core/BoundaryTerm.hpp"
+#include "RDM/Core/BcBase.hpp"
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+namespace CF {
+
+namespace Mesh { class CMesh; class CField; }
+
+namespace RDM {
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+class RDM_CORE_API WallEdwinBc : public RDM::BoundaryTerm {
+public: // typedefs
+
+  /// the actual BC implementation is a nested class
+  /// varyng with shape function (SF), quadrature rule (QD) and Physics (PHYS)
+  template < typename SF, typename QD, typename PHYS > class Term;
+
+  /// pointers
+  typedef boost::shared_ptr<WallEdwinBc> Ptr;
+  typedef boost::shared_ptr<WallEdwinBc const> ConstPtr;
+
+public: // functions
+  /// Contructor
+  /// @param name of the component
+  WallEdwinBc ( const std::string& name );
+
+  /// Virtual destructor
+  virtual ~WallEdwinBc() {};
+
+  /// Get the class name
+  static std::string type_name () { return "WallEdwinBc"; }
+
+  /// execute the action
+  virtual void execute ();
+
+private: // helper functions
+
+  void config_mesh();
+
+public: // data
+
+  /// access to the solution field on the mesh
+  boost::weak_ptr<Mesh::CField> solution;
+
+}; // !WallEdwinBc
+
+//------------------------------------------------------------------------------------------
+
+template < typename SF, typename QD, typename PHYS >
+class RDM_CORE_API WallEdwinBc::Term : public BcBase<SF,QD,PHYS> {
+
+public: // typedefs
+
+  /// base class type
+  typedef BcBase<SF,QD,PHYS> B;
+  /// pointers
+  typedef boost::shared_ptr< Term > Ptr;
+  typedef boost::shared_ptr< Term const> ConstPtr;
+
+public: // functions
+
+  /// Contructor
+  /// @param name of the component
+ Term ( const std::string& name ) :
+   BcBase<SF,QD,PHYS>(name),
+   m_quadrature( QD::instance() )
+
+ {
+   regist_typeinfo(this);
+
+   // Gradient of the shape functions in reference space
+   typename SF::MappedGradientT GradSF;
+   // Values of shape functions in reference space
+   typename SF::ShapeFunctionsT ValueSF;
+
+   // initialize the interpolation matrix
+
+   for(Uint q = 0; q < QD::nb_points; ++q)
+     for(Uint n = 0; n < SF::nb_nodes; ++n)
+     {
+        SF::shape_function_gradient( m_quadrature.coords.col(q), GradSF  );
+        SF::shape_function_value   ( m_quadrature.coords.col(q), ValueSF );
+
+        Ni(q,n)     = ValueSF[n];
+        dNdKSI(q,n) = GradSF[n];
+     }
+
+   // reflection matrix
+
+   RM.setIdentity();
+ }
+
+ /// Get the class name
+ static std::string type_name () { return "WallEdwinBc.BC<" + SF::type_name() + ">"; }
+
+protected: // data
+
+ /// helper object to compute the quadrature information
+ const QD& m_quadrature;
+
+ typedef Eigen::Matrix<Real, QD::nb_points, 1u>               WeightVT;
+
+ typedef typename SF::NodeMatrixT                             NodeMT;
+
+ typedef Eigen::Matrix<Real, QD::nb_points, SF::nb_nodes >     SFMatrixT;
+ typedef Eigen::Matrix<Real, QD::nb_points, PHYS::ndim   >     QCoordMT;
+ typedef Eigen::Matrix<Real, SF::nb_nodes,  PHYS::neqs   >     SolutionMT;
+ typedef Eigen::Matrix<Real, QD::nb_points, PHYS::neqs   >     QSolutionMT;
+ typedef Eigen::Matrix<Real, PHYS::neqs,    PHYS::ndim   >     QSolutionVT;
+ typedef Eigen::Matrix<Real, PHYS::neqs,    1u           >     SolutionVT;
+ typedef Eigen::Matrix<Real, PHYS::neqs,    PHYS::ndim   >     FluxMT;
+ typedef Eigen::Matrix<Real, PHYS::ndim,    1u           >     DimVT;
+
+ /// derivative matrix - values of shapefunction derivative in Ksi at each quadrature point
+ SFMatrixT  dNdKSI;
+ /// interporlation matrix - values of shapefunction at each quadrature point
+ SFMatrixT  Ni;
+ /// node values
+ NodeMT     X_n;
+ /// coordinates of quadrature points in physical space
+ QCoordMT   X_q;
+ /// derivatives of coordinate transformation physical->reference space
+ QCoordMT  dX_q;
+ /// Values of the solution located in the dof of the element
+ SolutionMT U_n;
+ /// solution at quadrature points in physical space
+ QSolutionMT U_q;
+ /// derivatives of solution to X at each quadrature point, one matrix per dimension
+ QSolutionMT dUdX[PHYS::ndim];
+ /// derivatives of solution with respect to x,y,z at ONE quadrature point
+ QSolutionVT dUdXq;
+ /// Integration factor (jacobian multiplied by quadrature weight)
+ WeightVT wj;
+ /// contribution to nodal residuals
+ SolutionMT Phi_n;
+
+ /// flux computed with current solution
+ FluxMT Fu_h;
+ /// flux computed with boundary value
+ FluxMT Fu_g;
+ /// ghost solution vector
+ SolutionVT u_g;
+
+ /// diagonal matrix with eigen values
+ typename B::PhysicsVT Dv;
+ /// diagonal matrix with positive eigen values
+ typename B::PhysicsVT DvPlus;
+
+ /// temporary normal on 1 quadrature point
+ DimVT dN;
+
+ /// corrective flux
+ SolutionMT F_c;
+
+ /// reflexion matrix for setting velocity tangent to wall
+ Eigen::Matrix<Real, PHYS::neqs, PHYS::neqs >  RM;
+
+public: // functions
+
+ /// virtual interface to execute the action
+ virtual void execute () { executeT(); }
+
+ /// execute the action
+ void executeT ()
+ {
+//    std::cout << "Face [" << B::idx() << "]" << std::endl;
+
+   // get face connectivity
+
+   const Mesh::CTable<Uint>::ConstRow nodes_idx = this->connectivity_table->array()[B::idx()];
+
+   // copy the coordinates from the large array to a small
+
+   Mesh::fill(X_n, *B::coordinates, nodes_idx );
+
+   // copy the solution from the large array to a small
+
+   for(Uint n = 0; n < SF::nb_nodes; ++n)
+     for (Uint v=0; v < PHYS::neqs; ++v)
+       U_n(n,v) = (*B::solution)[ nodes_idx[n] ][v];
+
+
+   const Real dx = X_n(1,XX) - X_n(0,XX);
+   const Real dy = X_n(1,YY) - X_n(0,YY);
+
+   const Real jacob = std::sqrt( dx*dx + dy*dy );
+
+   dN[XX] =  dy/jacob;
+   dN[YY] = -dx/jacob;
+
+   Phi_n.setZero();
+   F_c.setZero();
+
+   const Real alpha = 0.75;
+   const Real coeff_state =        alpha /SF::nb_nodes;
+   const Real coeff_other = (1.0 - alpha)/SF::nb_nodes;
+
+   /// For the moment, the corrective flux is equal to the solution vector at each node
+//   F_c = U_n;
+
+   for(Uint ni = 0; ni < SF::nb_nodes; ++ni)
+   {
+     for(Uint dim = 0; dim < PHYS::ndim; ++dim)
+     {
+       dUdXq.col(dim) = dUdX[dim].row(0).transpose();
+     }
+
+
+     PHYS::compute_properties(X_n.row(ni),
+                              U_n.row(ni),
+                              dUdXq,
+                              B::phys_props);
+
+     const Real vn = dN[XX] * B::phys_props.u + dN[YY] * B::phys_props.v;
+
+     F_c(ni,0) = B::phys_props.rho  * vn;
+     F_c(ni,1) = B::phys_props.rhou * vn;
+     F_c(ni,2) = B::phys_props.rhov * vn;
+     F_c(ni,3) = B::phys_props.H    * vn;
+
+//     F_c(ni,3) += B::phys_props.P;
+//     F_c.row(ni) *= vn;
+
+     for(Uint nj=0; nj < SF::nb_nodes; ++nj)
+     {
+       if( ni == nj)
+       {
+         for(Uint v=0; v < PHYS::neqs; ++v)
+         {
+           Phi_n.row(nj)[v] -= coeff_state * F_c(nj,v);
+         }
+       }
+       else
+       {
+         for(Uint v=0; v < PHYS::neqs; ++v)
+         {
+           Phi_n.row(nj)[v] -= coeff_other * F_c(nj,v);
+         }
+       }
+
+     } // Loop over nodes (nj)
+
+
+     // compute the wave_speed for scaling the update
+
+//     PHYS::jacobian_eigen_values(B::phys_props,
+//                                 X_n,
+//                                 U_q.row(q),
+//                                 dN,
+//                                 Dv );
+
+//     DvPlus = Dv.unaryExpr(std::ptr_fun(plus));
+
+//     for(Uint n = 0; n < SF::nb_nodes; ++n)
+//       (*B::wave_speed)[nodes_idx[n]][0] += DvPlus.array().maxCoeff() * wj[q];
+
+
+    } // Loop over nodes (ni)
+
+
+   // update the residual
+
+   for (Uint n=0; n<SF::nb_nodes; ++n)
+     for (Uint v=0; v < PHYS::neqs; ++v)
+       (*B::residual)[nodes_idx[n]][v] += Phi_n(n,v);
+
+ }
+
+
+}; // !WallEdwinBc::Term
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+} // RDM
+} // CF
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+#endif // CF_RDM_WallEdwinBc_hpp
