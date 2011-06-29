39033ea51ac1f145f1d1ac4a42c2476a2d6a372f
gquentin <gquentin@gmail.com>
2011-06-29 15:41:12 +0000

    [DOC] missing documentation
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@3154 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Common/BoostAnyConversion.hpp
+++ src/Common/BoostAnyConversion.hpp
@@ -20,10 +20,31 @@ namespace Common {
 
 /////////////////////////////////////////////////////////////////////////////
 
+/// @file Helper functions for boost::any manipulations
+/// @author Quentin Gasper.
+
+/// Converts a boost::any value to a string.
+
+/// The volue must of one of the types supported by the Property facility:
+/// bool, int, Real, Uint, std::string or URI.
+/// @param value The value to convert
+/// @return Returns a string repensation of the value
+/// @throw ProtocolError if the value type is not supported
+/// @throw CastingFailed if the conversion failed.
 Common_API std::string any_to_str( const boost::any & value );
 
+/// Gives the demangled type string of a boost::any value.
+
+/// @param value The value
+/// @return Returns the demangled type string. May return an empty string if
+/// the type is not registered to CF::Common::TypeInfo
 Common_API std::string any_type( const boost::any & value );
 
+/// Converts a boost::any value to the provided TYPE.
+
+/// @param value The value to convert.
+/// @return Returns the converted value.
+/// @throw CastingFailed if the value could not be converted.
 template<typename TYPE>
 Common_API TYPE any_to_value( const boost::any & value );
 
