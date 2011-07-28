ba3fb8bf443af5f132946711b671bd9981b125bc
tlmquintino <tlmquintino@gmail.com>
2011-07-28 07:06:41 +0000

    [ENH] to switch of the plugins selectively, set cmake var CF_PLUGIN_NAME=OFF
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@3356 4892e656-8fb9-448a-b5bc-b1919152596f

--- cmake/macros/CFDefinePlugin.cmake
+++ cmake/macros/CFDefinePlugin.cmake
@@ -4,12 +4,18 @@
 
 macro( coolfluid_define_plugin PLUGIN_NAME PLUGIN_DIR )
 
-      set( CF_PLUGIN_LIST ${CF_PLUGIN_LIST} ${PLUGIN_NAME} CACHE INTERNAL "" )
+  coolfluid_log_file( "PLUGIN [${PLUGIN_NAME}]" )
+  coolfluid_log_file( "+++ DIR [${PLUGIN_DIR}]" )
 
-      coolfluid_log_file( "PLUGIN [${PLUGIN_NAME}]" )
-      coolfluid_log_file( "+++ DIR [${PLUGIN_DIR}]" )
+  string( TOUPPER ${PLUGIN_NAME} PLUGIN_NAME_CAPS )
 
-     set( ${PLUGIN_NAME}_DIR ${PLUGIN_DIR} PARENT_SCOPE )
+  set( ${PLUGIN_NAME}_DIR ${PLUGIN_DIR} PARENT_SCOPE )
+
+  option( CF_PLUGIN_${PLUGIN_NAME_CAPS}  "Enable the plugin ${PLUGIN_NAME}" ON  )
+
+  if( CF_PLUGIN_${PLUGIN_NAME_CAPS} )
+    set( CF_PLUGIN_LIST ${CF_PLUGIN_LIST} ${PLUGIN_NAME} CACHE INTERNAL "" )
+  endif()
 
 endmacro()
 
