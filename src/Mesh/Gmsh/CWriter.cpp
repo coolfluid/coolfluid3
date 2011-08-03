62f298c31ff0fbadd731a808afaaa1b53ad71529
tlmquintino <tlmquintino@gmail.com>
2011-08-03 14:39:09 +0000

    [COSM] removed spurious output
    git-svn-id: https://coolfluidsrv.vki.ac.be/svn/coolfluid3/trunk@3417 4892e656-8fb9-448a-b5bc-b1919152596f

--- src/Mesh/Gmsh/CWriter.cpp
+++ src/Mesh/Gmsh/CWriter.cpp
@@ -283,13 +283,11 @@ void CWriter::write_elem_nodal_data(std::fstream& file)
 
   boost_foreach(boost::weak_ptr<CField> field_ptr, m_fields)
   {
-    std::cout << "1" << std::endl;
     CField& elementbased_field = *field_ptr.lock();
     if (elementbased_field.basis() == CField::Basis::ELEMENT_BASED ||
         elementbased_field.basis() == CField::Basis::CELL_BASED    ||
         elementbased_field.basis() == CField::Basis::FACE_BASED    )
     {
-      std::cout << "2" << std::endl;
       const Real field_time = 0;//elementbased_field.option("time").value<Real>();
       const Uint field_iter = 0;//elementbased_field.option("iteration").value<Uint>();
       const std::string field_name = elementbased_field.name();
@@ -304,7 +302,7 @@ void CWriter::write_elem_nodal_data(std::fstream& file)
           nb_elements += elements.size();
         }
       }
-std::cout << "3" << std::endl;
+
       // data_header
       Uint row_idx=0;
       for (Uint iVar=0; iVar<elementbased_field.nb_vars(); ++iVar)
@@ -335,19 +333,16 @@ std::cout << "3" << std::endl;
         file << 1 << "\n" << field_time << "\n";
         file << 3 << "\n" << field_iter << "\n" << datasize << "\n" << nb_elements <<"\n";
 
-std::cout << "4" << std::endl;
         boost_foreach(CEntities& elements, find_components_recursively<CEntities>(elementbased_field.topology()))
         {
           if (elementbased_field.exists_for_entities(elements))
           {
-            std::cout << "5" << std::endl;
             CMultiStateFieldView field_view("field_view");
             field_view.initialize(elementbased_field,elements.as_ptr<CEntities>());
             Uint elm_number = m_element_start_idx[&elements];
             Uint local_nb_elms = elements.size();
 
             const Uint nb_states = field_view.space().nb_states();
-            std::cout << "nb_states = " << nb_states << std::endl;
             RealMatrix field_data (field_view.space().nb_states(),var_type);
 
             const Uint nb_nodes = elements.element_type().nb_nodes();
@@ -355,7 +350,6 @@ std::cout << "4" << std::endl;
             /// write element
             for (Uint local_elm_idx = 0; local_elm_idx<local_nb_elms; ++local_elm_idx)
             {
-              std::cout << "6" << std::endl;
               file << ++elm_number << " " << nb_nodes << " ";
 
               /// set field data
@@ -367,7 +361,6 @@ std::cout << "4" << std::endl;
                   field_data(iState,j) = data_view[iState][row_idx+j];
               }
 
-              std::cout << "7" << std::endl;
               for (Uint iNode=0; iNode<nb_nodes; ++iNode)
               {
                 /// get element_node local coordinates
@@ -378,7 +371,6 @@ std::cout << "4" << std::endl;
                 cf_assert(node_data.size() == var_type);
 
 
-                std::cout << "8" << std::endl;
                 if (var_type==CField::TENSOR_2D)
                 {
                   data[0]=node_data[0];
