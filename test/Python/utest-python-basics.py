f4ba5f55d71d8322b80cb2b7ae8503930bf326a4
Bart Janssens <bart.janssens@lid.kviv.be>
2011-08-11 16:39:34 +0200

    [ENH] Add utest for python

---
+++ test/Python/utest-python-basics.py
@@ -0,0 +1,22 @@
+import libcoolfluid_python as cf
+import sys
+
+cf.Core.initiate(sys.argv)
+
+root = cf.Core.root()
+env = cf.Core.environment()
+
+env.configure_option('assertion_backtrace', False)
+env.configure_option('exception_backtrace', False)
+env.configure_option('regist_signal_handlers', False)
+env.configure_option('exception_log_level', 0)
+#env.configure_option('log_level', 4)
+env.configure_option('exception_outputs', False)
+
+journal = root.create_component("journal", "CF.Common.CJournal")
+print journal.option_value_str("RecordReplies")
+
+group = root.create_component("group", "CF.Common.CGroup")
+print "Before move",journal.uri()
+journal.move_component(group.uri())
+print "After move",journal.uri()
