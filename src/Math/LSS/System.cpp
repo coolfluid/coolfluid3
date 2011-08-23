fd74822c9c645e5f6801883a5e0bd6f5fe803643
Tamas Banyai <tamas.banyai@vki.ac.be>
2011-08-23 17:57:50 +0200

    [ENH] LSS interface advances and utest structure

---
+++ src/Math/LSS/System.cpp
@@ -0,0 +1,10 @@
+// Copyright (C) 2010 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include "Math/LSS/System.hpp"
+
+using namespace CF::Math::LSS;
+
