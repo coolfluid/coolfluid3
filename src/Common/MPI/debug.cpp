7fbc89baa6504a36971c0f708b1c349cdb1092ac
Bart Janssens <bart.janssens@lid.kviv.be>
2011-08-30 21:46:52 +0200

    [ENH] Add a debugging function to wait for attach

---
+++ src/Common/MPI/debug.cpp
@@ -0,0 +1,41 @@
+// Copyright (C) 2010-2011 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+
+#ifndef CF_Common_MPI_debug_HPP
+#define CF_Common_MPI_debug_HPP
+
+////////////////////////////////////////////////////////////////////////////////
+
+#include <boost/thread/thread.hpp>
+
+#include "Common/MPI/debug.hpp"
+#include "Common/MPI/PE.hpp"
+
+namespace CF {
+  namespace Common {
+    namespace Comm {
+
+void wait_for_debugger(const int rank)
+{
+  const Uint irank = PE::instance().rank();
+  if(rank >= 0 && rank != irank)
+    return;
+    
+  int stopped = 1;
+  std::cout << "Rank " << irank << " with PID " << getpid() << " is waiting for debugger attach" << std::endl;
+  while (0 != stopped)
+    boost::this_thread::sleep(boost::posix_time::milliseconds(250));
+}
+
+
+    } // end namespace Comm
+  } // end namespace Common
+} // end namespace CF
+
+////////////////////////////////////////////////////////////////////////////////
+
+#endif // CF_Common_MPI_debug_HPP
