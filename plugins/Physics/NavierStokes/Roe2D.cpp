c4c72f5b5b0e5ec12295a98a32e57f27d7a1e4bf
Willem Deconinck <ir.willem.deconinck@gmail.com>
2011-09-14 14:23:28 +0200

    [ENH] NavierStokes::Roe2D class

---
+++ plugins/Physics/NavierStokes/Roe2D.cpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2010-2011 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include "Common/CBuilder.hpp"
+
+#include "Roe2D.hpp"
+
+namespace CF {
+namespace Physics {
+namespace NavierStokes {
+
+////////////////////////////////////////////////////////////////////////////////////
+
+Common::ComponentBuilder < NavierStokes::Roe2D,
+                           Physics::Variables,
+                           LibNavierStokes >
+                           Builder_Roe2D;
+
+Roe2D::Roe2D(const std::string& name) : VariablesT<Roe2D>(name)
+{
+  description().set_variables("Z1,Z2,Z3,Z4",MODEL::_ndim);
+}
+
+Roe2D::~Roe2D() {}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+} // NavierStokes
+} // Physics
+} // CF
