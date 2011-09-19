f72eab49791e10fa6ee90673b127b9bd34ff3e19
Willem Deconinck <ir.willem.deconinck@gmail.com>
2011-09-19 12:24:21 +0200

    [ENH] Physics::NavierStokes::Prim2D

---
+++ plugins/Physics/NavierStokes/Prim2D.cpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2010-2011 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include "Common/CBuilder.hpp"
+
+#include "Prim2D.hpp"
+
+namespace CF {
+namespace Physics {
+namespace NavierStokes {
+
+////////////////////////////////////////////////////////////////////////////////////
+
+Common::ComponentBuilder < NavierStokes::Prim2D,
+                           Physics::Variables,
+                           LibNavierStokes >
+                           Builder_Prim2D;
+
+Prim2D::Prim2D(const std::string& name) : VariablesT<Prim2D>(name)
+{
+  description().set_variables("Rho,U[vector],P",MODEL::_ndim);
+}
+
+Prim2D::~Prim2D() {}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+} // NavierStokes
+} // Physics
+} // CF
