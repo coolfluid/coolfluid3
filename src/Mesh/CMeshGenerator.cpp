914e801adeefcce6a71f9fdefb3ff9b61a89a41e
Willem Deconinck <ir.willem.deconinck@gmail.com>
2011-10-05 09:39:18 +0200

    [ENH] CMeshGenerator / CSimpleMeshGenerator improvements

--- src/Mesh/CMeshGenerator.cpp
+++ src/Mesh/CMeshGenerator.cpp
@@ -25,28 +25,55 @@ using namespace Common::XML;
 ////////////////////////////////////////////////////////////////////////////////
 
 CMeshGenerator::CMeshGenerator ( const std::string& name  ) :
-  CAction ( name ),
-  m_name("mesh")
+  CAction ( name )
 {
   mark_basic();
 
-  m_options.add_option(OptionComponent<Component>::create("parent", &m_parent))
-      ->description("Where the mesh will be generated into")
-      ->pretty_name("Parent")
-      ->mark_basic();
-
-  m_options.add_option<OptionT<std::string> >("name", m_name)
-      ->description("Name of the mesh that will be generated")
-      ->pretty_name("Name")
-      ->link_to(&m_name)
-      ->mark_basic();
-
+  m_options.add_option(OptionURI::create("mesh","cpath:",URI::Scheme::CPATH))
+      ->description("Mesh that will be generated")
+      ->pretty_name("Mesh")
+      ->mark_basic()
+      ->attach_trigger( boost::bind( &CMeshGenerator::config_mesh , this) );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void CMeshGenerator::mesh_loaded(CMesh& mesh)
+void CMeshGenerator::config_mesh()
 {
+  URI mesh_uri = option("mesh").value<URI>();
+  if ( Component::Ptr found_mesh = Core::instance().root().access_component_ptr( mesh_uri ) )
+  {
+    m_mesh = found_mesh->as_ptr_checked<CMesh>();
+  }
+  else
+  {
+    URI parent_uri = mesh_uri.base_path();
+    std::string mesh_name = mesh_uri.name();
+    if ( Component::Ptr found_parent = Core::instance().root().access_component_ptr( parent_uri ) )
+    {
+      m_mesh = found_parent->create_component_ptr<CMesh>(mesh_name);
+    }
+    else
+    {
+      throw ValueNotFound(FromHere(), "Could not find component "+parent_uri.string()+" to create mesh \""+mesh_name+"\" into");
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+CMesh& CMeshGenerator::generate()
+{
+  execute();
+  return *m_mesh.lock();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void CMeshGenerator::raise_mesh_loaded()
+{
+  CMesh& mesh = *m_mesh.lock();
+
   mesh.update_statistics();
   mesh.elements().update();
 
