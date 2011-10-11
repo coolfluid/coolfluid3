5b55cb2c4555c504bce0b7a52eeb1b27f2d83410
Willem Deconinck <ir.willem.deconinck@gmail.com>
2011-10-11 16:30:35 +0200

    [ENH] scalar linear advection 1D

---
+++ plugins/Physics/Scalar/LinearAdv1D.cpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2010-2011 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include "Common/CBuilder.hpp"
+
+#include "LinearAdv1D.hpp"
+
+namespace CF {
+namespace Physics {
+namespace Scalar {
+
+////////////////////////////////////////////////////////////////////////////////////
+
+Common::ComponentBuilder < Scalar::LinearAdv1D,
+                           Physics::Variables,
+                           LibScalar >
+                           Builder_LinearAdv1D;
+
+LinearAdv1D::LinearAdv1D(const std::string& name) : VariablesT<LinearAdv1D>(name)
+{
+  description().set_variables("U",MODEL::_ndim);
+}
+
+LinearAdv1D::~LinearAdv1D() {}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+} // Scalar
+} // Physics
+} // CF
