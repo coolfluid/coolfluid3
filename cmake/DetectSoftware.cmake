d4765553e68f84e600436ae2e8125926e2b2d0b0
Willem Deconinck <ir.willem.deconinck@gmail.com>
2011-10-12 18:13:57 +0200

    [BUILD] find gnuplot quietly

--- cmake/DetectSoftware.cmake
+++ cmake/DetectSoftware.cmake
@@ -85,7 +85,7 @@ find_package(Curl)            # curl downloads files on the fly
 find_package(CGNS)            # CGNS library
 find_package(SuperLU)         # SuperLU sparse sirect solver
 find_package(Trilinos)        # Trilinos sparse matrix library
-find_package(Gnuplot)         # Find gnuplot executable
+find_package(Gnuplot QUIET)   # Find gnuplot executable
 
 # opencl support
 if( CF_ENABLE_OPENCL AND CF_ENABLE_GPU )
