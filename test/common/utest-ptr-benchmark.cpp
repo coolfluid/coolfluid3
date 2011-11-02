e81aea391ca0128d710aaf965ec0ad938b9c7b56
Bart Janssens <bart.janssens@lid.kviv.be>
2011-11-02 21:54:38 +0100

    [FIX] Properly add NDEBUG for all compilers and tweak pointer benchmark

--- test/common/utest-ptr-benchmark.cpp
+++ test/common/utest-ptr-benchmark.cpp
@@ -27,142 +27,151 @@ using namespace cf3::common;
 
 struct PtrFixture : Tools::Testing::TimedTestFixture
 {
-  typedef std::vector< boost::shared_ptr<Uint> > SharedVecT;
-  typedef std::vector< boost::weak_ptr<Uint> > WeakVecT;
-  typedef std::vector< Uint* > RawVecT;
+  typedef boost::shared_ptr<Uint> SharedT;
+  typedef boost::weak_ptr<Uint> WeakT;
+  typedef Uint* RawT;
 
-  SharedVecT& shared_vec()
+  SharedT* shared_vec()
   {
-    static SharedVecT v;
+    static SharedT* v = 0;
+    if(!v)
+      v = new SharedT[vec_size];
     return v;
   }
 
-  WeakVecT& weak_vec()
+  WeakT* weak_vec()
   {
-    static WeakVecT v;
+    static WeakT* v = 0;
+    if(!v)
+      v = new WeakT[vec_size];
     return v;
   }
 
-  RawVecT& raw_vec()
+  RawT* raw_vec()
   {
-    static RawVecT v;
+    static RawT* v = 0;
+    if(!v)
+      v = new RawT[vec_size];
     return v;
   }
 
   static const Uint vec_size = 1000000;
+  static const Uint deref_repeats = 100;
 };
 
 //////////////////////////////////////////////////////////////////////////////
 
 BOOST_FIXTURE_TEST_SUITE( BuildOptions, PtrFixture )
 
-////////////////////////////////////////////////////////////////////////////////
-
-BOOST_AUTO_TEST_CASE ( Initialize )
-{
-  shared_vec().reserve(vec_size);
-  weak_vec().reserve(vec_size);
-  raw_vec().reserve(vec_size);
-
-  BOOST_CHECK(shared_vec().capacity() == vec_size);
-  BOOST_CHECK(weak_vec().capacity() == vec_size);
-  BOOST_CHECK(raw_vec().capacity() == vec_size);
-}
-
 //////////////////////////////////////////////////////////////////////////////
 
 BOOST_AUTO_TEST_CASE ( FillShared )
 {
-  SharedVecT& vec = shared_vec();
+  SharedT* vec = shared_vec();
   for(Uint i = 0; i != vec_size; ++i)
-    vec.push_back(boost::shared_ptr<Uint>(new Uint(i)));
+    vec[i] = boost::shared_ptr<Uint>(new Uint(i));
 }
 
 BOOST_AUTO_TEST_CASE ( FillWeak )
 {
-  SharedVecT& s_vec = shared_vec();
-  WeakVecT& w_vec = weak_vec();
+  SharedT* s_vec = shared_vec();
+  WeakT* w_vec = weak_vec();
   for(Uint i = 0; i != vec_size; ++i)
-    w_vec.push_back(s_vec[i]);
+    w_vec[i] = s_vec[i];
 }
 
 BOOST_AUTO_TEST_CASE ( FillRaw )
 {
-  RawVecT& vec = raw_vec();
+  RawT* vec = raw_vec();
   for(Uint i = 0; i != vec_size; ++i)
-    vec.push_back(new Uint(i));
+    vec[i] = new Uint(i);
 }
 
 BOOST_AUTO_TEST_CASE ( CopyShared )
 {
-  SharedVecT vec = shared_vec();
-  BOOST_CHECK(vec_size == vec.size());
+  SharedT* new_vec = new SharedT[vec_size];
+  SharedT* old_vec = shared_vec();
+  for(Uint i = 0; i != vec_size; ++i)
+    new_vec[i] = old_vec[i];
 }
 
 BOOST_AUTO_TEST_CASE ( CopyWeak )
 {
-  WeakVecT vec = weak_vec();
-  BOOST_CHECK(vec_size == vec.size());
+  WeakT* new_vec = new WeakT[vec_size];
+  WeakT* old_vec = weak_vec();
+  for(Uint i = 0; i != vec_size; ++i)
+    new_vec[i] = old_vec[i];
 }
 
 BOOST_AUTO_TEST_CASE ( CopyRaw )
 {
-  RawVecT vec = raw_vec();
-  BOOST_CHECK(vec_size == vec.size());
+  RawT* new_vec = new RawT[vec_size];
+  RawT* old_vec = raw_vec();
+  for(Uint i = 0; i != vec_size; ++i)
+    new_vec[i] = old_vec[i];
 }
 
 BOOST_AUTO_TEST_CASE ( CheckShared )
 {
-  SharedVecT& vec = shared_vec();
+  SharedT* vec = shared_vec();
   for(Uint i = 0; i != vec_size; ++i)
     BOOST_CHECK(is_not_null(vec[i]));
 }
 
 BOOST_AUTO_TEST_CASE ( CheckWeak)
 {
-  WeakVecT& vec = weak_vec();
+  WeakT* vec = weak_vec();
   for(Uint i = 0; i != vec_size; ++i)
     BOOST_CHECK(!vec[i].expired());
 }
 
 BOOST_AUTO_TEST_CASE ( CheckRaw )
 {
-  RawVecT& vec = raw_vec();
+  RawT* vec = raw_vec();
   for(Uint i = 0; i != vec_size; ++i)
     BOOST_CHECK(is_not_null(vec[i]));
 }
 
 BOOST_AUTO_TEST_CASE ( DerefShared )
 {
-  SharedVecT& vec = shared_vec();
+  SharedT* vec = shared_vec();
   Uint result = 0;
-  for(Uint i = 0; i != vec_size; ++i)
+  for(Uint r = 0; r != deref_repeats; ++r)
   {
-    result += *vec[i];
+    for(Uint i = 0; i != vec_size; ++i)
+    {
+      result += *vec[i];
+    }
   }
-  BOOST_CHECK_EQUAL(((vec_size-1)*vec_size)/2, result);
+  BOOST_CHECK(result);
 }
 
 BOOST_AUTO_TEST_CASE ( DerefWeak)
 {
-  WeakVecT& vec = weak_vec();
+  WeakT* vec = weak_vec();
   Uint result = 0;
-  for(Uint i = 0; i != vec_size; ++i)
+  for(Uint r = 0; r != deref_repeats; ++r)
   {
-    result += *vec[i].lock();
+    for(Uint i = 0; i != vec_size; ++i)
+    {
+      result += *vec[i].lock();
+    }
   }
-  BOOST_CHECK_EQUAL(((vec_size-1)*vec_size)/2, result);
+  BOOST_CHECK(result);
 }
 
 BOOST_AUTO_TEST_CASE ( DerefRaw )
 {
-  RawVecT& vec = raw_vec();
+  RawT* vec = raw_vec();
   Uint result = 0;
-  for(Uint i = 0; i != vec_size; ++i)
+  for(Uint r = 0; r != deref_repeats; ++r)
   {
-    result += *vec[i];
+    for(Uint i = 0; i != vec_size; ++i)
+    {
+      result += *vec[i];
+    }
   }
-  BOOST_CHECK_EQUAL(((vec_size-1)*vec_size)/2, result);
+  BOOST_CHECK(result);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
