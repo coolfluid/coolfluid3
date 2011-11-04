5f48896163fdffd3e46f986a6399f671db8f4656
Bart Janssens <bart.janssens@lid.kviv.be>
2011-11-05 00:51:27 +0100

    [ENH] Issue #154: Add UUCount class with test

---
+++ cf3/common/UUCount.cpp
@@ -0,0 +1,121 @@
+// Copyright (C) 2010-2011 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/uuid/random_generator.hpp>
+#include <boost/uuid/string_generator.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
+#include "common/Assertions.hpp"
+#include "common/BasicExceptions.hpp"
+#include "common/UUCount.hpp"
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+namespace cf3 {
+namespace common {
+
+namespace detail
+{
+  inline const boost::uuids::uuid& static_uuid()
+  {
+    static boost::uuids::uuid uuid = boost::uuids::random_generator()();
+    return uuid;
+  }
+
+  inline Uint static_count()
+  {
+    static Uint offset = 0;
+    ++offset;
+    cf3_always_assert(offset); // Bail out if there is a UUCount overlap
+    return offset;
+  }
+}
+
+UUCount::UUCount() : m_uuid(detail::static_uuid()), m_count(detail::static_count())
+{
+}
+
+UUCount::UUCount(const std::string& str)
+{
+  std::vector<std::string> split_parts;
+  boost::algorithm::split(split_parts, str, boost::algorithm::is_any_of(":"));
+  if(split_parts.size() != 2)
+    throw ParsingFailed(FromHere(), "Did not find exactly one ':' separating UUID and count when parsing UUID string " + str);
+  
+  try
+  {
+    m_uuid = boost::uuids::string_generator()(split_parts.front());
+  }
+  catch(std::runtime_error&)
+  {
+    throw ParsingFailed(FromHere(), "Invalid UUID string: " + split_parts.front());
+  }
+  
+  try
+  {
+    m_count = boost::lexical_cast<Uint>(split_parts.back());
+  }
+  catch(boost::bad_lexical_cast& e)
+  {
+    throw ParsingFailed(FromHere(), "Error casting this string to Uint: " + split_parts.back());
+  }
+}
+
+
+std::string UUCount::string() const
+{
+  return boost::lexical_cast<std::string>(m_uuid) + ":" + boost::lexical_cast<std::string>(m_count);
+}
+
+
+const boost::uuids::uuid& UUCount::uuid() const
+{
+  return m_uuid;
+}
+
+
+Uint UUCount::count() const
+{
+  return m_count;
+}
+
+bool UUCount::operator!=(UUCount const& rhs) const
+{
+  return this->m_count != rhs.m_count || this->m_uuid != rhs.m_uuid;
+}
+
+bool UUCount::operator==(UUCount const& rhs) const
+{
+  return !(*this != rhs);
+}
+
+bool UUCount::operator<(UUCount const& rhs) const
+{
+  if(this->m_uuid == rhs.m_uuid)
+    return this->m_count < rhs.m_count;
+  
+  return this->m_uuid < rhs.m_uuid;
+}
+
+bool UUCount::operator>(UUCount const& rhs) const
+{
+  return rhs < *this;
+}
+
+bool UUCount::operator<=(UUCount const& rhs) const
+{
+  return !(rhs < *this);
+}
+
+bool UUCount::operator>=(UUCount const& rhs) const
+{
+  return !(*this < rhs);
+}
+
+} // common
+} // cf3
---
+++ test/common/utest-uucount.cpp
@@ -0,0 +1,58 @@
+// Copyright (C) 2010-2011 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "Test module for cf3::Component"
+
+#include <boost/test/unit_test.hpp>
+
+#include "common/BasicExceptions.hpp"
+#include "common/UUCount.hpp"
+
+using namespace cf3;
+using namespace cf3::common;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_SUITE( UUCountSuite )
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE( UUCountCompare )
+{
+  UUCount a, b;
+  UUCount c = a;
+  
+  BOOST_CHECK(a != b);
+  BOOST_CHECK(b > a);
+  BOOST_CHECK(a < b);
+  
+  BOOST_CHECK(a == c);
+  BOOST_CHECK(a >= c);
+  BOOST_CHECK(a <= c);
+}
+
+BOOST_AUTO_TEST_CASE( UUSerialization )
+{
+  UUCount a;
+  UUCount b(a.string());
+  BOOST_CHECK(a == b);
+  
+  BOOST_CHECK_THROW(UUCount("lala"), ParsingFailed);
+  BOOST_CHECK_THROW(UUCount("lala:lolo"), ParsingFailed);
+  BOOST_CHECK_THROW(UUCount("lala:1"), ParsingFailed);
+  BOOST_CHECK_THROW(UUCount("01234567-89ab-cdef-0123-456789abcdef:lala"), ParsingFailed);
+  
+  UUCount c("01234567-89ab-cdef-0123-456789abcdef:1");
+  BOOST_CHECK_EQUAL(c.string(), "01234567-89ab-cdef-0123-456789abcdef:1");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_SUITE_END()
+
+////////////////////////////////////////////////////////////////////////////////
