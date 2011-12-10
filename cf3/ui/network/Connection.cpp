4c2cdec5d0b0ecba09ac5daaeb365b7a5f7c9827
Quentin Gasper <gquentin@gmail.com>
2011-12-10 12:27:02 +0100

    [ENH] added coolfluid_ui_network library

---
+++ cf3/ui/network/Connection.cpp
@@ -0,0 +1,170 @@
+// Copyright (C) 2010-2011 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include <iomanip> // for std::setw()
+
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include "common/StringConversion.hpp"
+
+#include "common/XML/SignalFrame.hpp"
+#include "common/XML/FileOperations.hpp"
+
+#include "ui/network/ErrorHandler.hpp"
+#include "ui/network/Connection.hpp"
+
+using namespace boost;
+using namespace boost::asio::ip;
+using namespace cf3::common;
+using namespace cf3::common::XML;
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace cf3 {
+namespace ui {
+namespace network {
+
+//////////////////////////////////////////////////////////////////////////////
+
+TCPConnection::Ptr TCPConnection::create( asio::io_service & ios )
+{
+  return Ptr( new TCPConnection(ios) );
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+TCPConnection::TCPConnection( asio::io_service & io_service )
+  : m_socket(io_service),
+    m_incoming_data(nullptr),
+    m_incoming_data_size(0)
+{
+
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+TCPConnection::~TCPConnection()
+{
+  delete m_incoming_data;
+
+  try
+  {
+    // shutdown both reading and writing async ops => "shutdown_both"
+    m_socket.shutdown( tcp::socket::shutdown_both );
+    m_socket.close();
+  }
+  catch( system::error_code & e )
+  {
+    error("An error occured while the socket was shutting down: " + e.message());
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+void TCPConnection::prepare_write_buffers( SignalArgs & args,
+                                           std::vector<asio::const_buffer> & buffers )
+{
+  cf3_assert( args.node.is_valid() );
+
+  // prepare the outgoing data: flush to XML and convert to string
+  args.flush_maps();
+  XML::to_string( *args.xml_doc.get(), m_outgoing_data );
+
+  // create the header on HEADER_LENGTH characters
+  std::ostringstream header_stream;
+
+  header_stream << std::setw(HEADER_LENGTH) << m_outgoing_data.length();
+
+  m_outgoing_header = header_stream.str();
+
+  // write header and data to buffers and then on the socket
+  buffers.push_back( asio::buffer(m_outgoing_header) );
+  buffers.push_back( asio::buffer(m_outgoing_data) );
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void TCPConnection::process_header()
+{
+  std::string header_str = std::string( m_incoming_header, HEADER_LENGTH );
+
+  try
+  {
+    // trim the string to remove the leading spaces (cast fails if spaces are present)
+    boost::algorithm::trim( header_str );
+    m_incoming_data_size = boost::lexical_cast<cf3::Uint> ( header_str );
+
+    // destroy old buffer and allocate the new one
+    delete[] m_incoming_data;
+    m_incoming_data = new char[m_incoming_data_size];
+
+  }
+  catch ( boost::bad_lexical_cast & blc ) // thrown by from_str()
+  {
+    error( "Could not cast frame header to unsigned int (header content was ["
+           + header_str + "]).");
+  }
+  catch ( cf3::common::Exception & cfe )
+  {
+    error(cfe.what());
+  }
+  catch ( std::exception & stde )
+  {
+    error(stde.what());
+  }
+  catch ( ... ) // this function should catch all exception, since it is
+  {             // called by some kind of event handler from boost.
+    error("An unknown exception has been raised during frame header processsing.");
+  }
+
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void TCPConnection::parse_frame_data( SignalFrame &args )
+{
+  try
+  {
+    std::string frame( m_incoming_data, m_incoming_data_size );
+
+    args = SignalFrame( cf3::common::XML::parse_string( frame ) );
+  }
+
+  catch ( cf3::common::Exception & cfe )
+  {
+   error(cfe.what());
+  }
+  catch ( std::exception & stde )
+  {
+    error(stde.what());
+  }
+  catch ( ... ) // this function should catch all exception, since it is called
+  {             // by some kind of event handler from boost.
+    error("An unknown exception has been raised during frame data processsing.");
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void TCPConnection::set_error_handler( boost::weak_ptr<ErrorHandler const> handler)
+{
+  m_error_handler = handler;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void TCPConnection::error( const std::string & message ) const
+{
+  if( boost::shared_ptr<ErrorHandler const> h = m_error_handler.lock() )
+    h->error(message);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+} // network
+} // ui
+} // cf3
---
+++ cf3/ui/network/Connection.hpp
@@ -0,0 +1,244 @@
+// Copyright (C) 2010-2011 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#ifndef cf3_ui_network_connection_hpp
+#define cf3_ui_network_connection_hpp
+
+#include <boost/asio/ip/tcp.hpp>           // TCP related classes
+#include <boost/asio/placeholders.hpp>     // for placholder::error_code
+#include <boost/asio/read.hpp>             // for async_read()
+#include <boost/asio/write.hpp>            // for async_write()
+#include <boost/bind/bind.hpp>             // for boost::bind()
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/tuple/tuple.hpp>           // for managing multiple callback fcts
+#include <boost/variant/get.hpp>           // for calling callback functions
+
+#include "ui/network/LibNetwork.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+
+namespace cf3 {
+
+namespace common
+{
+namespace XML { class SignalFrame; }
+}
+
+namespace ui {
+namespace network {
+
+//////////////////////////////////////////////////////////////////////////////
+
+class ErrorHandler;
+
+/// Manages a TCP connection between to entities where one is a
+/// server and the other one is a client.@n
+
+/// This class is intented to be used in an asynchronous network architecture.
+/// Therefore, a @c TCPConnection object cannot live outside of a shared
+/// pointer because a such object has to maintain asynchronous operations.
+/// One cannot predict when all of those operations will be completed, using a
+/// shared pointer garantees the connection stays alive until all operations
+/// are done. @c
+
+/// Frames handled by this class have two main parts:
+/// @li A size-fixed header (8 bytes): contains the size in bytes of the frame
+/// data.
+/// @li Frame data: actual data that is sent.
+/// The header is complitely tansparent to the calling code and is use as a
+/// guard to check that all data has arrived and allocate the correct buffer
+/// for the reading process.
+
+/// A TCP connection is based on an I/O service that handles the asynchronous
+/// operations and calls an appropriate function when of those is completed. @n
+
+/// This class can be used in both client and server applications. However, an
+/// additional step is need on the server-side: open a network connection and
+/// start accepting new clients connections. @n
+
+/// @note Due to template code, a lot of Boost headers are included by this
+/// file, which might increase the compilation time. Please consider only
+/// including this file in a limited number of CPP files (typically, one per
+/// application is sufficient).
+
+/// @author Quentin Gasper
+
+
+class Network_API TCPConnection
+      : public boost::enable_shared_from_this<TCPConnection>
+{
+
+public: // typedefs
+
+  typedef boost::shared_ptr<TCPConnection> Ptr;
+  typedef boost::shared_ptr<TCPConnection const> ConstPtr;
+
+public:
+
+  /// Creates a @c TCPConnection instance.
+  /// @param io_service The I/O service the new connection will be based on.
+  /// @return Returns the created instance as a boost share pointer.
+  static Ptr create( boost::asio::io_service & ios );
+
+  /// Destructor.
+  /// Cleanly closes the socket.
+  ~TCPConnection();
+
+  /// Gives a referemce to the internal socket.
+  /// @return Returns a reference to the internal socket.
+  boost::asio::ip::tcp::socket & socket()
+  {
+    return m_socket;
+  }
+
+  /// Gives a constant referemce to the internal socket.
+  /// @return Returns a constant reference to the internal socket.
+  const boost::asio::ip::tcp::socket & socket() const
+  {
+    return m_socket;
+  }
+
+  /// Sends a message to the remote entity.
+  /// The message is sent asynchronously and the function returns directly,
+  /// before the data is actually send.
+  /// @param data The XML data to send. Must be valid.
+  /// @param callback_function The function to call when sending is finished
+  /// (success or failure).
+  template<typename HANDLER>
+  void send( cf3::common::XML::SignalFrame & args, HANDLER callback_function )
+  {
+    std::vector<boost::asio::const_buffer> buffers;
+
+    prepare_write_buffers( args, buffers );
+
+    boost::asio::async_write( m_socket, buffers, callback_function );
+  }
+
+  /// Initiates an asynchronous reading from the remote entity.
+  /// The function returns directly.
+  template< typename HANDLER >
+  void read( cf3::common::XML::SignalFrame & args,  HANDLER callback_function )
+  {
+
+    using namespace boost;
+
+    // initiate the async read for the header and bind the callback function
+    asio::async_read( m_socket,
+                      asio::buffer( m_incoming_header ),
+                      boost::bind( &TCPConnection::callback_header_read<HANDLER>,
+                                   shared_from_this(),
+                                   boost::ref( args ),
+                                   shared_from_this(),
+                                   boost::asio::placeholders::error,
+                                   boost::make_tuple( callback_function )
+                                 )
+                    );
+  }
+
+  void set_error_handler ( boost::weak_ptr<ErrorHandler const> handler );
+
+private: // functions
+
+  /// Function called when a reading operation is completed, successfully or not.
+  /// @param error Describes the error that occured, if any.
+  template< typename HANDLER >
+  void callback_header_read( cf3::common::XML::SignalFrame & args,
+                             TCPConnection::Ptr conn,
+                             const boost::system::error_code & error,
+                             boost::tuple<HANDLER> functions )
+  {
+    using namespace boost;
+
+    if ( !error )
+    {
+      process_header();
+
+      // initiate an async read to get the frame data
+      asio::async_read( m_socket,
+                        asio::buffer( m_incoming_data, m_incoming_data_size ),
+                        boost::bind( &TCPConnection::callback_data_read<HANDLER>,
+                                     shared_from_this(),
+                                     boost::ref( args ),
+                                     shared_from_this(),
+                                     boost::asio::placeholders::error,
+                                     functions
+                                     )
+                        );
+    }
+    else
+    {
+      boost::get<0>( functions )( error );
+    }
+  }
+
+  /// Function called when the frame data has been read
+  template< typename HANDLER >
+  void callback_data_read( cf3::common::XML::SignalFrame & args,
+                           TCPConnection::Ptr conn,
+                           const boost::system::error_code & error,
+                           boost::tuple<HANDLER> functions )
+  {
+    if ( !error )
+      parse_frame_data( args );
+
+    boost::get<0>( functions )( error );
+  }
+
+private: // functions
+
+  /// Constructor.
+  /// @param io_service The I/O service the connection will be based on.
+  TCPConnection( boost::asio::io_service& io_service );
+
+  void prepare_write_buffers( cf3::common::XML::SignalFrame & args,
+                              std::vector<boost::asio::const_buffer> & buffers );
+
+  void process_header ();
+
+  void parse_frame_data ( cf3::common::XML::SignalFrame & args );
+
+  void error( const std::string & message ) const;
+
+private: // data
+
+  /// Network socket.
+  boost::asio::ip::tcp::socket m_socket;
+
+  /// Buffer for outgoing data
+  std::string m_outgoing_data;
+
+  /// Buffer for outgoing header
+  std::string m_outgoing_header;
+
+  /// Nameless enum for header length
+  enum { HEADER_LENGTH = 8 };
+
+  /// Buffer the receiving header.
+  char m_incoming_header[HEADER_LENGTH];
+
+  /// Size of the receiving buffer.
+  unsigned int m_incoming_data_size;
+
+  /// Receiving buffer.
+  /// @warning This buffer does NOT finish by '\0'. Its size is given by
+  /// @c m_incoming_data_size.
+  char * m_incoming_data;
+
+  /// Weak pointer to the error handler.
+  boost::weak_ptr<ErrorHandler const> m_error_handler;
+
+}; // TCPConnection
+
+//////////////////////////////////////////////////////////////////////////////
+
+} // network
+} // ui
+} // cf3
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif // cf3_ui_network_connection_hpp
