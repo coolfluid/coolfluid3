f91bd6e8b445836e77040b1b6133f347d5f66e37
Willem Deconinck <wdeconinck@me.com>
2012-02-17 10:35:00 +0100

    [FIX] clang compiler warnings

--- plugins/UFEM/src/UFEM/NavierStokes.hpp
+++ plugins/UFEM/src/UFEM/NavierStokes.hpp
@@ -12,6 +12,9 @@
 #include <boost/accumulators/statistics/max.hpp>
 
 #define BOOST_PROTO_MAX_ARITY 10
+#ifdef BOOST_MPL_LIMIT_METAFUNCTION_ARITY
+  #undef BOOST_MPL_LIMIT_METAFUNCTION_ARITY
+#endif
 #define BOOST_MPL_LIMIT_METAFUNCTION_ARITY 10
 
 #include <boost/scoped_ptr.hpp>
