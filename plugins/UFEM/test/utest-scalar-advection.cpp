68e0a2ea3e892053309ae3627eaf2db2d05a1833
Sebastian Scholl <sebastian.scholl@vki.ac.be>
2012-04-06 15:43:07 +0200

    [FIX]utest scalar-advection

--- plugins/UFEM/test/utest-scalar-advection.cpp
+++ plugins/UFEM/test/utest-scalar-advection.cpp
@@ -70,6 +70,15 @@ static boost::proto::terminal< void(*)(Real, Real, Real) >::type const _check_cl
 struct ProtoHeatFixture
 {
   ProtoHeatFixture() :
+    length(1.),
+    Pe(0.0001),
+    Pe2(1.),
+    left_temp(1.),
+    right_temp(0.),
+    nb_segments(10),
+    alpha(1.),
+
+
     root( Core::instance().root() )
   {
     solver_config = boost::unit_test::framework::master_test_suite().argv[1];
@@ -78,7 +87,32 @@ struct ProtoHeatFixture
   Component& root;
   std::string solver_config;
 
+  /// Write the analytical solution
+  void set_analytical_solution(Region& region, const std::string& field_name, const std::string& var_name)
+  {
+    MeshTerm<0, ScalarField > Temp(field_name, var_name);
+
+      // Zero the field
+      for_each_node
+      (
+        region,
+            Temp = (_exp(Pe2*coordinates[0])-1.)/(_exp(Pe)-1.) + 1.
+      );
+
+    }
+
+  const Real length;
+  const Real Pe;
+  const Real Pe2;
+  const Uint nb_segments;
+  const Real alpha;
+  const Real left_temp;
+  const Real right_temp;
+
+  Real t;
+
 };
+
 
 BOOST_FIXTURE_TEST_SUITE( ProtoHeatSuite, ProtoHeatFixture )
 
@@ -93,8 +127,8 @@ BOOST_AUTO_TEST_CASE( Heat1DComponent )
   Core::instance().environment().options().configure_option("log_level", 4u);
 
   // Parameters
-  Real length            = 5.;
-  const Uint nb_segments = 5 ;
+  Real length            = 1.;
+  const Uint nb_segments = 100 ;
 
   // Setup a model
   ModelUnsteady& model = *root.create_component<ModelUnsteady>("Model");
@@ -102,12 +136,13 @@ BOOST_AUTO_TEST_CASE( Heat1DComponent )
   UFEM::LinearSolverUnsteady& solver = *model.create_component<UFEM::LinearSolverUnsteady>("Solver");
 
   math::LSS::System& lss = *model.create_component<math::LSS::System>("LSS");
-  lss.options().configure_option("matrix_builder", std::string("cf3.math.LSS.TrilinosFEVbrMatrix"));
+  lss.options().configure_option("solver", std::string("Trilinos"));
   solver.options().configure_option("lss", lss.handle<math::LSS::System>());
 
   // Proto placeholders
   MeshTerm<0, ScalarField> T("Temperature", UFEM::Tags::solution());
   MeshTerm<1, VectorField> u_adv("AdvectionVelocity","linearized_velocity");
+  MeshTerm<2, ScalarField> temperature_analytical("TemperatureAnalytical", UFEM::Tags::source_terms());
 
   UFEM::SUPGCoeffs c;
   c.mu = 1.7894e-5;
@@ -127,36 +162,14 @@ BOOST_AUTO_TEST_CASE( Heat1DComponent )
   // BCs
   boost::shared_ptr<UFEM::BoundaryConditions> bc = allocate_component<UFEM::BoundaryConditions>("BoundaryConditions");
 
-  RealVector initial_u(1); initial_u.setConstant(20.);
+  RealVector initial_u(1); initial_u.setConstant(1.);
 
   // add the top-level actions (assembly, BC and solve)
   solver
       << create_proto_action("Initialize", nodes_expression(group(T = 0., u_adv = initial_u)))
       << time_loop;
-
   time_loop
     << allocate_component<solver::actions::ZeroLSS>("ZeroLSS")
-//    << create_proto_action
-//    (
-//    "Assembly",
-//    elements_expression
-//    (
-//      allowed_elements,
-//      group
-//      (
-//        _A = _0, _T = 0,
-//        UFEM::compute_tau(u_adv, c),
-//       //Gaussian quadrature:
-//       element_quadrature
-//       (
-//         _A(T,T) +=  transpose(N(T)) * u_adv * nabla(N(T)) + c.tau_su * transpose(nabla(N)) * u_adv * u_adv * nabla(N) +  alpha * transpose(nabla(N)) * N,
-//         _T(T,T) +=  transpose(N(T)) * N(T) + c.tau_su * transpose(nabla(T)) * u_adv * N
-//       ),
-//        solver.system_matrix += _A,
-//        solver.system_matrix += _T
-//      )
-//    )
-//  )
     << create_proto_action
        (
          "Assembly",
@@ -205,6 +218,16 @@ BOOST_AUTO_TEST_CASE( Heat1DComponent )
 
   // Run the solver
   model.simulate();
+
+  // Check result
+  t = model.time().current_time();
+  std::cout << "Checking solution at time " << t << std::endl;
+  for_each_node
+  (
+    mesh.topology(),
+        _check_close(-(_exp(Pe2 * coordinates[0])-1.)/(_exp(Pe2)-1.)+1, T, 1.)
+  );
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
