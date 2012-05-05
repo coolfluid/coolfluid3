c5e90f62ddebadcab3bf0fedd279f35ba645145e
Bart Janssens <bart.janssens@lid.kviv.be>
2012-05-06 00:51:01 +0200

    [ENH] Subdivide hexahedra in the triangulator

--- cf3/mesh/MeshTriangulator.cpp
+++ cf3/mesh/MeshTriangulator.cpp
@@ -13,6 +13,7 @@
 #include "mesh/Mesh.hpp"
 #include "mesh/Region.hpp"
 #include "LagrangeP1/Quad2D.hpp"
+#include "LagrangeP1/Hexa3D.hpp"
 #include "Space.hpp"
 #include "Connectivity.hpp"
 
@@ -34,6 +35,7 @@ MeshTriangulator::~MeshTriangulator()
 void MeshTriangulator::execute()
 {
   std::vector< Handle<Component> > to_remove;
+
   // Convert 2D mesh
   BOOST_FOREACH(Elements& quads, find_components_recursively_with_filter<Elements>(mesh(), IsElementType<LagrangeP1::Quad2D>()))
   {
@@ -58,6 +60,40 @@ void MeshTriangulator::execute()
     }
 
     to_remove.push_back(quads.handle());
+  }
+  
+  // Convert 2D mesh
+  BOOST_FOREACH(Elements& hexas, find_components_recursively_with_filter<Elements>(mesh(), IsElementType<LagrangeP1::Hexa3D>()))
+  {
+    const Uint nb_hexas = hexas.size();
+    Elements& tetras = find_parent_component<Region>(hexas).create_elements("cf3.mesh.LagrangeP1.Tetra3D", hexas.geometry_fields());
+    tetras.resize(nb_hexas*5);
+    const Connectivity& hexa_conn = hexas.geometry_space().connectivity();
+    Connectivity& tetra_conn = tetras.geometry_space().connectivity();
+    
+    // Keeps track of the per-tetrahedron indices
+    Uint tetra_indices[5][4] = {
+      {0, 1, 2, 5},
+      {0, 2, 3, 7},
+      {5, 7, 6, 2},
+      {4, 7, 5, 0},
+      {0, 7, 5, 2}
+    };
+    
+    for(Uint i = 0; i != nb_hexas; ++i)
+    {
+      const Connectivity::ConstRow hexa_row = hexa_conn[i];
+      for(Uint j = 0; j != 5; ++j) // loop over the tetras
+      {
+        Connectivity::Row tetra_row = tetra_conn[i*5+j];
+        for(Uint k = 0; k != 4; ++k) // loop over tetra nodes
+        {
+          tetra_row[k] = hexa_row[tetra_indices[j][k]];
+        }
+      }
+    }
+
+    to_remove.push_back(hexas.handle());
   }
 
   BOOST_FOREACH(const Handle<Component>& comp, to_remove)
