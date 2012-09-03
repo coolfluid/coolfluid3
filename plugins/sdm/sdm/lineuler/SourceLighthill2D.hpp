33084abe0e46b8d189557bdb90a8dbb3e2500d12
Willem Deconinck <wdeconinck@me.com>
2012-09-03 15:27:28 +0200

    [ENH] smd::lineuler::SourceLighthill2D

--- plugins/sdm/sdm/lineuler/SourceLighthill2D.hpp
+++ plugins/sdm/sdm/lineuler/SourceLighthill2D.hpp
@@ -33,13 +33,7 @@ namespace lineuler {
 
 struct SourceLighthillPhysData2D : PhysDataBase<4u,2u>
 {
-  RealVectorNDIM grad_uu;
-  RealVectorNDIM grad_uv;
-  RealVectorNDIM grad_vv;
-  RealVectorNDIM grad_uu_avg;
-  RealVectorNDIM grad_uv_avg;
-  RealVectorNDIM grad_vv_avg;
-
+  RealVectorNDIM lighthill_source;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -67,105 +61,84 @@ public:
   static std::string type_name() { return "SourceLighthill2D"; }
   SourceLighthill2D(const std::string& name) : SourceTerm< PhysData >(name)
   {
-    options().add("lighthill_tensor",m_lighthill_tensor_field)
-      .description("Field containing lighthill tensor")
-      .link_to(&m_lighthill_tensor_field);
+    options().add("lighthill_source_field_past",m_lighthill_source_field_past)
+        .mark_basic()
+        .link_to(&m_lighthill_source_field_past)
+        .attach_trigger( boost::bind( &SourceLighthill2D::config_past, this ) )
+        .description("Vector field containing lighthill source for momentum equations");
 
-    options().add("avg_lighthill_tensor",m_avg_lighthill_tensor_field)
-      .description("Field containing time averaged lighthill tensor")
-      .link_to(&m_avg_lighthill_tensor_field);
 
-//    m_U0 << 0.,0.;
-//    std::vector<Real> U0(NDIM,0.);
-//    options().add("U0", U0)
-//        .description("Uniform mean velocity")
-//        .attach_trigger( boost::bind( &SourceLighthill2D::config_U0, this) );
+    options().add("lighthill_source_field_future",m_lighthill_source_field_future)
+        .description("Vector field containing lighthill source for momentum equations")
+        .link_to(&m_lighthill_source_field_future)
+        .mark_basic()
+        .attach_trigger( boost::bind( &SourceLighthill2D::config_future, this ) );
+
+    options().add("time",m_time).link_to(&m_time).mark_basic();
 
     m_rho0 = 1.;
     options().add("rho0",m_rho0)
         .description("Uniform mean density")
-        .link_to(&m_rho0);
+        .link_to(&m_rho0)
+        .mark_basic();
   }
-
-//  void config_U0()
-//  {
-//    std::vector<Real> U0 = options().value<std::vector<Real> >("U0");
-//    m_U0 = RealVectorNDIM::MapType(&U0.front(),U0.size());
-//  }
 
   virtual ~SourceLighthill2D() {}
 
-
   void compute_sol_pt_phys_data(const SFDElement& elem, const Uint sol_pt, PhysData& phys_data )
   {
-    SourceTerm<PhysData>::compute_sol_pt_phys_data(elem,sol_pt,phys_data);
-    mesh::Field::View sol_pt_lighthill_tensor = m_lighthill_tensor_field->view(elem.space->connectivity()[elem.idx]);
-    mesh::Field::View sol_pt_avg_lighthill_tensor = m_avg_lighthill_tensor_field->view(elem.space->connectivity()[elem.idx]);
+    cf3_always_assert(m_lighthill_source_field_past);
+    cf3_always_assert(m_lighthill_source_field_future);
+    cf3_always_assert(m_time);
 
-    // allocate beforehand
-    RealVector uu(elem.sf->nb_sol_pts());
-    RealVector uv(elem.sf->nb_sol_pts());
-    RealVector vv(elem.sf->nb_sol_pts());
-    RealVector uu_avg(elem.sf->nb_sol_pts());
-    RealVector uv_avg(elem.sf->nb_sol_pts());
-    RealVector vv_avg(elem.sf->nb_sol_pts());
+    mesh::Field::View lighthill_source_field_past   = m_lighthill_source_field_past->view(elem.space->connectivity()[elem.idx]);
+    mesh::Field::View lighthill_source_field_future = m_lighthill_source_field_future->view(elem.space->connectivity()[elem.idx]);
 
-    RealMatrix sf_grad((int)NDIM,elem.sf->nb_sol_pts());
-    elem.sf->compute_gradient(elem.sf->sol_pts().row(sol_pt),sf_grad);
-    for (Uint s=0; s<elem.sf->nb_sol_pts(); ++s)
+    m_time_past = m_lighthill_source_field_past->properties().value<Real>("time");
+    m_time_future = m_lighthill_source_field_future->properties().value<Real>("time");
+
+    cf3_always_assert(m_time->current_time() >= m_time_past);
+    cf3_always_assert(m_time->current_time() <= m_time_future);
+
+    for (Uint d=0; d<NDIM; ++d)
     {
-      // instantaneous Lighthill tensor
-      uu[s] = sol_pt_lighthill_tensor[s][0]; // XX
-      uv[s] = sol_pt_lighthill_tensor[s][1]; // XY
-      vv[s] = sol_pt_lighthill_tensor[s][3]; // YY
-
-      // time averaged Lighthill tensor
-      uu_avg[s] = sol_pt_avg_lighthill_tensor[s][0]; // XX
-      uv_avg[s] = sol_pt_avg_lighthill_tensor[s][1]; // XY
-      vv_avg[s] = sol_pt_avg_lighthill_tensor[s][3]; // YY
+      phys_data.lighthill_source[d] = lighthill_source_field_past[sol_pt][d] + (lighthill_source_field_future[sol_pt][d]-lighthill_source_field_past[sol_pt][d])/(m_time_future-m_time_past) * (m_time->current_time()-m_time_past);
     }
-
-    // compute mapped gradient using sf_grad
-    RealVectorNDIM mapped_grad_uu = sf_grad * uu;
-    RealVectorNDIM mapped_grad_uv = sf_grad * uv;
-    RealVectorNDIM mapped_grad_vv = sf_grad * vv;
-
-    // compute mapped gradient using sf_grad
-    RealVectorNDIM mapped_grad_uu_avg = sf_grad * uu_avg;
-    RealVectorNDIM mapped_grad_uv_avg = sf_grad * uv_avg;
-    RealVectorNDIM mapped_grad_vv_avg = sf_grad * vv_avg;
-
-    // transform to physical space: (dQ/dx, dQ/dy, dQ/dz)
-    RealMatrix geom_coord_matrix = elem.entities->geometry_space().get_coordinates(elem.idx);
-    RealMatrix Jinv; Jinv.resize(NDIM,NDIM);
-    Jinv = elem.entities->element_type().jacobian(elem.sf->sol_pts().row(sol_pt),geom_coord_matrix).inverse();
-    phys_data.grad_uu = Jinv * mapped_grad_uu;
-    phys_data.grad_uv = Jinv * mapped_grad_uv;
-    phys_data.grad_vv = Jinv * mapped_grad_vv;
-    phys_data.grad_uu_avg = Jinv * mapped_grad_uu_avg;
-    phys_data.grad_uv_avg = Jinv * mapped_grad_uv_avg;
-    phys_data.grad_vv_avg = Jinv * mapped_grad_vv_avg;
-
   }
+
 
 
   virtual void compute_source(PhysData& data,
                               RealVectorNEQS& source)
   {
     source[0] = 0.;
-    source[1] = m_rho0 * (data.grad_uu[XX] + data.grad_uv[YY]);
-    source[2] = m_rho0 * (data.grad_uv[XX] + data.grad_vv[YY]);
+    source[1] = m_rho0 * data.lighthill_source[XX];
+    source[2] = m_rho0 * data.lighthill_source[YY];
     source[3] = 0.;
   }
 
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
+  void config_past()
+  {
+    m_time_past = m_lighthill_source_field_past->properties().value<Real>("time");
+  }
+
+  void config_future()
+  {
+    m_time_future = m_lighthill_source_field_future->properties().value<Real>("time");
+  }
+
 private:
 
   Real m_rho0;                                   ///< Mean uniform density
-  Handle<mesh::Field> m_lighthill_tensor_field;  ///< Source velocity field
-  Handle<mesh::Field> m_avg_lighthill_tensor_field;  ///< Source velocity field
-//  RealVectorNDIM m_U0;                           ///< Mean uniform velocity
+  Handle<mesh::Field> m_lighthill_source_field_past;  ///< Source velocity field
+  Handle<mesh::Field> m_lighthill_source_field_future;  ///< Source velocity field
+
+
+  Handle<solver::Time> m_time;
+  Real m_time_past;
+  Real m_time_future;
 
 };
 
