877c93518d1156d5b227208794036ea10f6c57cc
Sebastian Scholl <sebastian.scholl@vki.ac.be>
2012-09-13 11:56:32 +0200

    [ENH] added file for accoustically filtered compressible(variable density) equations, called BoussinesqExtended.

--- plugins/UFEM/src/UFEM/NavierStokes.hpp
+++ plugins/UFEM/src/UFEM/NavierStokes.hpp
@@ -60,6 +60,11 @@ private:
   template<typename GenericElementsT, typename SpecializedElementsT>
   void set_boussinesq_assembly_expression(const std::string& action_name);
 
+  /// Helper function to set the expression, taking into account the user's option to use specializations or not.
+  /// Implemented in BoussinesqAssemblyExtended.hpp
+  template<typename GenericElementsT, typename SpecializedElementsT>
+  void set_boussinesq_assembly_extended_expression(const std::string& action_name);
+
   /// Helper functions to split the compilation over multiple units, to save memory. Each one is in a different cpp file.
   void set_triag_assembly(const bool use_specialization);
   void set_quad_assembly();
@@ -91,6 +96,12 @@ private:
   PhysicsConstant rho;
   PhysicsConstant nu;
 
+  PhysicsConstant temp_ref;
+  PhysicsConstant rho_ref;
+  PhysicsConstant betha;
+  PhysicsConstant cp_heat_capacity;
+  PhysicsConstant kappa_heat_cond;
+  PhysicsConstant g_acceleration;
 
   /// Storage of the stabilization coefficients
   Real tau_ps, tau_su, tau_bulk;
