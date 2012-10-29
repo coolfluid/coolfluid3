aa4c87d887fbee97d143d0a2597fc8e65875c82a
Bart Janssens <bart.janssens@lid.kviv.be>
2012-10-29 15:52:46 +0100

    [ENH] UFEM: Add some heat conduction solvers for performance comparisons

--- plugins/UFEM/src/UFEM/HeatConductionSteady.cpp
+++ plugins/UFEM/src/UFEM/HeatConductionSteady.cpp
@@ -44,13 +44,14 @@ struct HeatSpecialized
   {
     typedef mesh::LagrangeP1::Triag2D ElementT;
     const ElementT::NodesT& nodes = T.support().nodes();
-    const Real c = k / (4.*T.support().volume());
-    
+
     ElementT::NodesT normals;
     normals(0, XX) = nodes(1, YY) - nodes(2, YY); normals(0, YY) = nodes(2, XX) - nodes(1, XX);
     normals(1, XX) = nodes(2, YY) - nodes(0, YY); normals(1, YY) = nodes(0, XX) - nodes(2, XX);
     normals(2, XX) = nodes(0, YY) - nodes(1, YY); normals(2, YY) = nodes(1, XX) - nodes(0, XX);
-    
+
+    const Real c = k / (2.*(normals(2, YY)*normals(1, XX) - normals(1, YY)*normals(2, XX)));
+
     for(Uint i = 0; i != 3; ++i)
       for(Uint j = 0; j != 3; ++j)
         A(i, j) = c * (normals(i, XX)*normals(j, XX) + normals(i, YY)*normals(j, YY));
@@ -65,40 +66,40 @@ HeatConductionSteady::HeatConductionSteady ( const std::string& name ) : LSSActi
     .pretty_name("Heat Space Name")
     .description("The space to use for the heat source term")
     .attach_trigger(boost::bind(&HeatConductionSteady::trigger, this));
-    
+
   options().add("use_specializations", true)
     .pretty_name("Use specializations")
     .description("Use sepcialized code if available")
     .attach_trigger(boost::bind(&HeatConductionSteady::trigger, this));
-    
+
   set_solution_tag("heat_conduction_solution");
-  
+
   create_component<ZeroLSS>("ZeroLSS");
   m_assembly = create_component<ProtoAction>("Assembly");
-  
+
   Handle<BoundaryConditions> bc = create_component<BoundaryConditions>("BoundaryConditions");
   bc->mark_basic();
   bc->set_solution_tag(solution_tag());
-  
+
   create_component<SolveLSS>("SolveLSS");
   m_update = create_component<ProtoAction>("SetSolution");
 
   trigger();
-  
+
   configure_option_recursively(solver::Tags::physical_model(), m_physical_model);
 }
 
 void HeatConductionSteady::trigger()
 {
   const bool use_specializations = options().option("use_specializations").value<bool>();
-  
+
   ConfigurableConstant<Real> k("k", "Thermal conductivity (J/(mK))", 1.);
   ConfigurableConstant<Real> relaxation_factor_hc("relaxation_factor_hc", "factor for relaxation in case of coupling", 0.5);
 
   FieldVariable<0, ScalarField> T("Temperature", "heat_conduction_solution");
   FieldVariable<1, ScalarField> q("Heat", "source_terms", options().option("heat_space_name").value<std::string>());
 
-  
+
   if(use_specializations)
   {
     boost::proto::terminal< RestrictToElementTypeTag< boost::mpl::vector5<LagrangeP1::Line1D, LagrangeP1::Tetra3D, LagrangeP1::Quad2D, LagrangeP1::Hexa3D, LagrangeP2::Line1D> > >::type generic_elements;
@@ -117,7 +118,7 @@ void HeatConductionSteady::trigger()
         ),
         specialized_elements(heat_specialized(T, k, _A(T))),
         system_matrix +=  _A,
-        system_rhs += -_A * _x// + integral<2>(transpose(N(T))*N(q)*jacobian_determinant) * nodal_values(q)
+        system_rhs += -_A * _x + integral<2>(transpose(N(T))*N(q)*jacobian_determinant) * nodal_values(q)
       )
     ));
   }
@@ -134,11 +135,11 @@ void HeatConductionSteady::trigger()
           _A(T) += k * transpose(nabla(T)) * nabla(T)
         ),
         system_matrix +=  _A,
-        system_rhs += -_A * _x// + integral<2>(transpose(N(T))*N(q)*jacobian_determinant) * nodal_values(q)
+        system_rhs += -_A * _x + integral<2>(transpose(N(T))*N(q)*jacobian_determinant) * nodal_values(q)
       )
     ));
   }
-    
+
   m_update->set_expression(nodes_expression(T += relaxation_factor_hc*solution(T)));     // Set the solution
 }
 
