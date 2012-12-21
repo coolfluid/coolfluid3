f4261645a0e07305afdf4d2cc33f7e528899b125
Tiago Quintino <tlmquintino@gmail.com>
2012-12-21 00:24:02 +0000

    [FIX] finally fix trilinos link... maybe... possibly...

--- cmake/FindTrilinos.cmake
+++ cmake/FindTrilinos.cmake
@@ -8,92 +8,40 @@
 #   TRILINOS_LIBRARIES
 #   CF3_HAVE_TRILINOS
 #
+
 option( CF3_SKIP_TRILINOS "Skip search for Trilinos library" OFF )
 
-if( NOT CF3_HAVE_TRILINOS ) # skip if already have
+if( NOT CF3_SKIP_TRILINOS )
 
-set(CF3_TRILINOS_EXTRA_LIBS "" CACHE  STRING "Extra libraries needed to link with Trilinos")
+    # Try to find Trilinos using Trilinos recommendations
 
-# Try to find Trilinos using Trilinos recommendations
+    if( DEFINED TRILINOS_HOME )
+        find_package(Trilinos PATHS ${TRILINOS_HOME}/lib/cmake/Trilinos ${TRILINOS_HOME}/include )
+    endif()
 
-if( DEFINED TRILINOS_HOME )
-    find_package(Trilinos PATHS ${TRILINOS_HOME}/lib/cmake/Trilinos ${TRILINOS_HOME}/include )
-endif()
+    if( DEFINED DEPS_ROOT )
+        find_package(Trilinos PATHS ${DEPS_ROOT}/lib/cmake/Trilinos ${DEPS_ROOT}/include )
+    endif()
 
-if( DEFINED DEPS_ROOT )
-    find_package(Trilinos PATHS ${DEPS_ROOT}/lib/cmake/Trilinos ${DEPS_ROOT}/include )
-endif()
+    if( Trilinos_FOUND )
 
-if( Trilinos_FOUND )
+        set( TRILINOS_INCLUDE_DIRS "" )
 
-    set( TRILINOS_INCLUDE_DIRS "" )
-    list( APPEND TRILINOS_INCLUDE_DIRS ${Trilinos_INCLUDE_DIRS})
-    list( APPEND TRILINOS_INCLUDE_DIRS ${Trilinos_TPL_INCLUDE_DIRS})
+        list( APPEND TRILINOS_INCLUDE_DIRS ${Trilinos_INCLUDE_DIRS} )
+        list( APPEND TRILINOS_INCLUDE_DIRS ${Trilinos_TPL_INCLUDE_DIRS} )
 
-    foreach( test_lib ${Trilinos_LIBRARIES})
-      find_library( ${test_lib}_lib ${test_lib} PATHS  ${Trilinos_LIBRARY_DIRS}  NO_DEFAULT_PATH)
-      find_library( ${test_lib}_lib ${test_lib})
-      mark_as_advanced( ${test_lib}_lib )
-      list( APPEND TRILINOS_LIBRARIES ${${test_lib}_lib} )
-    endforeach()
+        foreach( test_lib ${Trilinos_LIBRARIES} )
+          find_library( ${test_lib}_lib ${test_lib} PATHS  ${Trilinos_LIBRARY_DIRS}  NO_DEFAULT_PATH)
+          find_library( ${test_lib}_lib ${test_lib})
+          mark_as_advanced( ${test_lib}_lib )
+          list( APPEND TRILINOS_LIBRARIES ${${test_lib}_lib} )
+        endforeach()
 
-    list( APPEND TRILINOS_LIBRARIES ${Trilinos_TPL_LIBRARIES} )
+        list( APPEND TRILINOS_LIBRARIES ${Trilinos_TPL_LIBRARIES} )
 
-else()
+    endif()
 
-  # Try to find Trilinos the hard way
-
-  coolfluid_set_trial_include_path("") # clear include search path
-  coolfluid_set_trial_library_path("") # clear library search path
-
-  coolfluid_add_trial_include_path( ${TRILINOS_HOME}/include )
-  coolfluid_add_trial_include_path( $ENV{TRILINOS_HOME}/include )
-
-  coolfluid_add_trial_library_path(${TRILINOS_HOME}/lib )
-  coolfluid_add_trial_library_path($ENV{TRILINOS_HOME}/lib)
-
-  find_path( TRILINOS_INCLUDE_DIRS Epetra_SerialComm.h PATHS ${TRIAL_INCLUDE_PATHS}  NO_DEFAULT_PATH )
-  find_path( TRILINOS_INCLUDE_DIRS Epetra_SerialComm.h )
-
-  list( APPEND trilinos_req_libs
-      epetra
-      teuchos
-      stratimikosamesos
-      stratimikosaztecoo
-      stratimikosbelos
-      stratimikosifpack
-      stratimikosml
-      stratimikos
-      aztecoo
-      ml
-      belos
-      ifpack
-      thyraepetra
-      thyratpetra
-      thyraepetraext
-      thyracore
-  )
-
-  foreach( test_lib ${trilinos_req_libs} )
-    find_library( ${test_lib}_lib ${test_lib} PATHS  ${TRIAL_LIBRARY_PATHS}  NO_DEFAULT_PATH)
-    find_library( ${test_lib}_lib ${test_lib})
-    mark_as_advanced( ${test_lib}_lib )
-    list( APPEND TRILINOS_LIBRARIES ${${test_lib}_lib} )
-  endforeach()
-
-  if( CF3_HAVE_PARMETIS )
-    list( APPEND TRILINOS_LIBRARIES ${PARMETIS_LIBRARIES} )
-    list( APPEND TRILINOS_INCLUDE_DIRS ${PARMETIS_INCLUDE_DIRS} )
-  endif()
-
-endif()
-
-  if( CF3_HAVE_PTSCOTCH )
-    list( APPEND TRILINOS_LIBRARIES ${PTSCOTCH_LIBRARIES} )
-    list( APPEND TRILINOS_INCLUDE_DIRS ${PTSCOTCH_INCLUDE_DIRS} )
-  endif()
-
-list(APPEND TRILINOS_LIBRARIES ${CF3_TRILINOS_EXTRA_LIBS})
+endif( NOT CF3_SKIP_TRILINOS )
 
 coolfluid_log("TRILINOS_INCLUDE_DIRS = ${TRILINOS_INCLUDE_DIRS}" )
 coolfluid_log("TRILINOS_LIBRARIES = ${TRILINOS_LIBRARIES}" )
@@ -110,5 +58,3 @@ if( Trilinos_FOUND )
 else()
     set( CF3_HAVE_TRILINOS 0 )
 endif()
-
-endif( NOT CF3_HAVE_TRILINOS )
