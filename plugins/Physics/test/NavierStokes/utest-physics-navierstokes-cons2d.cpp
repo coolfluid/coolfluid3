0832400d9eec6fe073b5afdd5669485764cd7894
Willem Deconinck <wdeconinck@me.com>
2013-01-29 21:45:20 +0000

    [FIX] compilation error

--- plugins/Physics/test/NavierStokes/utest-physics-navierstokes-cons2d.cpp
+++ plugins/Physics/test/NavierStokes/utest-physics-navierstokes-cons2d.cpp
@@ -13,11 +13,11 @@
 #include "cf3/common/Core.hpp"
 #include "cf3/common/Environment.hpp"
 
-#include "cf3/physics/NavierStokes/Cons2D.hpp"
+#include "cf3/physics/navierstokes/navierstokes2d/Functions.hpp"
 
 using namespace cf3;
 using namespace cf3::common;
-using namespace cf3::physics::NavierStokes;
+using namespace cf3::physics::navierstokes::navierstokes2d;
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -25,35 +25,24 @@ BOOST_AUTO_TEST_SUITE( NavierStokes_Cons2D_Suite )
 
 //////////////////////////////////////////////////////////////////////////////
 
-BOOST_AUTO_TEST_CASE( compute_properties )
+BOOST_AUTO_TEST_CASE( test_navierstokes2d )
 {
-#if 0 // work in progress
-  // create the model
+    Data p;
 
-  physics::PhysModel& pmodel =
-      Core::instance().root().create_component<NavierStokes2D>( "pmodel" );
+    p.mu = 4.;
+    p.k = 0.5;
+    p.U[0] = 2.;
+    p.U[1] = 2.;
+    p.grad_u << 3., 2.;
+    p.grad_v << 2., 3.;
+    p.grad_T << 3., 3.;
 
-  // values for testing
-
-  NavierStokes2D::GeoV coords;
-
-  coords[XX] = 0.0;
-  coords[YY] = 2.0;
-	
-  NavierStokes2D::SolV vars;
-	
-	vars[Cons2D::Rho ] = 1.0;
-	vars[Cons2D::RhoU] = 2.83972;
-	vars[Cons2D::RhoV] = 0.0;
-	vars[Cons2D::RhoE] = 6.532;
-
-  NavierStokes2D::SolM grad_vars;
-#endif
-
-}
-
-BOOST_AUTO_TEST_CASE( flux )
-{
+    RowVector_NEQS flux;
+    ColVector_NDIM normal;
+    normal << 1., 1.;
+    compute_diffusive_flux(p,normal,flux);
+    RowVector_NEQS check; check << 0, 24, 24, 99;
+    BOOST_CHECK(flux == check);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
