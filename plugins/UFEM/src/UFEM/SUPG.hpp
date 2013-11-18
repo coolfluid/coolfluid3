8a9f41a42c34df0a3d05d67e672b245a3539459d
Bart Janssens <bart@bartjanssens.org>
2013-11-18 20:34:33 +0100

    [DEV] UFEM: More SUPG coefficient adjustment

--- plugins/UFEM/src/UFEM/SUPG.hpp
+++ plugins/UFEM/src/UFEM/SUPG.hpp
@@ -57,21 +57,11 @@ struct ComputeTau
   template<typename UT, typename NUT>
   void operator()(const UT& u, const NUT& nu_eff, const Real& u_ref, const Real& dt, Real& tau_ps, Real& tau_su, Real& tau_bulk) const
   {
-    typedef typename UT::EtypeT ElementT;
-
     // Average viscosity
     Real element_nu = fabs(nu_eff.value().mean());
     
-    const Real he = UT::dimension == 2 ? sqrt(4./3.141592654*u.support().volume()) : ::pow(3./4./3.141592654*u.support().volume(),1./3.);
-    const Real ree=u_ref*he/(2.*element_nu);
-    cf3_assert(ree > 0.);
-    const Real xi = ree < 3. ? 0.3333333333333333*ree : 1.;
-    tau_ps = he*xi/(2.*u_ref);
-    tau_bulk = he*u_ref/xi;
-    
-    const Real umag = compute_tau_su(u, element_nu, dt, tau_su);
-    tau_bulk = tau_su*umag*umag;
-    //std::cout << "old tau_ps: " << tau_ps << ",  tau_su:" << tau_su << ", old tau_bu: " << tau_bulk << " new tau_bu: " << 1./(tau_su * tr_gij) << std::endl;
+    tau_bulk = compute_tau_su(u, element_nu, dt, tau_su);
+    tau_ps = tau_su;
   }
 
   /// Only compute the SUPG coefficient
@@ -97,18 +87,12 @@ struct ComputeTau
     // cell velocity
     u.compute_values(GaussT::instance().coords.col(0));
     Eigen::Matrix<Real, ElementT::dimensionality, ElementT::dimensionality> gij; // metric tensor
-    gij = u.nabla()*u.nabla().transpose();
+    gij.noalias() = u.nabla()*u.nabla().transpose();
     const Real c2 = 36.;
-
-    //tau_su = 1. / sqrt((4./(dt*dt)) + (u.eval()*gij*detail::transpose(u.eval()))[0] + c2*element_nu*element_nu*gij.squaredNorm());
     
-    const Real umag = std::max(detail::norm(u.eval()), 1e-8);
-
-    const Real h = 2.*umag / (u.eval()*u.nabla()).sum();
-    tau_su = 1./((u.eval()*u.nabla()).sum() + 2./dt + (4.*element_nu)/(h*h));
-
-    
-    return umag;
+    const Real tau_adv_sq = fabs((u.eval()*gij*detail::transpose(u.eval()))[0]); // Very close 0 but slightly negative sometimes
+    tau_su = 1. / sqrt((4./(dt*dt)) + tau_adv_sq + c2*element_nu*element_nu*gij.squaredNorm());
+    return tau_adv_sq < 1e-13 ? 0 : sqrt(tau_adv_sq)/gij.trace();
   }
 };
 
