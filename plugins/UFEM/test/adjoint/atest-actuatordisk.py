ebbe1968e3c1b6c3f0f6b51753f30f4945d749aa
cf3 <pierrolerigolo@msn.com>
2016-03-13 18:19:55 +0100

    [DEV] Adjointe

--- plugins/UFEM/test/adjoint/atest-actuatordisk.py
+++ plugins/UFEM/test/adjoint/atest-actuatordisk.py
@@ -11,44 +11,51 @@ rho = 1.225
 mu = 0.0000178
 D=0.5
 
-tstep = 1.
-num_steps = 3.
+tstep = 0.1
+num_steps = 5.
 
 env = cf.Core.environment()
 env.log_level = 4
 
 # Basic model setup (container of all the sumilation setup)
 model = cf.root.create_component('NavierStokes', 'cf3.solver.ModelUnsteady')
+model1 = cf.root.create_component('Adjoint', 'cf3.solver.ModelUnsteady') 
 
 # The domain contains the mesh
 domain = model.create_domain()
+domain1 = model1.create_domain()
 
 # Physics contains constants such as viscosity
 physics = model.create_physics('cf3.UFEM.KEpsilonPhysics')
+physics1 = model1.create_physics('cf3.UFEM.KEpsilonPhysics')
 # Solver contains all solvers for the problem (conduction, flow, ...)
 solver = model.create_solver('cf3.UFEM.Solver')
+solver1 = model1.create_solver('cf3.UFEM.Solver')
 
 # Add a concrete Navier-Stokes finite element solver
-disk = solver.add_unsteady_solver('cf3.UFEM.adjoint.ActuatorDisk3D')
+disk = solver.add_unsteady_solver('cf3.UFEM.adjoint.ActuatorDisk')
 disk.area = D
 disk.u_in = u_in[0]
 disk.th = 0.05
 disk.ct = 0.267
 ns_solver = solver.add_unsteady_solver('cf3.UFEM.NavierStokes')
-ad_solver = solver.add_unsteady_solver('cf3.UFEM.adjoint.Adjoint')
-mesh = domain.load_mesh(file = cf.URI('actuator2d.msh'), name = 'Mesh')
-
+ad_solver = solver1.add_unsteady_solver('cf3.UFEM.adjoint.Adjoint')
+mesh = domain.load_mesh(file = cf.URI('actuator2df.msh'), name = 'Mesh')
+mesh1 = domain1.load_mesh(file = cf.URI('actuator2df.msh'), name = 'Mesh')
 # actve region
 disk.regions = [mesh.topology.actuator.uri(), mesh.topology.actuator.uri()]
 ns_solver.regions = [mesh.topology.uri()]
-ad_solver.regions = [mesh.topology.uri()]#, mesh.topology.actuator.uri()]
+ad_solver.regions = [mesh1.topology.uri(), mesh1.topology.actuator.uri()]
 # initial conditions
 solver.InitialConditions.navier_stokes_solution.Velocity = initial_velocity
 solver.InitialConditions.density_ratio.density_ratio = 1. # This enables the body force
-solver.InitialConditions.adjoint_solution.AdjVelocity = initial_velocity
+
+solver1.InitialConditions.adjoint_solution.AdjVelocity = initial_velocity
 # set physical constants
 physics.density = rho
 physics.dynamic_viscosity = mu
+physics1.density = rho
+physics1.dynamic_viscosity = mu
 
 # Boundary conditions
 bc = ns_solver.BoundaryConditions
@@ -95,6 +102,9 @@ lss.SolutionStrategy.Parameters.LinearSolverTypes.Belos.SolverTypes.BlockGMRES.n
 time = model.create_time()
 time.time_step = tstep
 time.end_time = num_steps*tstep
+time1 = model1.create_time()
+time1.time_step = tstep
+time1.end_time = num_steps*tstep
 
 # solver.create_fields()
 # solver.InitialConditions.execute()
@@ -102,12 +112,12 @@ time.end_time = num_steps*tstep
 
 # run the simulation
 model.simulate()
-
+model1.simulate()
 # lss.print_system("adjlss.tec")
 # run paraview output.pvtu to see the result
 # writer = domain.create_component('VTKWriter', 'cf3.vtk.MultiblockWriter')
 # writer.mesh = mesh
 # writer.file = cf.URI('output.vtm')
 # writer.execute()
-domain.write_mesh(cf.URI('outputactutest.pvtu'))
+domain1.write_mesh(cf.URI('outputactutest.pvtu'))
 model.print_timing_tree()
