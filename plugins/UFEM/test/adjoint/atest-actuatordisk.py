4063b19a8712e953048b44f593af943bb5746435
cf3 <pierrolerigolo@msn.com>
2016-04-04 10:28:45 +0200

    [DEV] Adjoint kepsilon

--- plugins/UFEM/test/adjoint/atest-actuatordisk.py
+++ plugins/UFEM/test/adjoint/atest-actuatordisk.py
@@ -12,14 +12,15 @@ mu = 0.0000178
 D=0.5
 l0 = 230.60
 I=0.06
-
+area = D
+Ct0=0.36
 k_init = 1.5*(u_in[0]*I)**2
 k_wall = 1.5*(u_in[0]*I)**2
 e_init = 0.09**(3/4)*k_init**1.5/l0
 e_wall = 0.09**(3/4)*k_init**1.5/l0
 
 tstep = 1.
-num_steps = 6.
+num_steps = 3.
 
 env = cf.Core.environment()
 env.log_level = 4
@@ -37,26 +38,33 @@ solver = model.create_solver('cf3.UFEM.Solver')
 
 # Add a concrete Navier-Stokes finite element solver
 disk = solver.add_unsteady_solver('cf3.UFEM.adjoint.ActuatorDisk')
-disk.area = D
+disk.area = area
 disk.u_in = u_in[0]
 disk.th = 0.05
-disk.ct = 0.267
+disk.ct = Ct0
 ns_solver = solver.add_unsteady_solver('cf3.UFEM.NavierStokes')
 ad_solver = solver.add_unsteady_solver('cf3.UFEM.adjoint.Adjoint')
+ad_solver.ct = [Ct0]
+ad_solver.th = 0.05
+ad_solver.area = area
 #k-epsilon
 ke = solver.add_unsteady_solver('cf3.UFEM.StandardKEpsilon')
 ke.options.theta = 1.
 ke.options.l_max = 1000.
+kaea = solver.add_unsteady_solver('cf3.UFEM.adjoint.keAdjoint')
+kaea.options.theta = 1.
+kaea.options.l_max = 1000.
 
 
 
 mesh = domain.load_mesh(file = cf.URI('actuator2d.msh'), name = 'Mesh')
 
-# actve region
+# active region
 disk.regions = [mesh.topology.actuator.uri(), mesh.topology.actuator.uri()]
 ns_solver.regions = [mesh.topology.uri()]
 ke.regions = [mesh.topology.uri()]
 ad_solver.regions = [mesh.topology.uri(), mesh.topology.actuator.uri()]
+kaea.regions = [mesh.topology.uri()]
 # initial conditions
 solver.InitialConditions.navier_stokes_solution.Velocity = initial_velocity
 solver.InitialConditions.density_ratio.density_ratio = 1. # This enables the body force
@@ -66,15 +74,26 @@ ic_k.variable_name = 'k'
 ic_k.value = [str(k_init)]
 ic_k.regions = [mesh.topology.uri()]
 
+ic_ka = solver.InitialConditions.create_initial_condition(builder_name = 'cf3.UFEM.InitialConditionFunction', field_tag = 'keAdjoint_solution')
+ic_ka.variable_name = 'ka'
+ic_ka.value = [str(0.)]
+ic_ka.regions = [mesh.topology.uri()]
+
 ic_epsilon = solver.InitialConditions.create_initial_condition(builder_name = 'cf3.UFEM.InitialConditionFunction', field_tag = 'ke_solution')
 ic_epsilon.variable_name = 'epsilon'
 ic_epsilon.value = [str(e_init)]
 ic_epsilon.regions = [mesh.topology.uri()]
 
+ic_epsilona = solver.InitialConditions.create_initial_condition(builder_name = 'cf3.UFEM.InitialConditionFunction', field_tag = 'keAdjoint_solution')
+ic_epsilona.variable_name = 'epsilona'
+ic_epsilona.value = [str(0.)]
+ic_epsilona.regions = [mesh.topology.uri()]
+
 ic_wall_distance = solver.InitialConditions.create_initial_condition(builder_name = 'cf3.UFEM.InitialConditionFunction', field_tag = 'wall_distance')
 ic_wall_distance.variable_name = 'wall_distance'
 ic_wall_distance.value = ['100']
 ic_wall_distance.regions = [mesh.topology.uri()]
+
 # set physical constants
 physics.density = rho
 physics.dynamic_viscosity = mu
@@ -90,6 +109,18 @@ bca.add_constant_bc(region_name = 'outlet', variable_name = 'AdjPressure').value
 bc = ke.LSS.BoundaryConditions
 bc.add_constant_bc(region_name = 'inlet', variable_name = 'k').value = k_wall
 bc.add_constant_bc(region_name = 'inlet', variable_name = 'epsilon').value = e_wall
+
+bca = kaea.LSS.BoundaryConditions
+bca.add_constant_bc(region_name = 'inlet', variable_name = 'ka').value = 0.
+bca.add_constant_bc(region_name = 'inlet', variable_name = 'epsilona').value = 0.
+bca.add_constant_bc(region_name = 'outlet', variable_name = 'ka').value = 0.
+bca.add_constant_bc(region_name = 'outlet', variable_name = 'epsilona').value = 0.
+bca.add_constant_bc(region_name = 'top', variable_name = 'ka').value = 0.
+bca.add_constant_bc(region_name = 'top', variable_name = 'epsilona').value = 0.
+bca.add_constant_bc(region_name = 'bottom', variable_name = 'ka').value = 0.
+bca.add_constant_bc(region_name = 'bottom', variable_name = 'epsilona').value = 0.
+
+
 # Solver setup
 lss = ns_solver.create_lss(matrix_builder = 'cf3.math.LSS.TrilinosFEVbrMatrix', solution_strategy = 'cf3.math.LSS.TrilinosStratimikosStrategy')
 lss.SolutionStrategy.Parameters.preconditioner_type = 'ML'
@@ -137,6 +168,21 @@ lss.SolutionStrategy.Parameters.LinearSolverTypes.Belos.SolverTypes.BlockGMRES.c
 lss.SolutionStrategy.Parameters.LinearSolverTypes.Belos.SolverTypes.BlockGMRES.maximum_iterations = 2000
 lss.SolutionStrategy.Parameters.LinearSolverTypes.Belos.SolverTypes.BlockGMRES.num_blocks = 1000
 
+lss = kaea.LSS.LSS
+lss.SolutionStrategy.Parameters.preconditioner_type = 'ML'
+lss.SolutionStrategy.Parameters.PreconditionerTypes.ML.MLSettings.add_parameter(name = 'ML output', value = 0)
+lss.SolutionStrategy.Parameters.PreconditionerTypes.ML.MLSettings.default_values = 'NSSA'
+lss.SolutionStrategy.Parameters.PreconditionerTypes.ML.MLSettings.aggregation_type = 'Uncoupled'
+lss.SolutionStrategy.Parameters.PreconditionerTypes.ML.MLSettings.smoother_type = 'symmetric block Gauss-Seidel'#'Chebyshev'
+
+
+lss.SolutionStrategy.Parameters.PreconditionerTypes.ML.MLSettings.smoother_sweeps = 4
+lss.SolutionStrategy.Parameters.PreconditionerTypes.ML.MLSettings.smoother_pre_or_post = 'post'
+
+lss.SolutionStrategy.Parameters.LinearSolverTypes.Belos.solver_type = 'Block GMRES'
+lss.SolutionStrategy.Parameters.LinearSolverTypes.Belos.SolverTypes.BlockGMRES.convergence_tolerance = 1e-5
+lss.SolutionStrategy.Parameters.LinearSolverTypes.Belos.SolverTypes.BlockGMRES.maximum_iterations = 2000
+lss.SolutionStrategy.Parameters.LinearSolverTypes.Belos.SolverTypes.BlockGMRES.num_blocks = 1000
 
 
 # time setup
@@ -148,7 +194,7 @@ time.end_time = num_steps*tstep
 # solver.InitialConditions.execute()
 
 # run the simulation (forward, NS only)
-solver.TimeLoop.options.disabled_actions = ['Adjoint']
+solver.TimeLoop.options.disabled_actions = ['Adjoint','keAdjoint']
 model.simulate()
 
 # run adjoint, starting from converged NS solution
